cfa3a4c9db688c34f205a905c04434537911d443
sam.h <sam.h@binance.com>
2021-11-16 12:45:35 +0800

    hoist all the vai user related data fetch logic to global context to avoid duplicated requests, thus improve performance

--- src/hooks/useVaiUser.js
+++ src/hooks/useVaiUser.js
@@ -1,51 +1,12 @@
-import { useCallback, useEffect, useState } from 'react';
-import useRefresh from './useRefresh';
-import { useComptroller, useVaiToken, useVaiUnitroller } from './useContract';
-import { useWeb3React } from '@web3-react/core';
-import BigNumber from 'bignumber.js';
-import { getVaiUnitrollerAddress } from '../utilities/addressHelpers';
+import { useContext } from 'react';
+import { VaiContext } from '../context/VaiContext';
 
 export const useVaiUser = () => {
-  const [userVaiMinted, setMintedAmount] = useState(new BigNumber(0));
-  const [userVaiBalance, setWalletAmount] = useState(new BigNumber(0));
-  const [userVaiEnabled, setEnabled] = useState(false);
-  const [mintableVai, setMintableAmount] = useState(new BigNumber(0));
-
-  const { fastRefresh } = useRefresh();
-  const comptrollerContract = useComptroller();
-  const vaiControllerContract = useVaiUnitroller();
-  const vaiContract = useVaiToken();
-  const { account } = useWeb3React();
-
-  const updateVaiInfo = useCallback(async () => {
-    if (!account) {
-      return;
-    }
-    let [
-      userVaiBalance,
-      userVaiMinted,
-      { 1: mintableVai },
-      allowBalance
-    ] = await Promise.all([
-      vaiContract.methods.balanceOf(account).call(),
-      comptrollerContract.methods.mintedVAIs(account).call(),
-      vaiControllerContract.methods.getMintableVAI(account).call(),
-      vaiContract.methods.allowance(account, getVaiUnitrollerAddress()).call()
-    ]);
-    userVaiBalance = new BigNumber(userVaiBalance).div(1e18);
-    userVaiMinted = new BigNumber(userVaiMinted).div(1e18);
-    mintableVai = new BigNumber(mintableVai).div(1e18);
-    allowBalance = new BigNumber(allowBalance).div(1e18);
-
-    setMintedAmount(userVaiMinted);
-    setWalletAmount(userVaiBalance);
-    setEnabled(allowBalance.gte(userVaiMinted));
-    setMintableAmount(mintableVai);
-  }, [vaiControllerContract, vaiContract, comptrollerContract, account]);
-
-  useEffect(() => {
-    updateVaiInfo();
-  }, [fastRefresh, updateVaiInfo]);
-
+  const {
+    userVaiMinted,
+    userVaiBalance,
+    userVaiEnabled,
+    mintableVai
+  } = useContext(VaiContext);
   return { userVaiMinted, userVaiBalance, userVaiEnabled, mintableVai };
 };
