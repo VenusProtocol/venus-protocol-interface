200bf884c3372b4e325065b9ddb37b763195df1c
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-07 19:48:21 +0300

    Fix rule: @typescript-eslint/no-shadow

--- src/hooks/wallet.ts
+++ src/hooks/wallet.ts
@@ -40,16 +40,16 @@ export function useInactiveListener(suppress = false) {
     if (ethereum && ethereum.on && !active && !error && !suppress) {
       const handleChainChanged = () => {
         // eat errors
-        activate(injected, undefined, true).catch((error) => {
-          console.error('Failed to activate after chain changed', error);
+        activate(injected, undefined, true).catch((err) => {
+          console.error('Failed to activate after chain changed', err);
         });
       };
 
       const handleAccountsChanged = (accounts: $TSFixMe) => {
         if (accounts.length > 0) {
           // eat errors
-          activate(injected, undefined, true).catch((error) => {
-            console.error('Failed to activate after accounts changed', error);
+          activate(injected, undefined, true).catch((err) => {
+            console.error('Failed to activate after accounts changed', err);
           });
         }
       };
