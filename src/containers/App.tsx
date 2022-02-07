5d3b08dd19d44e0bcac4795eddb48e8abbc03074
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-07 15:28:33 +0300

    package-lock->yarn.lock

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'reac... Remove this comment to see the full error message
 import { Provider } from 'react-redux';
 import {
   BrowserRouter, Route, Redirect, Switch,
--- src/core/modules/account/connectAccount.ts
+++ src/core/modules/account/connectAccount.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'reac... Remove this comment to see the full error message
 import { connect } from 'react-redux';
 import { accountActionCreators } from 'core/modules/account/actions';
 
