287ca3fefc4a48341741c9673ca787ba9acc55b8
Corey Rice <coreyarice@gmail.com>
2022-02-07 18:20:58 -0300

    Add types and remove comments for redux-actions

--- src/core/modules/account/actions.ts
+++ src/core/modules/account/actions.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'redu... Remove this comment to see the full error message
 import { createAction } from 'redux-actions';
 import { createPromiseAction } from 'core/modules/utils';
 
