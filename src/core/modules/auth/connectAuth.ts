bcf81f77cb8b002246ed4d118ce4496eb1556f00
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-07 15:28:33 +0300

    package-lock->yarn.lock

--- src/core/modules/auth/connectAuth.ts
+++ src/core/modules/auth/connectAuth.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'reac... Remove this comment to see the full error message
 import { connect } from 'react-redux';
 import { authActionCreators } from 'core/modules/auth/actions';
 
