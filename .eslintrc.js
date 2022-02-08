c585bdc030361f8cc55f2737c094178d996d691a
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-08 14:03:21 +0300

    set up airbnb linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,12 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   plugins: ['import', 'react', 'jsx-a11y', 'prettier'],
-  extends: ['airbnb-typescript', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
@@ -14,6 +19,28 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
     // handled by prettier
     '@typescript-eslint/indent': 0,
+
+    /* airbnb rules */
+    'implicit-arrow-linebreak': 0,
+    'import/prefer-default-export': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'max-len': [0,
+      {
+        code: 100,
+        ignoreComments: true,
+      },
+    ],
+    'no-param-reassign': 0,
+    'no-plusplus': 0,
+    'operator-linebreak': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-indent': 1,
+    'react/jsx-no-target-blank': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/no-array-index-key': 0,
+    'react/no-unused-prop-types': 0,
   },
   settings: {
     'import/resolver': {
