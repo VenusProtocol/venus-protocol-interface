0e32d30efdb2eb905d1bd09f40bcfc04473544ea
Corey Rice <coreyarice@gmail.com>
2022-02-08 12:16:26 -0300

    :shirt:

--- src/types/comma-number.d.ts
+++ src/types/comma-number.d.ts
@@ -1,6 +1,5 @@
 // PR to add type Definitely Typed `https://github.com/DefinitelyTyped/DefinitelyTyped/pull/58653
 declare module 'comma-number' {
-
   declare function commaNumber(
     inputNumber: number | string,
     optionalSeparator?: string,
