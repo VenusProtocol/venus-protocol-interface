eced207bb03171e29bf3c82d79c1dc02f28c14a8
Corey Rice <coreyarice@gmail.com>
2022-02-11 11:37:08 -0300

    configure css-in-js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,31 +1,24 @@
 module.exports = {
   root: true,
-  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss'],
+  customSyntax: '@stylelint/postcss-css-in-js',
   rules: {
     'at-rule-empty-line-before': null,
     'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: [
-          'function',
-          'if',
-          'else',
-          'return',
-          'each',
-          'include',
-          'mixin'
-        ]
-      }
+        ignoreAtRules: ['function', 'if', 'else', 'return', 'each', 'include', 'mixin'],
+      },
     ],
     'block-closing-brace-newline-after': [
       'always',
       {
-        ignoreAtRules: ['if', 'else']
-      }
+        ignoreAtRules: ['if', 'else'],
+      },
     ],
     'declaration-empty-line-before': null,
     'rule-empty-line-before': null,
-    'selector-list-comma-newline-after': null
-  }
+    'selector-list-comma-newline-after': null,
+  },
 };
