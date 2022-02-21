af91f3c914a76ff51a5995a101893d042d5eefd2
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-21 17:41:13 +0300

    remove styled engine provider for scss support

--- src/theme/MuiThemeProvider/MuiThemeProvider.tsx
+++ src/theme/MuiThemeProvider/MuiThemeProvider.tsx
@@ -1,16 +1,13 @@
 import React, { ReactNode } from 'react';
 import { CssBaseline } from '@mui/material';
-import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import mainTheme from './muiTheme';
 
 export function MuiThemeProvider({ children }: { children: ReactNode }) {
   return (
-    // https://mui.com/guides/interoperability/#css-injection-order-3
-    <StyledEngineProvider injectFirst={false}>
-      <ThemeProvider theme={mainTheme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <ThemeProvider theme={mainTheme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
   );
 }
