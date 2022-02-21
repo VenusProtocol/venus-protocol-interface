d4a8202f57a6c3229f8a0bbd0d443d5c3a068008
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-21 19:20:48 +0300

    move styles to separate files

--- src/components/v2/Layout/Layout.tsx
+++ src/components/v2/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode } from 'react';
 import Box from '@mui/material/Box';
-import { Sidebar } from './Sidebar';
-import { Header } from './Header';
-import { PageContainer } from './PageContainer';
+import { Sidebar } from './Sidebar/Sidebar';
+import { Header } from './Header/Header';
+import { PageContainer } from './PageContainer/PageContainer';
 
 export const Layout = ({ children }: { children: ReactNode }) => (
-  <Box sx={{ display: 'flex' }}>
+  <Box display="flex">
     <Header pageTitle="Dashboard" />
     <Sidebar />
     <PageContainer>{children}</PageContainer>
