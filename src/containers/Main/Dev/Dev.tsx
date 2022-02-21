94fe5011a54656c25503098c9d3039c55a440aeb
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-21 18:46:36 +0300

    add index.ts for lauout

--- src/containers/Main/Dev/Dev.tsx
+++ src/containers/Main/Dev/Dev.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import { Layout } from '../../../components/v2/Layout/Layout';
+import { Layout } from 'components/v2/Layout';
 
 export const Dev = () => <Layout>Dev page</Layout>;
