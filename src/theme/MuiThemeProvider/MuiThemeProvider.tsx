ccdf14e320d898e357e9db0af091f283e54f1978
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-02-22 16:21:15 +0300

    fix lint issues

--- src/theme/MuiThemeProvider/MuiThemeProvider.tsx
+++ src/theme/MuiThemeProvider/MuiThemeProvider.tsx
@@ -1,15 +1,15 @@
 import React, { ReactNode } from 'react';
-import {CssBaseline, StyledEngineProvider} from '@mui/material';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import mainTheme from './muiTheme';
 
 export function MuiThemeProvider({ children }: { children: ReactNode }) {
   return (
-      <StyledEngineProvider injectFirst>
-    <ThemeProvider theme={mainTheme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
-      </StyledEngineProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={mainTheme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
