f5890a3b7bf711bdd043ff28074f7557602041fb
Corey Rice <coreyarice@gmail.com>
2022-02-22 20:05:27 -0300

    warn gtag is missing outside of production

--- src/clients/googleAnalytics.ts
+++ src/clients/googleAnalytics.ts
@@ -3,11 +3,14 @@ type EventParameters = Parameters<(eventName: string, parameters?: Record<string
 const googleAnalytics = () => {
   let gtag: Gtag.Gtag;
   const loadGTag = (callback: (...args: EventParameters) => void) => (...args: EventParameters) => {
-    gtag = window?.gtag;
-    if (!gtag && process.env.NODE_ENV !== 'production') {
-      console.warn('gtag not loaded. Google Analytics event not sent', args);
-    } else {
+    ({ gtag } = window);
+    // This check exists because the gtag script is loaded outside of react and could potentially be missing
+    // although it is typed as if it alwasy exists
+    // @ts-expect-error gtag could be missing if script isn't loaded from document
+    if (gtag) {
       callback(...args);
+    } else if (process.env.NODE_ENV !== 'production') {
+      console.warn('gtag not loaded. Google Analytics event not sent', args);
     }
   };
 
