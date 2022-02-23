210634db408360f9e5071383d2db558ee9ba6b8a
Corey Rice <coreyarice@gmail.com>
2022-02-23 17:51:54 -0300

    Add wallet connect events

--- src/clients/googleAnalytics.spec.ts
+++ src/clients/googleAnalytics.spec.ts
@@ -13,14 +13,14 @@ describe('Google Analytics Client', () => {
   test('event is called when gtag exists on window', () => {
     window.gtag = () => {};
     const spy = jest.spyOn(window, 'gtag');
-    googleAnalytics.buttonPressed('connect');
+    googleAnalytics.buttonPressed('connect_wallet');
     expect(spy).toHaveBeenCalled();
   });
 
   test('calling event does not crash when gtag does not exit window', () => {
     // @ts-expect-error gtag could be missing if script isn't loaded from document
     window.gtag = undefined;
-    googleAnalytics.buttonPressed('connect');
+    googleAnalytics.buttonPressed('connect_wallet');
     expect(consoleSpy).toHaveBeenCalled();
     expect(consoleSpy.mock.calls[0][1]).toStrictEqual([
       'button_pressed',
--- src/components/Basic/ConnectButton.tsx
+++ src/components/Basic/ConnectButton.tsx
@@ -13,6 +13,7 @@ import logoImg from 'assets/img/logo.png';
 import { useWeb3React } from '@web3-react/core';
 import { Button } from 'components/v2/Button';
 import toast from 'components/Basic/Toast';
+import ga from 'clients/googleAnalytics';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { BASE_BSC_SCAN_URL } from '../../config';
 import { ConnectorNames } from '../../utilities/connectors';
@@ -257,9 +258,10 @@ function ConnectButton() {
   };
 
   const onConnect = useCallback(
-    (type) => {
+    type => {
       onClose();
       login(type);
+      ga.buttonPressed('connect_wallet', { type });
     },
     [login, setShowConnect],
   );
@@ -274,10 +276,7 @@ function ConnectButton() {
       >
         {!account
           ? 'Connect'
-          : `${account.substr(0, 6)}...${account.substr(
-            account.length - 4,
-            4,
-          )}`}
+          : `${account.substr(0, 6)}...${account.substr(account.length - 4, 4)}`}
       </Button>
       <Modal
         className="venus-modal"
@@ -290,12 +289,7 @@ function ConnectButton() {
         centered
       >
         <ModalContent className="flex flex-column align-center just-center">
-          <img
-            className="close-btn pointer"
-            src={closeImg}
-            alt="close"
-            onClick={onClose}
-          />
+          <img className="close-btn pointer" src={closeImg} alt="close" onClick={onClose} />
           {!account ? (
             <>
               <div className="flex flex-column align-center just-center header-content">
@@ -361,11 +355,7 @@ function ConnectButton() {
               </div>
               <p className="center terms-of-use">
                 <span>By connecting, I accept Venus&lsquo;s</span>
-                <a
-                  href="https://www.swipe.io/terms"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <a href="https://www.swipe.io/terms" target="_blank" rel="noreferrer">
                   Terms of Service
                 </a>
               </p>
@@ -379,10 +369,7 @@ function ConnectButton() {
                   <div
                     className="wallet-link-scan"
                     onClick={() => {
-                      window.open(
-                        `${BASE_BSC_SCAN_URL}/address/${account}`,
-                        '_blank',
-                      );
+                      window.open(`${BASE_BSC_SCAN_URL}/address/${account}`, '_blank');
                     }}
                   >
                     <span>View on BscScan</span>
