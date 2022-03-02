a9a158f64dab25d90f9294ca1d494811a7ca93b8
Corey Rice <coreyarice@gmail.com>
2022-03-02 13:19:51 -0300

    Revert "warn gtag is missing outside of production"
    This reverts commit f5890a3b7bf711bdd043ff28074f7557602041fb.

--- src/clients/googleAnalytics.ts
+++ src/clients/googleAnalytics.ts
@@ -9,14 +9,11 @@ export type ButtonEventName = keyof ButtonEventMap;
 const googleAnalytics = () => {
   let gtag: Gtag.Gtag;
   const loadGTag = (callback: (...args: EventParameters) => void) => (...args: EventParameters) => {
-    ({ gtag } = window);
-    // This check exists because the gtag script is loaded outside of react and could potentially be missing
-    // although it is typed as if it alwasy exists
-    // @ts-expect-error gtag could be missing if script isn't loaded from document
-    if (gtag) {
-      callback(...args);
-    } else if (process.env.NODE_ENV !== 'production') {
+    gtag = window?.gtag;
+    if (!gtag && process.env.NODE_ENV !== 'production') {
       console.warn('gtag not loaded. Google Analytics event not sent', args);
+    } else {
+      callback(...args);
     }
   };
 
