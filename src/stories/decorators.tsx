ea89e051c8d792c9c5deeb213b4791246a7b8222
Corey Rice <coreyarice@gmail.com>
2022-03-03 13:16:26 -0300

    Add decorator to center stories

--- src/stories/decorators.tsx
+++ src/stories/decorators.tsx
@@ -4,6 +4,7 @@ import { addDecorator } from '@storybook/react';
 import { Provider } from 'react-redux';
 import { store } from 'core/store';
 import { Web3ReactProvider } from '@web3-react/core';
+import Box from '@mui/material/Box';
 import { getLibrary } from 'utilities/connectors';
 import Web3ReactManager from 'utilities/Web3ReactManager';
 import { MarketContextProvider } from 'context/MarketContext';
@@ -58,3 +59,22 @@ export const withThemeProvider: DecoratorFunction = Story => (
     </EmotionThemeProvider>
   </Theme>
 );
+
+export const withCenterStory: (props: { width: number }) => DecoratorFunction = props => {
+  const { width } = props;
+  return Story => (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <Box sx={{ flexShrink: 0, width }}>
+        <Story />
+      </Box>
+    </Box>
+  );
+};
