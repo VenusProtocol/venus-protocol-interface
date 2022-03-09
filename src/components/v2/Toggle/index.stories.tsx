21b3e9604f58e869bad1e2aa24360e85efa39e79
therealemjy <maxime.julian.creations@gmail.com>
2022-03-09 15:56:11 +0000

    rebase with develop

--- src/components/v2/Toggle/index.stories.tsx
+++ src/components/v2/Toggle/index.stories.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { ComponentMeta } from '@storybook/react';
-import { withThemeProvider } from 'stories/decorators';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import { withThemeProvider, withCenterStory } from 'stories/decorators';
 import { Toggle } from '.';
 
 export default {
   title: 'Components/Toggle',
   component: Toggle,
-  decorators: [withThemeProvider],
+  decorators: [withThemeProvider, withCenterStory({ width: 600 })],
 } as ComponentMeta<typeof Toggle>;
 
 export const Default = () => <Toggle onChange={console.log} />;
