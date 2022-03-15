7a8202759fe1844713a1366cce2a78347898709a
therealemjy <maxime.julian.creations@gmail.com>
2022-03-15 14:58:44 +0000

    refactor auth flow to allow users to log out

--- src/hooks/wallet.ts
+++ src/hooks/wallet.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { injected } from '../utilities/connectors';
+import { LS_KEY_IS_USER_LOGGED_IN } from '../config';
 
 export function useEagerConnect() {
   const { activate, active } = useWeb3React();
@@ -8,15 +9,30 @@ export function useEagerConnect() {
   const [tried, setTried] = useState(!process.env.STORYBOOK);
 
   useEffect(() => {
-    injected.isAuthorized().then(isAuthorized => {
-      if (isAuthorized) {
-        activate(injected, undefined, true).catch(() => {
-          setTried(true);
-        });
-      } else {
+    const init = async () => {
+      // Check if user previously connected their wallet with the dApp
+      const isUserConnected = window.localStorage.getItem(LS_KEY_IS_USER_LOGGED_IN);
+      if (!isUserConnected) {
+        return;
+      }
+
+      // Check if user previously connected their wallet with the dApp
+      const isAuthorized = await injected.isAuthorized();
+      if (!isAuthorized) {
+        setTried(true);
+        return;
+      }
+
+      // Fetch user account details
+      try {
+        await activate(injected, undefined, true);
+      } catch {
+        // TODO: handle error (?)
         setTried(true);
       }
-    });
+    };
+
+    init();
   }, []); //eslint-disable-line
   // intentionally only running on mount(make sure it's only mounted once :))
 
