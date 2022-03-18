28fb6e9e8683f92d173600863b363bebf0c0423e
therealemjy <maxime.julian.creations@gmail.com>
2022-03-18 08:35:29 +0000

    add link to XVS in footer

--- src/components/v2/Layout/Footer/Footer.tsx
+++ src/components/v2/Layout/Footer/Footer.tsx
@@ -8,6 +8,7 @@ import {
   VENUS_DISCORD_URL,
   VENUS_TWITTER_URL,
   VENUS_GITHUB_URL,
+  ETHERSCAN_XVS_URL,
 } from 'config';
 import { Icon } from 'components/v2/Icon';
 import { useStyles } from './styles';
@@ -32,20 +33,24 @@ export const Footer: React.FC<IFooterProps> = ({ currentBlockNumber }) => {
         Latest Block:&nbsp;<span css={styles.StatusBlockNumber}>{currentBlockNumber}</span>
       </Typography>
 
-      <div css={styles.SocialMediaLinks}>
-        <a css={styles.SocialMediaLink} href={VENUS_MEDIUM_URL} target="_blank" rel="noreferrer">
+      <div css={styles.Links}>
+        <a css={styles.Link} href={ETHERSCAN_XVS_URL} target="_blank" rel="noreferrer">
+          <Icon name="venus" color={styles.theme.palette.text.primary} size="12px" />
+        </a>
+
+        <a css={styles.Link} href={VENUS_MEDIUM_URL} target="_blank" rel="noreferrer">
           <Icon name="medium" color={styles.theme.palette.text.primary} size="12px" />
         </a>
 
-        <a css={styles.SocialMediaLink} href={VENUS_DISCORD_URL} target="_blank" rel="noreferrer">
+        <a css={styles.Link} href={VENUS_DISCORD_URL} target="_blank" rel="noreferrer">
           <Icon name="discord" color={styles.theme.palette.text.primary} size="12px" />
         </a>
 
-        <a css={styles.SocialMediaLink} href={VENUS_TWITTER_URL} target="_blank" rel="noreferrer">
+        <a css={styles.Link} href={VENUS_TWITTER_URL} target="_blank" rel="noreferrer">
           <Icon name="twitter" color={styles.theme.palette.text.primary} size="12px" />
         </a>
 
-        <a css={styles.SocialMediaLink} href={VENUS_GITHUB_URL} target="_blank" rel="noreferrer">
+        <a css={styles.Link} href={VENUS_GITHUB_URL} target="_blank" rel="noreferrer">
           <Icon name="github" color={styles.theme.palette.text.primary} size="12px" />
         </a>
       </div>
