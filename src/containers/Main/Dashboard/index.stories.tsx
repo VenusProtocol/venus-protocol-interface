6913c6746577ab05ceea009bea51f58c23cf9960
Corey Rice <coreyarice@gmail.com>
2022-03-21 10:50:44 -0600

    Remove extra decorators that break stories

--- src/containers/Main/Dashboard/index.stories.tsx
+++ src/containers/Main/Dashboard/index.stories.tsx
@@ -1,17 +1,11 @@
 import { ComponentMeta } from '@storybook/react';
-import {
-  withRouter,
-  withProvider,
-  withWeb3Provider,
-  withMarketContext,
-  withVaiContext,
-} from 'stories/decorators';
+import { withRouter, withProvider } from 'stories/decorators';
 import Dashboard from '.';
 
 export default {
   title: 'Pages/Dashboard',
   component: Dashboard,
-  decorators: [withRouter, withProvider, withWeb3Provider, withMarketContext, withVaiContext],
+  decorators: [withRouter, withProvider],
 } as ComponentMeta<typeof Dashboard>;
 
 export { Dashboard };
