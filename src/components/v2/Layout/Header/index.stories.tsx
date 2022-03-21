c9c4a8ad454b00f9eee0a638aee767788d1bb41d
Corey Rice <coreyarice@gmail.com>
2022-03-21 10:50:44 -0600

    Remove extra decorators that break stories

--- src/components/v2/Layout/Header/index.stories.tsx
+++ src/components/v2/Layout/Header/index.stories.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import { ComponentMeta } from '@storybook/react';
-import {
-  withRouter,
-  withProvider,
-  withWeb3Provider,
-  withMarketContext,
-  withVaiContext,
-  withThemeProvider,
-} from 'stories/decorators';
+import { withRouter, withProvider } from 'stories/decorators';
 import { Header } from '.';
 
 export default {
   title: 'Components/Layout/Header',
   component: Header,
-  decorators: [
-    withRouter,
-    withProvider,
-    withWeb3Provider,
-    withMarketContext,
-    withVaiContext,
-    withThemeProvider,
-  ],
+  decorators: [withRouter, withProvider],
 } as ComponentMeta<typeof Header>;
 
 export const HeaderDefault = () => <Header pageTitle="Hello from storybook" />;
