8cba6456504addb4443b2176b9d07bb42acef48d
Corey Rice <coreyarice@gmail.com>
2022-03-21 10:50:44 -0600

    Remove extra decorators that break stories

--- src/containers/Main/Vault/index.stories.tsx
+++ src/containers/Main/Vault/index.stories.tsx
@@ -1,17 +1,11 @@
 import { ComponentMeta } from '@storybook/react';
-import {
-  withRouter,
-  withProvider,
-  withWeb3Provider,
-  withMarketContext,
-  withVaiContext,
-} from 'stories/decorators';
+import { withRouter, withProvider } from 'stories/decorators';
 import Vault from '.';
 
 export default {
   title: 'Pages/Vault',
   component: Vault,
-  decorators: [withRouter, withProvider, withWeb3Provider, withMarketContext, withVaiContext],
+  decorators: [withRouter, withProvider],
 } as ComponentMeta<typeof Vault>;
 
 export { Vault };
