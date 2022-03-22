e2ce7bc50aaa47f7ba26fef17253fcd2f3bd1156
therealemjy <maxime.julian.creations@gmail.com>
2022-03-22 14:09:44 +0000

    refactor Button component to support all variants

--- src/components/v2/Button/index.stories.tsx
+++ src/components/v2/Button/index.stories.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
 import { ComponentMeta } from '@storybook/react';
 import { withThemeProvider, withCenterStory } from 'stories/decorators';
-import { PrimaryButton, SecondaryButton, TextButton, Button } from '.';
+import { PrimaryButton, SecondaryButton, TertiaryButton, TextButton, Button } from '.';
 
 export default {
   title: 'Components/Button',
   component: Button,
-  decorators: [withThemeProvider, withCenterStory({ width: 150 })],
+  decorators: [withThemeProvider, withCenterStory({ width: 250 })],
 } as ComponentMeta<typeof Button>;
 
-export const Primary = () => <PrimaryButton onClick={console.log}>Primary</PrimaryButton>;
+export const Primary = () => <PrimaryButton onClick={console.log} label="Primary" />;
+export const Secondary = () => <SecondaryButton onClick={console.log} label="Secondary" />;
+export const Tertiary = () => <TertiaryButton onClick={console.log} label="Tertiary" />;
+export const Text = () => <TextButton onClick={console.log} label="Text" />;
 
-export const PrimaryDisabled = () => (
-  <PrimaryButton onClick={console.log} disabled>
-    Primary
-  </PrimaryButton>
-);
-
-export const Loading = () => (
-  <PrimaryButton onClick={console.log} loading>
-    Loading
-  </PrimaryButton>
-);
+export const Small = () => <PrimaryButton onClick={console.log} label="Small" small />;
+export const FullWidth = () => <PrimaryButton onClick={console.log} fullWidth label="Full width" />;
+export const Disabled = () => <PrimaryButton onClick={console.log} disabled label="Disabled" />;
+export const Loading = () => <PrimaryButton onClick={console.log} loading label="Loading" />;
 Loading.story = {
   parameters: {
     loki: { skip: true },
   },
 };
-
-export const Secondary = () => <SecondaryButton onClick={console.log}>Secondary</SecondaryButton>;
-
-export const Text = () => <TextButton onClick={console.log}>Text</TextButton>;
