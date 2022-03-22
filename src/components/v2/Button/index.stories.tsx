88bcbab9b0f35528bc745d28a99e993ef11a5ed0
therealemjy <maxime.julian.creations@gmail.com>
2022-03-22 15:35:52 +0000

    update Tabs style

--- src/components/v2/Button/index.stories.tsx
+++ src/components/v2/Button/index.stories.tsx
@@ -9,15 +9,34 @@ export default {
   decorators: [withThemeProvider, withCenterStory({ width: 250 })],
 } as ComponentMeta<typeof Button>;
 
-export const Primary = () => <PrimaryButton onClick={console.log} label="Primary" />;
-export const Secondary = () => <SecondaryButton onClick={console.log} label="Secondary" />;
-export const Tertiary = () => <TertiaryButton onClick={console.log} label="Tertiary" />;
-export const Text = () => <TextButton onClick={console.log} label="Text" />;
+export const Primary = () => <PrimaryButton onClick={console.log}>Primary</PrimaryButton>;
+export const Secondary = () => <SecondaryButton onClick={console.log}>Secondary</SecondaryButton>;
+export const Tertiary = () => <TertiaryButton onClick={console.log}>Tertiary</TertiaryButton>;
+export const Text = () => <TextButton onClick={console.log}>Text</TextButton>;
 
-export const Small = () => <PrimaryButton onClick={console.log} label="Small" small />;
-export const FullWidth = () => <PrimaryButton onClick={console.log} fullWidth label="Full width" />;
-export const Disabled = () => <PrimaryButton onClick={console.log} disabled label="Disabled" />;
-export const Loading = () => <PrimaryButton onClick={console.log} loading label="Loading" />;
+export const Small = () => (
+  <PrimaryButton onClick={console.log} small>
+    Small
+  </PrimaryButton>
+);
+
+export const FullWidth = () => (
+  <PrimaryButton onClick={console.log} fullWidth>
+    Full width
+  </PrimaryButton>
+);
+
+export const Disabled = () => (
+  <PrimaryButton onClick={console.log} disabled>
+    Disabled
+  </PrimaryButton>
+);
+
+export const Loading = () => (
+  <PrimaryButton onClick={console.log} loading>
+    Loading
+  </PrimaryButton>
+);
 Loading.story = {
   parameters: {
     loki: { skip: true },
