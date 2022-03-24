1498dfbde78a9769ef670a76448b771afd8b0c37
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-03-24 21:23:45 +0400

    Add modal wrapper

--- src/components/v2/Icon/icons/index.tsx
+++ src/components/v2/Icon/icons/index.tsx
@@ -21,3 +21,4 @@ export { ReactComponent as twitter } from './twitter.svg';
 export { ReactComponent as github } from './github.svg';
 export { ReactComponent as venus } from './venus.svg';
 export { ReactComponent as convert } from './convert.svg';
+export { ReactComponent as close } from './close.svg';
