a9e571d799ad05b6e00a5ecd2a5a7b7ebeed8264
therealemjy <maxime.julian.creations@gmail.com>
2022-03-24 08:46:54 +0000

    decouple AuthModal

--- src/components/v2/AuthModal/index.stories.tsx
+++ src/components/v2/AuthModal/index.stories.tsx
@@ -11,4 +11,17 @@ export default {
   decorators: [withThemeProvider, withCenterStory({ width: 800 })],
 } as ComponentMeta<typeof AuthModal>;
 
-export const Default = () => <AuthModal isOpen onClose={noop} onLogOut={noop} onLogin={noop} />;
+export const Default = () => (
+  <AuthModal isOpen onClose={noop} onLogOut={noop} onLogin={noop} onCopyAccount={noop} />
+);
+
+export const WithAccount = () => (
+  <AuthModal
+    isOpen
+    onClose={noop}
+    onLogOut={noop}
+    onLogin={noop}
+    onCopyAccount={noop}
+    account="0x2Ce1d0ffD7E869D9DF33e28552b12DdDed326706"
+  />
+);
