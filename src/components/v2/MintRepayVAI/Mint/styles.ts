10501919807d1c0315983cfdcece46b485dfd109
therealemjy <mjulian@ciedigital.com>
2022-03-29 14:46:57 +0100

    improve min and max options on TextField + add coin info

--- src/components/v2/MintRepayVAI/Mint/styles.ts
+++ src/components/v2/MintRepayVAI/Mint/styles.ts
@@ -5,6 +5,28 @@ export const useStyles = () => {
   const theme = useTheme();
 
   return {
-    container: css``,
+    textField: css`
+      margin-bottom: ${theme.spacing(4)};
+    `,
+    getRow: ({ isLast }: { isLast: boolean }) => css`
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      margin-bottom: ${theme.spacing(isLast ? 3 : 2)};
+    `,
+    infoColumn: css`
+      display: flex;
+      align-items: center;
+      padding-right: ${theme.spacing(2)};
+    `,
+    coinIcon: css`
+      width: 20px;
+      height: 20px;
+      margin-right: ${theme.spacing(1)};
+    `,
+    infoValue: css`
+      color: ${theme.palette.text.primary};
+    `,
   };
 };
