364ad6aebb5fa5ae60640a69322861dc6fba1dba
Corey Rice <coreyarice@gmail.com>
2022-03-31 13:03:00 -0600

    Remove chain flag for conversion

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -78,9 +78,7 @@ class App extends React.Component {
                           <Route exact path="/vote/leaderboard" component={VoterLeaderboard} />
                           <Route exact path="/vote/proposal/:id" component={VoteOverview} />
                           <Route exact path="/vote/address/:address" component={ProposerDetail} />
-                          {process.env.REACT_APP_CHAIN_ID === '97' && (
-                            <Route exact path="/convert-vrt" component={VrtConversion} />
-                          )}
+                          <Route exact path="/convert-vrt" component={VrtConversion} />
                           {process.env.REACT_APP_CHAIN_ID === '97' && (
                             <Route exact path="/faucet" component={Faucet} />
                           )}
