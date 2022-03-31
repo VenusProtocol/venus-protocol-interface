fd807f1ce4b18990ad042af97dddc07f1d693a52
therealemjy <maxime.julian.creations@gmail.com>
2022-03-31 16:15:57 +0100

    WIP

--- src/components/v2/BorrowMarket/types.ts
+++ src/components/v2/BorrowMarket/types.ts
@@ -1,6 +1,8 @@
+import BigNumber from 'bignumber.js';
 import { Asset } from 'types';
 
 export type BorrowAsset = Pick<Asset, 'id' | 'name'> & {
+  walletBalanceCoins: BigNumber;
   liquidityCents: Asset['liquidity'];
   borrowApyPercentage: number;
 };
