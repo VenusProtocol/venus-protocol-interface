1ac7db7c3335d1514410fa79fddaa2b4cbe3307e
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-04-04 17:22:00 +0100

    Create BorrowMarket component (#252)
    * create basic BorrowMarket component
    
    * add logic for formatting data from contracts into borrow assets
    
    * add logic for formatting data into rows
    
    * add coin icons + update token cell
    
    * create formatDollarsToReadableValue util
    
    * add initialOrder prop to Table component
    
    * design fixes on Table component
    
    * fix non-orderable table headers
    
    * fix font weight of table cells
    
    * add Token component
    
    * fix alignment and color of sorting icon in Table header
    
    * fix style linting issue
    
    * add TODOs about handling loading and error states to Table

---
+++ src/components/v2/BorrowMarket/styles.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+import { useTheme } from '@mui/material';
+
+export const useStyles = () => {
+  const theme = useTheme();
+
+  return {
+    container: css`
+      width: 100%;
+      background-color: ${theme.palette.background.paper};
+      border-radius: ${theme.shape.borderRadius.large}px;
+      padding: ${theme.spacing(3, 0, 1)};
+    `,
+    tokenCell: css`
+      display: flex;
+      align-items: center;
+    `,
+    tokenCellIcon: css`
+      margin-right: ${theme.spacing(1)};
+      width: ${theme.shape.iconSize.large}px;
+      height: ${theme.shape.iconSize.large}px;
+    `,
+  };
+};
