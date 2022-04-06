c55ae3dca59d3af775d253b6a05c26bfb31c0be0
therealemjy <maxime.julian.creations@gmail.com>
2022-04-06 14:32:57 +0100

    add mintVai mutation to MintVai container

--- src/pages/Dashboard/MintRepayVai/MintVai/index.tsx
+++ src/pages/Dashboard/MintRepayVai/MintVai/index.tsx
@@ -8,6 +8,8 @@ import { AuthContext } from 'context/AuthContext';
 import { SecondaryButton, LabeledInlineContent, TokenTextField } from 'components';
 import { useVaiUser } from 'hooks/useVaiUser';
 import useGetVaiTreasuryPercentage from 'hooks/operations/queries/useGetVaiTreasuryPercentage';
+import useMintVai from 'hooks/operations/mutations/useMintVai';
+import toast from 'components/Basic/Toast';
 import { VAI_SYMBOL } from '../constants';
 import getReadableFeeVai from './getReadableFeeVai';
 import { useStyles } from '../styles';
@@ -15,7 +17,7 @@ import { useStyles } from '../styles';
 export interface IMintVaiUiProps {
   disabled: boolean;
   isMintVaiLoading: boolean;
-  onSubmit: (value: BigNumber) => Promise<void>;
+  onSubmit: (value: BigNumber) => void;
   limitWei?: BigNumber;
   mintFeePercentage?: number;
 }
@@ -30,7 +32,6 @@ export const MintVaiUi: React.FC<IMintVaiUiProps> = ({
   const styles = useStyles();
 
   // Convert limit into VAI
-  // @TODO: check if we need to apply the 40% of user borrow balance limit like in the current app
   const readableVaiLimit = React.useMemo(
     () =>
       !limitWei
@@ -42,6 +43,8 @@ export const MintVaiUi: React.FC<IMintVaiUiProps> = ({
           }).toString(),
     [limitWei?.toString()],
   );
+
+  const hasMintableVai = limitWei?.isGreaterThan(0) || false;
 
   const getReadableMintFee = React.useCallback(
     (valueWei: BigNumber | '') => {
@@ -57,17 +60,17 @@ export const MintVaiUi: React.FC<IMintVaiUiProps> = ({
 
   return (
     <AmountForm onSubmit={onSubmit}>
-      {({ values, setFieldValue, handleBlur, isSubmitting, isValid, touched }) => (
+      {({ values, setFieldValue, handleBlur, isValid }) => (
         <>
           <TokenTextField
             name="amount"
             css={styles.textField}
             tokenSymbol={VAI_SYMBOL}
             value={values.amount}
-            onChange={amount => setFieldValue('amount', amount)}
+            onChange={amount => setFieldValue('amount', amount, true)}
             onBlur={handleBlur}
             maxWei={limitWei}
-            disabled={disabled || isSubmitting || isMintVaiLoading}
+            disabled={disabled || isMintVaiLoading || !hasMintableVai}
             rightMaxButtonLabel="SAFE MAX"
           />
 
@@ -89,8 +92,8 @@ export const MintVaiUi: React.FC<IMintVaiUiProps> = ({
 
           <SecondaryButton
             type="submit"
-            loading={isSubmitting || isMintVaiLoading}
-            disabled={disabled || !isValid || !touched.amount}
+            loading={isMintVaiLoading}
+            disabled={disabled || !isValid}
           >
             Mint VAI
           </SecondaryButton>
@@ -106,15 +109,25 @@ const MintVai: React.FC = () => {
   const { data: vaiTreasuryPercentage, isLoading: isGetVaiTreasuryPercentageLoading } =
     useGetVaiTreasuryPercentage();
 
+  const { mutate: mintVai, isLoading: isMintVaiLoading } = useMintVai({
+    onError: error => {
+      toast.error({ title: error.message });
+    },
+  });
+
   // Convert limit into wei of VAI
   const limitWei = React.useMemo(
     () => convertCoinsToWei({ value: mintableVai, tokenSymbol: VAI_SYMBOL }),
     [mintableVai.toString()],
   );
 
-  const onSubmit: IMintVaiUiProps['onSubmit'] = async value => {
-    // TODO: call contract
-    console.log('Amount to mint:', value.toString());
+  const onSubmit: IMintVaiUiProps['onSubmit'] = amountWei => {
+    if (account) {
+      mintVai({
+        fromAccountAddress: account.address,
+        amountWei,
+      });
+    }
   };
 
   return (
@@ -122,7 +135,7 @@ const MintVai: React.FC = () => {
       disabled={!account || isGetVaiTreasuryPercentageLoading}
       limitWei={limitWei}
       mintFeePercentage={vaiTreasuryPercentage}
-      isMintVaiLoading={false}
+      isMintVaiLoading={isMintVaiLoading}
       onSubmit={onSubmit}
     />
   );
