54bac9df9694a0040b85674363133bbb991037ee
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-04-07 13:31:03 -0300

    Adds the ValueUpdate Component (#266)
    This component takes two numbers and displays them horizontall with a colored arrow
    designation the direction change in value ( up or down)
    
    |name|type|required|description|
    ------------------------------
    |classname| string| false|optional className for applying css object styles|
    |original| number or BigNumber|true| the compared left value|
    |new|number or BigNumber|true|the compared right value|
    |format|function|false| optional function to format numbers defaults to formatCentsToReadableValue|

---
+++ src/components/v2/ValueUpdate/styles.ts
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+import { useTheme } from '@mui/material';
+
+export const useStyles = ({ increase }: { increase: boolean }) => {
+  const theme = useTheme();
+
+  return {
+    container: css`
+      display: flex;
+      align-items: center;
+    `,
+    icon: css`
+      color: ${increase ? theme.palette.interactive.success : theme.palette.interactive.error};
+      margin: 0 ${theme.spacing(1.25)};
+      width: ${theme.shape.iconSize.medium}px;
+      height: ${theme.shape.iconSize.medium}px;
+    `,
+  };
+};
