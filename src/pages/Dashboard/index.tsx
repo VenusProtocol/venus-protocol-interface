7fb24c0d51326ea6fc7eaec0cd0b62c36b5a5e8e
therealemjy <maxime.julian.creations@gmail.com>
2022-04-12 13:06:47 +0100

    wire up BorrowMarket + move Table title outside of container

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
+
 import SupplyMarket from './Markets/SupplyMarket';
+import BorrowMarket from './Markets/BorrowMarket';
 
 const DashboardUi: React.FC = () => (
   <div>
     <SupplyMarket />
+    <BorrowMarket />
   </div>
 );
 
