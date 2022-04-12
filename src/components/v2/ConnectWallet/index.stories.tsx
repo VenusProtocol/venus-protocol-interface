a924c698be59765815938de2a3da89a4dad0b42f
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-04-12 14:56:42 -0300

    Update connect wallet (#294)
    * Move logic into for rending children to UI component
    
    * Fix Connect Button story

--- src/components/v2/ConnectWallet/index.stories.tsx
+++ src/components/v2/ConnectWallet/index.stories.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import noop from 'noop-ts';
 
 import { ComponentMeta } from '@storybook/react';
-import { withWeb3Provider, withCenterStory } from 'stories/decorators';
+import { withCenterStory } from 'stories/decorators';
 import { Prompt as PromptUi } from '.';
 
 export default {
   title: 'Components/ConnectWallet',
   component: PromptUi,
-  decorators: [withWeb3Provider, withCenterStory({ width: 450 })],
+  decorators: [withCenterStory({ width: 450 })],
 } as ComponentMeta<typeof PromptUi>;
 
 export const Prompt = () => (
-  <PromptUi message="Please connect your wallet to mint VAI" openAuthModal={noop}>
+  <PromptUi message="Please connect your wallet to mint VAI" openAuthModal={noop} connected={false}>
     Protected content
   </PromptUi>
 );
