d219cf4984de1a871ca6828cf31065949fe77577
therealemjy <maxime.julian.creations@gmail.com>
2022-04-13 13:14:12 +0100

    remove @TODO

--- src/utilities/getToken.ts
+++ src/utilities/getToken.ts
@@ -1,8 +1,6 @@
 import { TokenSymbol, IToken } from 'types';
 import { TOKENS } from 'constants/tokens';
 
-// @TODO: refactor to take chain ID in consideration (currently handled in
-// constant file directly)
 const getToken = (key: TokenSymbol): IToken => TOKENS[key as keyof typeof TOKENS] as IToken;
 
 export default getToken;
