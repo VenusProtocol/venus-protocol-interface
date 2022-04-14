3d52b66991fb5f8085ce4ef533cc0b582b15f210
therealemjy <maxime.julian.creations@gmail.com>
2022-04-14 07:16:37 +0100

    fix output path used by i18n parser

--- src/components/v2/Layout/constants.tsx
+++ src/components/v2/Layout/constants.tsx
@@ -16,42 +16,49 @@ export const menuItems: IMenuItem[] = [
   {
     href: '/dashboard',
     // Translation key: do not remove this comment
+    // t('layout.menuItems.dashboard')
     i18nKey: 'layout.menuItems.dashboard',
     icon: 'dashboard',
   },
   {
     href: '/vote',
     // Translation key: do not remove this comment
+    // t('layout.menuItems.vote')
     i18nKey: 'layout.menuItems.vote',
     icon: 'vote',
   },
   {
     href: '/xvs',
     // Translation key: do not remove this comment
+    // t('layout.menuItems.xvs')
     i18nKey: 'layout.menuItems.xvs',
     icon: 'xvsOutlined',
   },
   {
     href: '/market',
     // Translation key: do not remove this comment
+    // t('layout.menuItems.market')
     i18nKey: 'layout.menuItems.market',
     icon: 'market',
   },
   {
     href: '/vault',
     // Translation key: do not remove this comment
+    // t('layout.menuItems.vault')
     i18nKey: 'layout.menuItems.vault',
     icon: 'vault',
   },
   {
     href: '/transaction',
     // Translation key: do not remove this comment
+    // t('layout.menuItems.history')
     i18nKey: 'layout.menuItems.history',
     icon: 'history',
   },
   {
     href: 'https://prdt.finance/XVS',
     // Translation key: do not remove this comment
+    // t('layout.menuItems.xvsPrediction')
     i18nKey: 'layout.menuItems.xvsPrediction',
     icon: 'market',
   },
@@ -64,12 +71,14 @@ if (isOnTestnet) {
     {
       href: '/convert-vrt',
       // Translation key: do not remove this comment
+      // t('layout.menuItems.convertXvs')
       i18nKey: 'layout.menuItems.convertXvs',
       icon: 'convert',
     },
     {
       href: '/faucet',
       // Translation key: do not remove this comment
+      // t('layout.menuItems.faucet')
       i18nKey: 'layout.menuItems.faucet',
       icon: 'info',
     },
