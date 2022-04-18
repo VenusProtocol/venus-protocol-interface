27538bb237c2bea6b38cb8c23cf2bf6024ff73fe
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-04-18 17:06:39 +0100

    Wire up MyAccount component (#299)
    * double spacings
    
    * fix small design issues on MyAccount + display ProgressBarHorizontal as block
    
    * fix small design issues on MyAccount + display ProgressBarHorizontal as block
    
    * allow undefined props to be passed to MyAccount
    
    * add basic container for MyAccount
    
    * add basic calculations
    
    * add calculations for borrow balance, supply balance and borrow limit
    
    * add calculation for daily earnings
    
    * add net APY calculation
    
    * fix alignment issues with info icons and tooltips
    
    * update tooltips content of progress bar
    
    * fix NaN issue with net APY
    
    * conditionally add XVS distribution to net APY
    
    * decouple UI from container
    
    * make sure 0 and undefined values are displayed properly
    
    * update tooltips content (2)
    
    * fix internationalization issues
    
    * add TODO comments
    
    * rename ITooltipProps to TooltipProps
    
    * move TODO
    
    * update type of Tooltip
    
    * add ariaLabel to translations
    
    * DOM updates
    
    * create PLACEHOLDER_KEY constant + small tooltip updates
    
    * extract variable calculations to highlight duplicated logic
    
    * create formatToReadablePercentage util + update return type of formatCentsToReadableValue
    
    * update Trans component usage
    
    * use userTotalBorrowBalance and userTotalBorrowLimit from useUserMarketInfo hook

---
+++ src/pages/Dashboard/MyAccount/MyAccountUi.tsx
@@ -0,0 +1,228 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+import PLACEHOLDER_KEY from 'constants/placeholderKey';
+import { formatCentsToReadableValue, formatToReadablePercentage } from 'utilities/common';
+import { useTranslation } from 'translation';
+import { IToggleProps, Toggle, Icon, ProgressBarHorizontal, Tooltip } from 'components';
+import { useMyAccountStyles as useStyles } from './styles';
+
+export interface IMyAccountUiProps {
+  netApyPercentage: number | undefined;
+  dailyEarningsCents: number | undefined;
+  supplyBalanceCents: number | undefined;
+  borrowBalanceCents: number | undefined;
+  borrowLimitCents: number | undefined;
+  safeBorrowLimitPercentage: number;
+  withXvs: boolean;
+  onXvsToggle: (newValue: boolean) => void;
+  className?: string;
+}
+
+export const MyAccountUi = ({
+  netApyPercentage,
+  dailyEarningsCents,
+  supplyBalanceCents,
+  borrowBalanceCents,
+  borrowLimitCents,
+  safeBorrowLimitPercentage,
+  withXvs,
+  onXvsToggle,
+  className,
+}: IMyAccountUiProps) => {
+  const styles = useStyles();
+  const { t, Trans } = useTranslation();
+
+  const handleXvsToggleChange: IToggleProps['onChange'] = (_event, checked) => onXvsToggle(checked);
+
+  let borrowLimitUsedPercentage: number | undefined;
+  if (borrowLimitCents === 0) {
+    borrowLimitUsedPercentage = 0;
+  } else if (typeof borrowBalanceCents === 'number' && typeof borrowLimitCents === 'number') {
+    borrowLimitUsedPercentage = Math.round((borrowBalanceCents * 100) / borrowLimitCents);
+  }
+
+  const readableBorrowLimitUsedPercentage = formatToReadablePercentage(borrowLimitUsedPercentage);
+
+  const safeBorrowLimitCents =
+    typeof borrowLimitCents === 'number'
+      ? Math.floor((borrowLimitCents * safeBorrowLimitPercentage) / 100)
+      : undefined;
+
+  const readableSafeBorrowLimit = formatCentsToReadableValue({
+    value: safeBorrowLimitCents,
+    removeDecimals: true,
+  });
+
+  const readableBorrowLimit = formatCentsToReadableValue({
+    value: borrowLimitCents,
+    removeDecimals: true,
+  });
+
+  const readableNetApyPercentage = formatToReadablePercentage(netApyPercentage);
+
+  const readableBorrowBalance = formatCentsToReadableValue({
+    value: borrowBalanceCents,
+  });
+
+  const readableDailyEarnings = formatCentsToReadableValue({
+    value: dailyEarningsCents,
+  });
+
+  const readableSupplyBalance = formatCentsToReadableValue({
+    value: supplyBalanceCents,
+  });
+
+  return (
+    <div css={styles.container} className={className}>
+      <div css={[styles.row, styles.header]}>
+        <Typography variant="h4">{t('myAccount.title')}</Typography>
+
+        <div css={styles.apyWithXvs}>
+          <Tooltip css={styles.tooltip} title={t('myAccount.apyWithXvsTooltip')}>
+            <Icon css={styles.infoIcon} name="info" />
+          </Tooltip>
+
+          <Typography
+            color="text.primary"
+            variant="small1"
+            component="span"
+            css={styles.apyWithXvsLabel}
+          >
+            {t('myAccount.apyWithXvs')}
+          </Typography>
+
+          <Toggle css={styles.toggle} value={withXvs} onChange={handleXvsToggleChange} />
+        </div>
+      </div>
+
+      <div css={styles.netApyContainer}>
+        <div css={styles.netApy}>
+          <Typography component="span" variant="small2" css={styles.netApyLabel}>
+            {t('myAccount.netApy')}
+          </Typography>
+
+          <Tooltip css={styles.tooltip} title={t('myAccount.netApyTooltip')}>
+            <Icon css={styles.infoIcon} name="info" />
+          </Tooltip>
+        </div>
+
+        <Typography variant="h1" color="interactive.success" component="span">
+          {readableNetApyPercentage}
+        </Typography>
+      </div>
+
+      <ul css={styles.list}>
+        <Typography component="li" variant="h4" css={styles.item}>
+          <Typography component="span" variant="small2" css={styles.labelListItem}>
+            {t('myAccount.dailyEarnings')}
+          </Typography>
+
+          {readableDailyEarnings}
+        </Typography>
+
+        <Typography component="li" variant="h4" css={styles.item}>
+          <Typography component="span" variant="small2" css={styles.labelListItem}>
+            {t('myAccount.supplyBalance')}
+          </Typography>
+
+          {readableSupplyBalance}
+        </Typography>
+
+        <Typography component="li" variant="h4" css={styles.item}>
+          <Typography component="span" variant="small2" css={styles.labelListItem}>
+            {t('myAccount.borrowBalance')}
+          </Typography>
+
+          {readableBorrowBalance}
+        </Typography>
+      </ul>
+
+      <div css={[styles.row, styles.topProgressBarLegend]}>
+        <div css={styles.inlineContainer}>
+          <Typography component="span" variant="small2" css={styles.inlineLabel}>
+            {t('myAccount.borrowLimitUsed')}
+          </Typography>
+
+          <Typography component="span" variant="small1" color="text.primary">
+            {readableBorrowLimitUsedPercentage}
+          </Typography>
+        </div>
+
+        <div css={styles.inlineContainer}>
+          <Typography component="span" variant="small2" css={styles.inlineLabel}>
+            {t('myAccount.limit')}
+          </Typography>
+
+          <Typography component="span" variant="small1" color="text.primary">
+            {readableBorrowLimit}
+          </Typography>
+        </div>
+      </div>
+
+      <ProgressBarHorizontal
+        css={styles.progressBar}
+        value={borrowLimitUsedPercentage || 0}
+        mark={safeBorrowLimitPercentage}
+        step={1}
+        ariaLabel={t('myAccount.progressBar.accessibilityLabel')}
+        min={0}
+        max={100}
+        trackTooltip={
+          readableBorrowBalance !== PLACEHOLDER_KEY &&
+          readableBorrowLimitUsedPercentage !== PLACEHOLDER_KEY && (
+            <Trans
+              i18nKey="myAccount.progressBar.borrowLimitTooltip"
+              components={{
+                LineBreak: <br />,
+              }}
+              values={{
+                borrowBalance: readableBorrowBalance,
+                borrowLimitUsedPercentage: readableBorrowLimitUsedPercentage,
+              }}
+            />
+          )
+        }
+        markTooltip={
+          readableSafeBorrowLimit !== PLACEHOLDER_KEY &&
+          borrowBalanceCents &&
+          borrowBalanceCents > 0 && (
+            <Trans
+              i18nKey="myAccount.progressBar.safeBorrowLimitTooltip"
+              components={{
+                LineBreak: <br />,
+              }}
+              values={{
+                safeBorrowLimit: readableSafeBorrowLimit,
+                safeBorrowLimitPercentage,
+              }}
+            />
+          )
+        }
+        isDisabled
+      />
+
+      <div css={styles.bottom}>
+        <Icon name="shield" css={styles.shieldIcon} />
+
+        <Typography component="span" variant="small2" css={styles.inlineLabel}>
+          {t('myAccount.safeLimit')}
+        </Typography>
+
+        <Typography component="span" variant="small1" color="text.primary" css={styles.safeLimit}>
+          {readableSafeBorrowLimit}
+        </Typography>
+
+        <Tooltip
+          css={styles.tooltip}
+          title={t('myAccount.safeLimitTooltip', { safeBorrowLimitPercentage })}
+        >
+          <Icon css={styles.infoIcon} name="info" />
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
+
+export default MyAccountUi;
--- src/pages/Dashboard/MyAccount/index.stories.tsx
+++ src/pages/Dashboard/MyAccount/index.stories.tsx
@@ -1,27 +1,56 @@
 import React from 'react';
 import { ComponentMeta } from '@storybook/react';
+
+import noop from 'noop-ts';
 import { withThemeProvider, withCenterStory } from 'stories/decorators';
-import { MyAccount } from '.';
+import MyAccountUi from './MyAccountUi';
 
 export default {
   title: 'Pages/Dashboard/MyAccount',
-  component: MyAccount,
+  component: MyAccountUi,
   decorators: [withThemeProvider, withCenterStory({ width: 650 })],
   parameters: {
     backgrounds: {
       default: 'Primary',
     },
   },
-} as ComponentMeta<typeof MyAccount>;
+} as ComponentMeta<typeof MyAccountUi>;
 
 export const Default = () => (
-  <MyAccount
-    netApyPercentage={2493}
-    dailyEarningsCents={14829}
+  <MyAccountUi
+    netApyPercentage={24.93}
+    dailyEarningsCents={1482}
     supplyBalanceCents={122889}
     borrowBalanceCents={30243}
-    borrowLimitCents={7373}
-    borrowLimitUsedPercentage={41}
-    safeLimitPercentage={41}
+    borrowLimitCents={73730}
+    safeBorrowLimitPercentage={65}
+    onXvsToggle={noop}
+    withXvs
+  />
+);
+
+export const WithZeroValues = () => (
+  <MyAccountUi
+    netApyPercentage={0}
+    dailyEarningsCents={0}
+    supplyBalanceCents={0}
+    borrowBalanceCents={0}
+    borrowLimitCents={0}
+    safeBorrowLimitPercentage={65}
+    onXvsToggle={noop}
+    withXvs={false}
+  />
+);
+
+export const WithoutUndefinedValues = () => (
+  <MyAccountUi
+    netApyPercentage={undefined}
+    dailyEarningsCents={undefined}
+    supplyBalanceCents={undefined}
+    borrowBalanceCents={undefined}
+    borrowLimitCents={undefined}
+    safeBorrowLimitPercentage={65}
+    onXvsToggle={noop}
+    withXvs={false}
   />
 );
