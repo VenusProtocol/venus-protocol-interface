dbc9fb61133bd5f6a8fdc9e3a46e1070d432102b
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-04-18 11:35:42 -0500

    Move flag to switch individual routes (#313)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppV1 from 'containers/App';
 import App from 'pages/App';
 // import * as serviceWorker from 'serviceWorker';
 
 import 'antd/dist/antd.css';
 import 'assets/styles/index.scss';
 
-ReactDOM.render(
-  process.env.REACT_APP_RUN_V2 ? <App /> : <AppV1 />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // serviceWorker.register();
