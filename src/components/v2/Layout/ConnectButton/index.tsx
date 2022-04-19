5da772f5a6e3e324fab0e6af122d67d1807ff57d
therealemjy <maxime.julian.creations@gmail.com>
2022-04-19 09:38:13 +0100

    remove title prop from ConnectButton

--- src/components/v2/Layout/ConnectButton/index.tsx
+++ src/components/v2/Layout/ConnectButton/index.tsx
@@ -1,24 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 
+import { useTranslation } from 'translation';
 import { AuthContext } from 'context/AuthContext';
 import { truncateAddress } from 'utilities/truncateAddress';
 import { SecondaryButton, IButtonProps } from '../../Button';
 
 export interface IConnectButton extends IButtonProps {
   accountAddress?: string;
-  title?: string;
 }
 
-export const ConnectButtonUi: React.FC<IConnectButton> = ({
-  accountAddress,
-  title = 'Connect',
-  ...otherProps
-}) => (
-  <SecondaryButton {...otherProps}>
-    {!accountAddress ? title : truncateAddress(accountAddress)}
-  </SecondaryButton>
-);
+export const ConnectButtonUi: React.FC<IConnectButton> = ({ accountAddress, ...otherProps }) => {
+  const { t } = useTranslation();
+
+  return (
+    <SecondaryButton {...otherProps}>
+      {!accountAddress ? t('connectButton.title') : truncateAddress(accountAddress)}
+    </SecondaryButton>
+  );
+};
 
 export const ConnectButton: React.FC<IButtonProps> = props => {
   const { account, openAuthModal } = React.useContext(AuthContext);
