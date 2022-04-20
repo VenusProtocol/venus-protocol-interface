ac1af595d29a74c3c333d29e6eb732a0accf7fbe
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-04-20 19:02:37 +0100

    Update header UI (#320)
    * remove duplicated ConnectButton
    
    * remove title prop from ConnectButton
    
    * add basic ClaimXvsRewardButton
    
    * minor styling fixes on Button
    
    * add ClaimXvsRewardButton to mobile menu
    
    * move ConnectButton to header on mobile
    
    * handle responsiveness of Header using hook
    
    * add TODOs
    
    * set ConnectButton variant based on auth state
    
    * remove CoinInfo component
    
    * fix issue with padding on connect button

---
+++ src/components/v2/Layout/ClaimXvsRewardButton/index.tsx
@@ -0,0 +1,60 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import BigNumber from 'bignumber.js';
+
+import { useTranslation } from 'translation';
+import { convertWeiToCoins } from 'utilities/common';
+import { Icon } from '../../Icon';
+import { SecondaryButton, IButtonProps } from '../../Button';
+import { useStyles } from './styles';
+
+const XVS_SYMBOL = 'xvs';
+
+export interface IClaimXvsRewardButton extends IButtonProps {
+  amountWei?: BigNumber;
+}
+
+export const ClaimXvsRewardButtonUi: React.FC<IClaimXvsRewardButton> = ({
+  amountWei,
+  ...otherProps
+}) => {
+  const { Trans } = useTranslation();
+  const styles = useStyles();
+
+  if (!amountWei) {
+    return null;
+  }
+
+  const readableAmount = convertWeiToCoins({
+    value: amountWei,
+    tokenSymbol: XVS_SYMBOL,
+    returnInReadableFormat: true,
+  });
+
+  return (
+    <SecondaryButton css={styles.button} {...otherProps}>
+      <Trans
+        i18nKey="claimXvsRewardButton.title"
+        components={{
+          Icon: <Icon css={styles.icon} name={XVS_SYMBOL} />,
+        }}
+        values={{
+          amount: readableAmount,
+        }}
+      />
+    </SecondaryButton>
+  );
+};
+
+export const ClaimXvsRewardButton: React.FC<IButtonProps> = props => {
+  const claimXvs = () => {
+    // @TODO: send transaction to claim XVS
+  };
+
+  // @TODO: fetch actual amount of claimable XVS wei
+  const fakeAmount = new BigNumber(100000000000000);
+
+  return <ClaimXvsRewardButtonUi amountWei={fakeAmount} onClick={claimXvs} {...props} />;
+};
+
+export default ClaimXvsRewardButton;
