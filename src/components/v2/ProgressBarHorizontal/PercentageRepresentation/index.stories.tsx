1ab2f4f28bca16d40bda1ea8307d1de74a8cb8c6
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2022-04-21 20:39:24 +0400

    Add PercentageRepresentation component (#329)

---
+++ src/components/v2/ProgressBarHorizontal/PercentageRepresentation/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { withCenterStory } from 'stories/decorators';
+import { PercentageRepresentation } from '.';
+
+export default {
+  title: 'Components/PercentageRepresentation',
+  component: PercentageRepresentation,
+  decorators: [withCenterStory({ width: 100 })],
+} as ComponentMeta<typeof PercentageRepresentation>;
+
+export const PercentageRepresentationDefault = () => <PercentageRepresentation value={30} />;
