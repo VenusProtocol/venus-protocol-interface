7eeb3c08d0dcdd97859bf99b9faf03f873822193
therealemjy <maxime.julian.creations@gmail.com>
2022-04-22 15:55:06 +0100

    add back convert icon + remove wallet util

--- src/components/v2/Icon/icons/index.tsx
+++ src/components/v2/Icon/icons/index.tsx
@@ -18,6 +18,7 @@ export { ReactComponent as discord } from './discord.svg';
 export { ReactComponent as twitter } from './twitter.svg';
 export { ReactComponent as github } from './github.svg';
 export { ReactComponent as venus } from './venus.svg';
+export { ReactComponent as convert } from './convert.svg';
 export { ReactComponent as close } from './close.svg';
 export { ReactComponent as open } from './open.svg';
 export { ReactComponent as copy } from './copy.svg';
