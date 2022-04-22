b9d4186f6707cebd87fa3d3473e76da3b55eddd0
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-04-22 12:36:07 +0100

    Claim XVS updates (#346)
    * use successful transaction modal in ClaimXvsRewardButton
    
    * fix how queries are invalidated after claiming XVS reward

---
+++ src/hooks/__mocks__/useSuccessfulTransactionModal.ts
@@ -0,0 +1,7 @@
+const openSuccessfulTransactionModal = jest.fn();
+const closeSuccessfulTransactionModal = jest.fn();
+
+export default () => ({
+  openSuccessfulTransactionModal,
+  closeSuccessfulTransactionModal,
+});
