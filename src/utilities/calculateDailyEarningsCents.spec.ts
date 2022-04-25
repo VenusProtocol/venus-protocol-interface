885bc0974652c0d36642beaa58544327a9c3b8ad
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-04-25 07:14:04 -0500

    Use daily earnings util in supply modal (#347)

--- src/utilities/calculateDailyEarningsCents.spec.ts
+++ src/utilities/calculateDailyEarningsCents.spec.ts
@@ -2,7 +2,9 @@ import BigNumber from 'bignumber.js';
 import calculateDailyEarningsCents from './calculateDailyEarningsCents';
 
 describe('utilities/calculateDailyEarningsCentss', () => {
-  test('calculates yearly Earnings for array of assets', () => {
-    expect(+calculateDailyEarningsCents(new BigNumber('1924.21991227022443813375'))).toBe(5);
+  test('calculates daily Earnings for a single asset', () => {
+    expect(calculateDailyEarningsCents(new BigNumber('1924.21991227022443813375')).toString()).toBe(
+      '5.27183537608280667982',
+    );
   });
 });
