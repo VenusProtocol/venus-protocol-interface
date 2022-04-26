1b61ed5d5ce3bc36c50a641c70205e99f2118062
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-04-26 21:17:46 +0100

    Create AccountHealth component (#365)
    * fix mark position on ProgressBarHorizontal component
    
    * rename ProgressBarHorizontal to ProgressBar
    
    * add basic AccountHealth component
    
    * add secondary variant
    
    * use AccountHealth on MyAccountUi
    
    * use AccountHealth on SupplyWithdrawForm
    
    * Add descriptive names
    
    * Properly nest story
    
    Co-authored-by: Corey Rice <coreyarice@gmail.com>

---
+++ src/components/v2/ProgressBar/AccountHealth/styles.ts
@@ -0,0 +1,21 @@
+import { useTheme } from '@mui/material';
+import { css } from '@emotion/react';
+
+export const useStyles = () => {
+  const theme = useTheme();
+
+  return {
+    topProgressBarLegend: css`
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: ${theme.spacing(3)};
+    `,
+    inlineContainer: css`
+      display: flex;
+    `,
+    inlineLabel: css`
+      margin-right: ${theme.spacing(1)};
+    `,
+  };
+};
