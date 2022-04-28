1c36e79810a9369b23f82e40cb699ebfe368387a
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-04-28 11:33:05 +0400

    chore: conflicts

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,106 @@
 /** @jsxImportSource @emotion/react */
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import BigNumber from 'bignumber.js';
 import { AuthContext } from 'context/AuthContext';
 import { useUserMarketInfo } from 'clients/api';
 import { Asset } from 'types';
+
+import { useIsSmDown, useIsXlDown } from 'hooks/responsive';
+import { Tabs } from 'components';
+import { useTranslation } from 'translation';
 import MyAccount from './MyAccount';
+import MintRepayVai from './MintRepayVai';
 import { SupplyMarket, BorrowMarket } from './Markets';
 import { useStyles } from './styles';
+
+interface IDashboardMarketsUiProps {
+  isXvsEnabled: boolean;
+  accountAddress: string;
+  userTotalBorrowLimit: BigNumber;
+  suppliedAssets: Asset[];
+  supplyMarketAssets: Asset[];
+  borrowingAssets: Asset[];
+  borrowMarketAssets: Asset[];
+}
+
+const DashboardMarketsUi: React.FC<IDashboardMarketsUiProps> = ({
+  isXvsEnabled,
+  accountAddress,
+  userTotalBorrowLimit,
+  suppliedAssets,
+  supplyMarketAssets,
+  borrowingAssets,
+  borrowMarketAssets,
+}) => {
+  const { t } = useTranslation();
+  const isXlDown = useIsXlDown();
+  const isSmDown = useIsSmDown();
+  const styles = useStyles();
+
+  const [activeTab, setActiveTab] = useState(0);
+
+  if (isXlDown) {
+    const tabsContent = [
+      {
+        name: t('markets.supplyMarketTableTitle'),
+        title: t('dashboard.markets.tabSupply'),
+        content: (
+          <SupplyMarket
+            isXvsEnabled={isXvsEnabled}
+            suppliedAssets={suppliedAssets}
+            supplyMarketAssets={supplyMarketAssets}
+            accountAddress={accountAddress}
+            css={styles.item}
+          />
+        ),
+      },
+      {
+        name: t('markets.borrowMarketTableTitle'),
+        title: t('dashboard.markets.tabBorrow'),
+        content: (
+          <BorrowMarket
+            isXvsEnabled={isXvsEnabled}
+            borrowingAssets={borrowingAssets}
+            borrowMarketAssets={borrowMarketAssets}
+            userTotalBorrowLimit={userTotalBorrowLimit}
+          />
+        ),
+      },
+    ];
+    const tabletTitle = isSmDown ? undefined : tabsContent[activeTab].name;
+    return (
+      <div css={styles.tabsWrapper}>
+        {isSmDown && (
+          <h4 css={[styles.tabsHeader, styles.tabsTitle]}>{t('dashboard.markets.title')}</h4>
+        )}
+        <Tabs
+          css={styles.tabsHeader}
+          componentTitle={tabletTitle}
+          tabsContent={tabsContent}
+          onTabChange={setActiveTab}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div css={styles.container}>
+      <SupplyMarket
+        isXvsEnabled={isXvsEnabled}
+        suppliedAssets={suppliedAssets}
+        supplyMarketAssets={supplyMarketAssets}
+        accountAddress={accountAddress}
+        css={styles.item}
+      />
+      <BorrowMarket
+        isXvsEnabled={isXvsEnabled}
+        borrowingAssets={borrowingAssets}
+        borrowMarketAssets={borrowMarketAssets}
+        userTotalBorrowLimit={userTotalBorrowLimit}
+      />
+    </div>
+  );
+};
 
 interface IDashboardUiProps {
   accountAddress: string;
@@ -49,30 +143,30 @@ const DashboardUi: React.FC<IDashboardUiProps> = ({
       );
       return sortedAssets;
     }, [JSON.stringify(assets)]);
+
   return (
-    <div>
-      <MyAccount
-        assets={assets}
-        setIsXvsEnabled={setIsXvsEnabled}
-        isXvsEnabled={isXvsEnabled}
-        userTotalBorrowBalance={userTotalBorrowBalance}
-        userTotalBorrowLimit={userTotalBorrowLimit}
-      />
+    <>
       <div css={styles.container}>
-        <SupplyMarket
+        <MyAccount
+          assets={assets}
+          setIsXvsEnabled={setIsXvsEnabled}
           isXvsEnabled={isXvsEnabled}
-          suppliedAssets={suppliedAssets}
-          supplyMarketAssets={supplyMarketAssets}
-          accountAddress={accountAddress}
-        />
-        <BorrowMarket
-          isXvsEnabled={isXvsEnabled}
-          borrowingAssets={borrowingAssets}
-          borrowMarketAssets={borrowMarketAssets}
+          css={styles.item}
+          userTotalBorrowBalance={userTotalBorrowBalance}
           userTotalBorrowLimit={userTotalBorrowLimit}
         />
+        <MintRepayVai css={styles.item} />
       </div>
-    </div>
+      <DashboardMarketsUi
+        isXvsEnabled={isXvsEnabled}
+        accountAddress={accountAddress}
+        userTotalBorrowLimit={userTotalBorrowLimit}
+        suppliedAssets={suppliedAssets}
+        supplyMarketAssets={supplyMarketAssets}
+        borrowingAssets={borrowingAssets}
+        borrowMarketAssets={borrowMarketAssets}
+      />
+    </>
   );
 };
 
