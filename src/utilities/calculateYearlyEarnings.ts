4d482649f12390efd3b10b97ff8e7a8605d51985
Corey Rice <coreyarice@gmail.com>
2022-04-27 21:47:43 -0500

    Update calculateYearlyEarningsCents

--- src/utilities/calculateYearlyEarnings.ts
+++ src/utilities/calculateYearlyEarnings.ts
@@ -12,48 +12,41 @@ import BigNumber from 'bignumber.js';
 export const calculateYearlyEarningsCents = ({
   asset,
   isXvsEnabled,
-  borrowBalanceCents,
-  supplyBalanceCents = new BigNumber(0),
   yearlyEarningsCents = new BigNumber(0),
 }: {
   asset: Asset;
-  borrowBalanceCents: BigNumber;
   isXvsEnabled: boolean;
-  supplyBalanceCents?: BigNumber;
   yearlyEarningsCents?: BigNumber;
 }) => {
-  let assetSupplyBalanceCents = supplyBalanceCents;
-  let assetYearlyEarningsCents = yearlyEarningsCents;
+  const assetBorrowBalanceCents = asset.borrowBalance
+    .multipliedBy(asset.tokenPrice)
+    .multipliedBy(100);
+  const assetSupplyBalanceCents = asset.supplyBalance
+    .multipliedBy(asset.tokenPrice)
+    .multipliedBy(100);
 
-  assetSupplyBalanceCents = assetSupplyBalanceCents.plus(
-    asset.supplyBalance.multipliedBy(asset.tokenPrice).multipliedBy(100),
-  );
-
-  const supplyYearlyEarnings = assetSupplyBalanceCents.multipliedBy(asset.supplyApy).dividedBy(100);
+  const supplyYearlyEarningsCents = assetSupplyBalanceCents.multipliedBy(asset.supplyApy);
   // Note that borrowYearlyInterests will always be negative (or 0), since
   // the borrow APY is expressed with a negative percentage)
-  const borrowYearlyInterests = borrowBalanceCents.multipliedBy(asset.borrowApy).dividedBy(100);
+  const borrowYearlyInterestsCents = assetBorrowBalanceCents.multipliedBy(asset.borrowApy);
 
-  assetYearlyEarningsCents = assetYearlyEarningsCents.plus(
-    supplyYearlyEarnings.plus(borrowYearlyInterests),
+  let totalYearlyEarningsCents = yearlyEarningsCents.plus(
+    supplyYearlyEarningsCents.plus(borrowYearlyInterestsCents),
   );
   // Add XVS distribution earnings if enabled
   if (isXvsEnabled) {
-    const supplyDistributionYearlyEarnings = assetSupplyBalanceCents
-      .multipliedBy(asset.xvsSupplyApy)
-      .dividedBy(100);
-    const borrowDistributionYearlyEarnings = borrowBalanceCents
-      .multipliedBy(asset.xvsBorrowApy)
-      .dividedBy(100);
+    const supplyDistributionYearlyEarnings = assetSupplyBalanceCents.multipliedBy(
+      asset.xvsSupplyApy,
+    );
+    const borrowDistributionYearlyEarnings = assetBorrowBalanceCents.multipliedBy(
+      asset.xvsBorrowApy,
+    );
 
-    assetYearlyEarningsCents = assetYearlyEarningsCents
+    totalYearlyEarningsCents = totalYearlyEarningsCents
       .plus(supplyDistributionYearlyEarnings)
       .plus(borrowDistributionYearlyEarnings);
   }
-  return {
-    yearlyEarningsCents: assetYearlyEarningsCents,
-    supplyBalanceCents: assetSupplyBalanceCents,
-  };
+  return totalYearlyEarningsCents;
 };
 
 /**
@@ -66,37 +59,23 @@ export const calculateYearlyEarningsCents = ({
  */
 export const calculateYearlyEarningsForAssets = ({
   assets,
-  borrowBalanceCents,
   isXvsEnabled,
 }: {
   assets: Asset[];
-  borrowBalanceCents: BigNumber;
   isXvsEnabled: boolean;
 }) => {
-  let supplyBalanceCents: BigNumber | undefined;
-
   // We use the yearly earnings to calculate the daily earnings the net APY
   let yearlyEarningsCents: BigNumber | undefined;
   assets.forEach(asset => {
-    if (!supplyBalanceCents) {
-      supplyBalanceCents = new BigNumber(0);
-    }
-
     if (!yearlyEarningsCents) {
       yearlyEarningsCents = new BigNumber(0);
     }
-    const {
-      yearlyEarningsCents: assetYearlyEarningsCents,
-      supplyBalanceCents: assetSupplyBalanceCents,
-    } = calculateYearlyEarningsCents({
+    const assetYearlyEarningsCents = calculateYearlyEarningsCents({
       asset,
       isXvsEnabled,
-      borrowBalanceCents,
-      supplyBalanceCents,
       yearlyEarningsCents,
     });
     yearlyEarningsCents = assetYearlyEarningsCents;
-    supplyBalanceCents = assetSupplyBalanceCents;
   });
-  return { yearlyEarningsCents, supplyBalanceCents };
+  return yearlyEarningsCents;
 };
