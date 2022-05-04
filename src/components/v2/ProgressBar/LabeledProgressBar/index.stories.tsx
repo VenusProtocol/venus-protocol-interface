36a02c79adce4eced4ec673c1ebae8a03218c14c
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-05-04 02:23:21 -0500

    Add labeled progressbar (#399)

---
+++ src/components/v2/ProgressBar/LabeledProgressBar/index.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { withCenterStory } from 'stories/decorators';
+import { LabeledProgressBar } from '.';
+
+export default {
+  title: 'Components/ProgressBar/LabeledProgressBar',
+  component: LabeledProgressBar,
+  decorators: [withCenterStory({ width: 600 })],
+} as ComponentMeta<typeof LabeledProgressBar>;
+
+export const BorrowBalanceAccountHealthWithValues = () => (
+  <LabeledProgressBar
+    greyLeftText="Max:"
+    whiteLeftText="$5000"
+    greyRightText="Minimum:"
+    whiteRightText="$100"
+    value={50}
+    step={5}
+    mark={75}
+    ariaLabel="Storybook slider"
+    min={0}
+    max={100}
+  />
+);
