e2015f84dd1c2a7c70a36469acce2ae5830b825b
therealemjy <maxime.julian.creations@gmail.com>
2022-05-06 11:23:17 +0100

    fix max input on withdraw form

--- src/pages/Dashboard/Modals/SupplyWithdraw/SupplyWithdrawForm.tsx
+++ src/pages/Dashboard/Modals/SupplyWithdraw/SupplyWithdrawForm.tsx
@@ -29,12 +29,13 @@ interface ISupplyWithdrawFormUiProps {
   asset: Asset;
   assets: Asset[];
   tokenInfo: ILabeledInlineContentProps[];
+  availableBalance: BigNumber;
+  maxInput: BigNumber;
   userTotalBorrowBalance: BigNumber;
   userTotalBorrowLimit: BigNumber;
   inputLabel: string;
   enabledButtonKey: string;
   disabledButtonKey: string;
-  maxInput: BigNumber;
   calculateNewBalance: (initial: BigNumber, amount: BigNumber) => BigNumber;
   isTransactionLoading: boolean;
   isXvsEnabled: boolean;
@@ -46,6 +47,7 @@ export const SupplyWithdrawContent: React.FC<ISupplyWithdrawFormUiProps> = ({
   userTotalBorrowBalance,
   userTotalBorrowLimit,
   assets,
+  availableBalance,
   maxInput,
   inputLabel,
   enabledButtonKey,
@@ -132,7 +134,7 @@ export const SupplyWithdrawContent: React.FC<ISupplyWithdrawFormUiProps> = ({
           components={{
             White: <span css={styles.whiteLabel} />,
           }}
-          values={{ amount: format(maxInput), symbol: assetId?.toUpperCase() }}
+          values={{ amount: format(availableBalance), symbol: assetId?.toUpperCase() }}
         />
       </Typography>
 
@@ -199,6 +201,7 @@ interface ISupplyWithdrawFormProps extends ISupplyWithdrawFormUiProps {
 const SupplyWithdrawForm: React.FC<ISupplyWithdrawFormProps> = ({
   onSubmit,
   maxInput,
+  availableBalance,
   ...props
 }) => {
   const onSubmitHandleError: IAmountFormProps['onSubmit'] = async (value: string) => {
@@ -210,7 +213,9 @@ const SupplyWithdrawForm: React.FC<ISupplyWithdrawFormProps> = ({
   };
   return (
     <AmountForm onSubmit={onSubmitHandleError} maxAmount={maxInput.toFixed()}>
-      {() => <SupplyWithdrawContent maxInput={maxInput} {...props} />}
+      {() => (
+        <SupplyWithdrawContent maxInput={maxInput} availableBalance={availableBalance} {...props} />
+      )}
     </AmountForm>
   );
 };
