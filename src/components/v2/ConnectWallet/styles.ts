17d802f6a340bd4a9ca0a9d832f4788729654aef
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2022-05-06 17:51:23 +0400

    Chore: use notice component for Prompt message (#423)

--- src/components/v2/ConnectWallet/styles.ts
+++ src/components/v2/ConnectWallet/styles.ts
@@ -5,23 +5,9 @@ export const useStyles = () => {
   const theme = useTheme();
 
   return {
-    prompt: css`
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-radius: ${theme.shape.borderRadius.medium}px;
-      background-color: ${theme.palette.background.default};
-      padding: ${theme.spacing(8, 4)};
+    notice: css`
       margin-bottom: ${theme.spacing(10)};
-    `,
-    icon: css`
-      color: ${theme.palette.interactive.success};
-      width: ${theme.shape.iconSize.large}px;
-      height: ${theme.shape.iconSize.large}px;
-      margin-right: ${theme.spacing(2)};
-    `,
-    message: css`
-      color: ${theme.palette.text.primary};
+      margin-top: ${theme.spacing(2)};
     `,
   };
 };
