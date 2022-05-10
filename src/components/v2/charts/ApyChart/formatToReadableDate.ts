98632426aedadd0894c1e9cd87c9dff86c193643
therealemjy <maxime.julian.creations@gmail.com>
2022-05-10 16:38:08 +0100

    move formatToReadableDate out of utils + add missing translation

--- src/components/v2/charts/ApyChart/formatToReadableDate.ts
+++ src/components/v2/charts/ApyChart/formatToReadableDate.ts
@@ -1,6 +1,6 @@
 import formatDate from 'date-fns/format';
 
-const READABLE_DATE_FORMAT = 'MM.dd';
+const READABLE_DATE_FORMAT = 'MM.DD';
 
 const formatToReadableDate = (timestampMs: number) =>
   formatDate(new Date(timestampMs), READABLE_DATE_FORMAT);
