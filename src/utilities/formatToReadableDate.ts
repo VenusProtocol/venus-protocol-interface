11c67f31a1d84408587322a6661b6f3f48cd7af1
therealemjy <maxime.julian.creations@gmail.com>
2022-05-10 10:02:10 +0100

    style axis

--- src/utilities/formatToReadableDate.ts
+++ src/utilities/formatToReadableDate.ts
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-const READABLE_DATE_FORMAT = 'MM/DD';
+const READABLE_DATE_FORMAT = 'MM.DD';
 
 const formatToReadableDate = (timestamp: Date) => moment(timestamp).format(READABLE_DATE_FORMAT);
 export default formatToReadableDate;
