8df282858c6046b82da237d93403538d56084d22
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-05-10 19:34:31 +0100

    Improve dashboard tables responsiveness (#452)
    * fix dashboard tables responsiveness on tablet
    
    * use correct hook on table
    
    * fix responsiveness issues on tables

--- src/pages/Dashboard/Markets/BorrowMarket/BorrowMarketTable.tsx
+++ src/pages/Dashboard/Markets/BorrowMarket/BorrowMarketTable.tsx
@@ -89,7 +89,7 @@ const BorrowMarketTable: React.FC<IBorrowMarketTableProps> = ({
       }}
       rowKeyIndex={0}
       rowOnClick={rowOnClick}
-      gridTemplateColumns={styles.getGridTemplateColumns({ isCardLayout: isLgDown })}
+      gridTemplateColumns={styles.getGridTemplateColumns({ isCardLayout: isSmDown })}
     />
   );
 };
