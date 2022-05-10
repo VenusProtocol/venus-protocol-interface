e2107d22d5b95c11380d04b7201801af677efa5f
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-05-10 08:30:32 +0100

    Update supply form tests (#436)
    * add test for wallet balance
    
    * add test for supply balance
    
    * add test on input max
    
    * add test on supply non-BNB tokens
    
    * add test on supply BNB tokens
    
    * fix existing tests
    
    * rename amount param of mutations to amountWei
    
    * fix linting issue

--- src/clients/api/mutations/redeem.ts
+++ src/clients/api/mutations/redeem.ts
@@ -1,15 +1,17 @@
+import BigNumber from 'bignumber.js';
 import type { TransactionReceipt } from 'web3-core';
+
 import { VBep20 } from 'types/contracts';
 
 export interface IRedeemInput {
   tokenContract: VBep20;
   account: string;
-  amount: string;
+  amountWei: BigNumber;
 }
 
 export type RedeemOutput = TransactionReceipt;
 
-const redeem = async ({ tokenContract, account, amount }: IRedeemInput): Promise<RedeemOutput> =>
-  tokenContract.methods.redeem(amount).send({ from: account });
+const redeem = async ({ tokenContract, account, amountWei }: IRedeemInput): Promise<RedeemOutput> =>
+  tokenContract.methods.redeem(amountWei.toFixed()).send({ from: account });
 
 export default redeem;
--- src/clients/api/mutations/redeemUnderlying.ts
+++ src/clients/api/mutations/redeemUnderlying.ts
@@ -1,10 +1,12 @@
+import BigNumber from 'bignumber.js';
 import type { TransactionReceipt } from 'web3-core';
+
 import { VBep20, VBnbToken } from 'types/contracts';
 
 export interface IRedeemUnderlyingInput {
   tokenContract: VBep20 | VBnbToken;
   account: string;
-  amount: string;
+  amountWei: BigNumber;
 }
 
 export type RedeemUnderlyingOutput = TransactionReceipt;
@@ -12,8 +14,8 @@ export type RedeemUnderlyingOutput = TransactionReceipt;
 const redeemUnderlying = async ({
   tokenContract,
   account,
-  amount,
+  amountWei,
 }: IRedeemUnderlyingInput): Promise<RedeemUnderlyingOutput> =>
-  tokenContract.methods.redeemUnderlying(amount).send({ from: account });
+  tokenContract.methods.redeemUnderlying(amountWei.toFixed()).send({ from: account });
 
 export default redeemUnderlying;
