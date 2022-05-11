d1fc47b4e3d9ba96e4d937bf6d4bbb11d559ed82
therealemjy <maxime.julian.creations@gmail.com>
2022-05-11 14:45:40 +0100

    add empty MarketDetails page

--- src/pages/MarketDetails/index.tsx
+++ src/pages/MarketDetails/index.tsx
@@ -1,21 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 
-import MarketInfo from './MarketInfo';
-import { useStyles } from './styles';
-
-const MarketDetails: React.FC = () => {
-  const styles = useStyles();
-
-  return (
-    <div css={styles.container}>
-      <div css={[styles.column, styles.graphsColumn]}>Graphs here</div>
-
-      <div css={[styles.column, styles.statsColumn]}>
-        <MarketInfo />
-      </div>
-    </div>
-  );
-};
+const MarketDetails: React.FC = () => null;
 
 export default MarketDetails;
