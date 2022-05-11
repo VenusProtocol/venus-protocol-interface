8ee80eda2f7d068341df69e71c95c23add3bd5de
therealemjy <maxime.julian.creations@gmail.com>
2022-05-11 14:56:35 +0100

    add basic page DOM

--- src/pages/MarketDetails/index.tsx
+++ src/pages/MarketDetails/index.tsx
@@ -1,6 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 
-const MarketDetails: React.FC = () => null;
+import { useStyles } from './styles';
+
+const MarketDetails: React.FC = () => {
+  const styles = useStyles();
+
+  return (
+    <div css={styles.container}>
+      <div css={[styles.column, styles.content]}>Graphs here</div>
+
+      <div css={[styles.column, styles.sideBar]}>Market detials</div>
+    </div>
+  );
+};
 
 export default MarketDetails;
