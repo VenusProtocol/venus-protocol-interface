284070f3e698f969e79b1737d446f6e730de0957
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-05-16 20:09:00 +0400

    Use notice component for banner message

--- src/components/v2/Layout/index.tsx
+++ src/components/v2/Layout/index.tsx
@@ -1,11 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { Icon } from '../Icon';
 import Sidebar from './Sidebar';
 import Header from './Header';
 import { PageContainer } from './PageContainer';
+import { NoticeWarning } from '../Notice';
 import { useStyles } from './styles';
 
 export interface ILayoutProps {
@@ -18,13 +17,10 @@ export const Layout: React.FC<ILayoutProps> = ({ children }) => {
     <div css={styles.layout}>
       <Sidebar />
       <Box display="flex" flexDirection="column" flex="1">
-        <div css={styles.ustWarning}>
-          <Typography component="p" variant="small1">
-            <Icon name="attention" />
-            Venus uses Wormhole UST. Many CEXs currently only recognize Wrapped UST. Please convert
-            your UST as needed.
-          </Typography>
-        </div>
+        <NoticeWarning
+          css={styles.banner}
+          description="Venus uses Wormhole UST. Many CEXs currently only recognize Wrapped UST. Please convert your UST as needed."
+        />
         <Header />
         <PageContainer>{children}</PageContainer>
       </Box>
--- src/components/v2/Layout/styles.ts
+++ src/components/v2/Layout/styles.ts
@@ -13,30 +13,16 @@ export const useStyles = () => {
         flex-direction: column;
       }
     `,
-    ustWarning: css`
-      background-color: ${theme.palette.interactive.tan};
-      width: 100%;
-      padding: ${theme.spacing(1)} ${theme.spacing(4)};
-      justify-content: center;
-      align-items: center;
-      display: inline-flex;
-      min-height: 56px;
-
-      p {
-        color: rgba(0, 0, 0, 1);
-        display: flex;
-        text-align: center;
+    banner: css`
+      margin: ${theme.spacing(4, 12)};
+      ${theme.breakpoints.down('xl')} {
+        margin: ${theme.spacing(4, 10)};
       }
-
-      svg {
-        display: flex;
-        margin-right: ${theme.spacing(2)};
-        align-self: center;
-        ${theme.breakpoints.down('md')} {
-          height: ${theme.spacing(6)};
-          width: ${theme.spacing(6)};
-          margin-right: 0;
-        }
+      ${theme.breakpoints.down('lg')} {
+        margin: ${theme.spacing(6)};
+      }
+      ${theme.breakpoints.down('md')} {
+        margin: ${theme.spacing(4)};
       }
     `,
   };
