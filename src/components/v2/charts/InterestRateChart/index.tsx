332f1f1bafd3ce2eca4490979ee3207255d182ae
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-05-16 15:54:50 +0100

    Create InterestRateChart component (#462)
    * add basic AreaChart component
    
    * progress on chart
    
    * get colors from theme
    
    * style cursor
    
    * shape data
    
    * WIP
    
    * style tooltip
    
    * add types
    
    * house keeping
    
    * style axis
    
    * fix positioning of chart
    
    * make ApyChart responsive
    
    * move formatToReadableDate out of utils + add missing translation
    
    * use date-fns instead of moment
    
    * use null instead of fragment
    
    * remove duplicated className
    
    * rename apy to apyPercentage
    
    * add basic InterestRateChart
    
    * move charts inside common folder
    
    * create shared styles
    
    * add X axis + update ApyChart to improve its scale
    
    * make TooltipContent component
    
    * rename Charts folder to charts
    
    * add vertical reference line
    
    * decouple shared and local styles

---
+++ src/components/v2/charts/InterestRateChart/index.tsx
@@ -0,0 +1,131 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import {
+  ResponsiveContainer,
+  YAxis,
+  XAxis,
+  CartesianGrid,
+  LineChart,
+  Line,
+  ReferenceLine,
+  Tooltip,
+} from 'recharts';
+
+import { useTranslation } from 'translation';
+import { formatToReadablePercentage } from 'utilities/common';
+import TooltipContent from '../TooltipContent';
+import { useStyles as useSharedStyles } from '../styles';
+import { useStyles as useLocalStyles } from './styles';
+
+export interface IInterestRateItem {
+  utilizationRate: number;
+  borrowApyPercentage: number;
+  supplyApyPercentage: number;
+}
+
+export interface IInterestRateChartProps {
+  data: IInterestRateItem[];
+  currentUtilizationRate: number;
+  className?: string;
+}
+
+export const InterestRateChart: React.FC<IInterestRateChartProps> = ({
+  className,
+  currentUtilizationRate,
+  data,
+}) => {
+  const sharedStyles = useSharedStyles();
+  const localStyles = useLocalStyles();
+
+  const { t } = useTranslation();
+
+  return (
+    <div css={sharedStyles.container} className={className}>
+      <ResponsiveContainer>
+        <LineChart data={data} margin={sharedStyles.chartMargin}>
+          <XAxis
+            dataKey="utilizationRate"
+            axisLine={false}
+            tickLine={false}
+            tickFormatter={formatToReadablePercentage}
+            stroke={sharedStyles.accessoryColor}
+            tickMargin={sharedStyles.tickMargin}
+            tickCount={5}
+            type="number"
+            style={sharedStyles.axis}
+          />
+          <YAxis
+            axisLine={false}
+            tickLine={false}
+            tickFormatter={formatToReadablePercentage}
+            tickMargin={sharedStyles.tickMargin}
+            stroke={sharedStyles.accessoryColor}
+            style={sharedStyles.axis}
+            tickCount={10}
+          />
+          <Tooltip
+            isAnimationActive={false}
+            cursor={sharedStyles.cursor}
+            content={({ payload }) =>
+              payload && payload[0] ? (
+                <TooltipContent
+                  items={[
+                    {
+                      label: t('interestRateChart.tooltipItemLabels.utilizationRate'),
+                      value: formatToReadablePercentage(
+                        (payload[0].payload as IInterestRateItem).utilizationRate,
+                      ),
+                    },
+                    {
+                      label: t('interestRateChart.tooltipItemLabels.borrowApy'),
+                      value: formatToReadablePercentage(
+                        (payload[0].payload as IInterestRateItem).borrowApyPercentage,
+                      ),
+                    },
+                    {
+                      label: t('interestRateChart.tooltipItemLabels.supplyApy'),
+                      value: formatToReadablePercentage(
+                        (payload[0].payload as IInterestRateItem).supplyApyPercentage,
+                      ),
+                    },
+                  ]}
+                />
+              ) : null
+            }
+          />
+          <CartesianGrid vertical={false} stroke={sharedStyles.gridLineColor} />
+          <Line
+            type="monotone"
+            dataKey="borrowApyPercentage"
+            stroke={localStyles.lineBorrowApyColor}
+            strokeWidth={sharedStyles.lineStrokeWidth}
+            isAnimationActive={false}
+            activeDot={localStyles.lineActiveDot}
+            dot={false}
+          />
+          <Line
+            type="monotone"
+            dataKey="supplyApyPercentage"
+            stroke={localStyles.lineSupplyApyColor}
+            strokeWidth={sharedStyles.lineStrokeWidth}
+            activeDot={localStyles.lineActiveDot}
+            isAnimationActive={false}
+            dot={false}
+          />
+          <ReferenceLine
+            x={currentUtilizationRate}
+            stroke={localStyles.referenceLineColor}
+            // Note: we can not use the spread operator to extend
+            // styles.referenceLineLabel because its type is not accepted for
+            // that
+            label={Object.assign(localStyles.referenceLineLabel || {}, {
+              value: t('interestRateChart.currentUtilizationRateLabelValue', {
+                percentage: formatToReadablePercentage(currentUtilizationRate),
+              }),
+            })}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
---
+++ src/components/v2/charts/InterestRateChart/styles.ts
@@ -0,0 +1,26 @@
+import { css } from '@emotion/react';
+import { useTheme } from '@mui/material';
+import { ReferenceLine } from 'recharts';
+
+import { SPACING } from 'theme/MuiThemeProvider/muiTheme';
+
+export const useStyles = () => {
+  const theme = useTheme();
+
+  return {
+    lineBorrowApyColor: theme.palette.interactive.error,
+    lineSupplyApyColor: theme.palette.interactive.success,
+    lineActiveDot: { r: SPACING * 1.5, strokeWidth: 0 },
+    container: css`
+      width: 100%;
+      height: ${theme.spacing(95)};
+    `,
+    referenceLineColor: theme.palette.interactive.primary,
+    referenceLineLabel: {
+      position: 'top',
+      fill: theme.palette.text.primary,
+      fontSize: theme.typography.small1.fontSize,
+      fontWeight: theme.typography.small1.fontWeight,
+    } as React.ComponentProps<typeof ReferenceLine>['label'],
+  };
+};
