d9c1efa086d15c94992af31bbe24145b037d6700
Corey Rice <coreyarice@gmail.com>
2022-05-18 14:36:16 -0300

    Dynamically set id

--- src/components/v2/Icon/index.tsx
+++ src/components/v2/Icon/index.tsx
@@ -1,5 +1,6 @@
+import React, { useRef } from 'react';
 import { useTheme } from '@mui/material';
-import React from 'react';
+import { uniqueId } from 'lodash';
 import * as icons from './icons';
 
 export type IconName = keyof typeof icons;
@@ -14,6 +15,11 @@ export interface IIconProps {
 
 export const Icon: React.FC<IIconProps> = ({ name, size, color, ...otherProps }) => {
   const theme = useTheme();
+  const idRef = useRef<string>();
+
+  if (idRef.current === undefined) {
+    idRef.current = uniqueId();
+  }
   const sanitizedSize = size ?? theme.shape.iconSize.medium;
   const sanitizedColor = color ?? theme.palette.text.secondary;
   // Because "name" could come from fetched data, we use a default icon in case
@@ -25,6 +31,7 @@ export const Icon: React.FC<IIconProps> = ({ name, size, color, ...otherProps })
       width={sanitizedSize}
       height={sanitizedSize}
       color={sanitizedColor}
+      id={idRef.current}
       {...otherProps}
     />
   );
