6c8d3287c5f5a48b2e2c0bfa26b7339a8dd816e5
therealemjy <maxime.julian.creations@gmail.com>
2022-05-18 15:12:01 +0100

    handle MarketDetails page edge-case on Header

--- src/components/v2/Layout/Header/index.tsx
+++ src/components/v2/Layout/Header/index.tsx
@@ -1,13 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import React, { useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 
+import Path from 'constants/path';
+import { getToken } from 'utilities';
+import { VTokenId } from 'types';
 import { useTranslation } from 'translation';
 import { useIsMdDown } from 'hooks/responsive';
 import { Toolbar } from '../Toolbar';
 import ClaimXvsRewardButton from '../ClaimXvsRewardButton';
 import ConnectButton from '../ConnectButton';
+import { Icon } from '../../Icon';
+import isOnMarketDetailsPage from './isOnMarketDetailsPage';
 import { menuItems } from '../constants';
 import { useStyles } from './styles';
 
@@ -19,19 +24,35 @@ const Header = () => {
   const isOnMobile = useIsMdDown();
 
   const title = useMemo(() => {
+    // Handle special case of Market Details page
+    if (isOnMarketDetailsPage(pathname)) {
+      const vTokenId = pathname.substring(pathname.lastIndexOf('/') + 1) as VTokenId;
+      const token = getToken(vTokenId);
+
+      return (
+        <Link to={Path.MARKET} css={styles.backButton}>
+          <Icon name="chevronLeft" css={styles.backButtonChevronIcon} />
+          <Icon name={vTokenId} css={styles.backButtonTokenIcon} />
+          <h3 css={styles.backButtonTokenSymbol}>{token.symbol}</h3>
+        </Link>
+      );
+    }
+
     const currentItem = menuItems.find(item => item.href === pathname);
     const currentItemKey = currentItem?.i18nTitleKey || currentItem?.i18nKey;
+
     if (!currentItemKey) {
-      console.error(`missed key for translation: ${pathname}`);
+      console.error(`Missing translation for key: ${pathname}`);
       return null;
     }
-    return t(currentItemKey);
+
+    return <h3>{t(currentItemKey)}</h3>;
   }, [pathname]);
 
   return (
     <AppBar position="relative" css={styles.appBar}>
       <Toolbar css={styles.toolbar}>
-        {title && <h3>{title}</h3>}
+        {title}
 
         {!isOnMobile && (
           <div css={styles.ctaContainer}>
--- src/components/v2/Layout/Header/styles.ts
+++ src/components/v2/Layout/Header/styles.ts
@@ -25,6 +25,24 @@ export const useStyles = () => {
         padding: ${theme.spacing(6, 4, 0)} !important;
       }
     `,
+    backButton: css`
+      display: flex;
+      align-items: center;
+    `,
+    backButtonChevronIcon: css`
+      width: ${theme.spacing(6)};
+      height: ${theme.spacing(6)};
+      color: ${theme.palette.text.primary};
+      margin-right: ${theme.spacing(2)};
+    `,
+    backButtonTokenIcon: css`
+      width: ${theme.spacing(8)};
+      height: ${theme.spacing(8)};
+      margin-right: ${theme.spacing(2)};
+    `,
+    backButtonTokenSymbol: css`
+      color: ${theme.palette.text.primary};
+    `,
     claimXvsButton: css`
       margin-right: ${theme.spacing(6)};
     `,
