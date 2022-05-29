d67f0b366af972065f9cdc936af07d92d6f13d67
Dmitriy Lisyanskiy <baomastr@users.noreply.github.com>
2022-05-29 15:15:56 +0400

    Feature: Add pagination (#549)
    * Feature: Add pagination with data handling
    
    * remove data handling from usePagination hook

---
+++ src/components/v2/Pagination/index.stories.tsx
@@ -0,0 +1,38 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import noop from 'noop-ts';
+import { ComponentMeta, Story } from '@storybook/react';
+import { withThemeProvider, withCenterStory } from 'stories/decorators';
+import { PALETTE } from 'theme/MuiThemeProvider/muiTheme';
+import { Pagination } from '.';
+
+export default {
+  title: 'Components/Pagination',
+  component: Pagination,
+  decorators: [withThemeProvider, withCenterStory({ width: 800 })],
+  parameters: {
+    backgrounds: {
+      default: PALETTE.background.default,
+    },
+  },
+} as ComponentMeta<typeof Pagination>;
+
+const PaginationTemplate: Story<{
+  itemsCount: number;
+  initialPageNumber?: number;
+  itemsPerPageCount?: number;
+}> = ({ initialPageNumber, itemsPerPageCount }) => (
+  <Pagination
+    itemsCount={25}
+    onChange={noop}
+    initialPageNumber={initialPageNumber}
+    itemsPerPageCount={itemsPerPageCount}
+  />
+);
+
+export const PaginationWithCustomContentPerPage = PaginationTemplate.bind({});
+PaginationWithCustomContentPerPage.args = {
+  itemsCount: 25,
+  initialPageNumber: 1,
+  itemsPerPageCount: 4,
+};
---
+++ src/components/v2/Pagination/usePagination.ts
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'translation';
+
+type IPaginationProps = {
+  itemsCount: number;
+  onChange: (newPageIndex: number) => void;
+  initialPageNumber?: number;
+  itemsPerPageCount?: number;
+};
+
+const PAGES_TO_SHOW_COUNT = 4;
+
+export function usePagination({
+  itemsCount,
+  onChange,
+  initialPageNumber = 1,
+  itemsPerPageCount = 10,
+}: IPaginationProps) {
+  const { t } = useTranslation();
+
+  const initialPageIndex = initialPageNumber - 1;
+
+  const [activePageIndex, setActivePageIndex] = useState(initialPageIndex);
+  const [pagesCount, setPagesCount] = useState(0);
+
+  /* calculating items per page count */
+  useEffect(() => {
+    setPagesCount(Math.ceil(itemsCount / itemsPerPageCount));
+  }, [itemsPerPageCount]);
+
+  const isLastPage = activePageIndex === pagesCount - 1;
+  const currentPageFirstIndex = activePageIndex * itemsPerPageCount;
+  const currentPageLastIndex = isLastPage ? itemsCount : currentPageFirstIndex + itemsPerPageCount;
+  const firstItemNumber = currentPageFirstIndex + 1;
+  const isSingleItemOnPage = firstItemNumber === currentPageLastIndex;
+  const itemsCountString = isSingleItemOnPage
+    ? t('pagination.itemOf', { currentPageLastIndex, itemsCount })
+    : t('pagination.itemsOf', { firstItemNumber, currentPageLastIndex, itemsCount });
+
+  /* creating pages array */
+  const pagesArray = Array.from({ length: pagesCount }, (_, i) => i + 1);
+
+  const halfOfPagesCount = Math.ceil(PAGES_TO_SHOW_COUNT / 2);
+  const lastPageIndex = pagesCount - 1;
+  const isActivePageInEnd = activePageIndex > lastPageIndex - halfOfPagesCount;
+  const isActivePageInStart = activePageIndex < halfOfPagesCount;
+
+  const minPageIndexToShow = isActivePageInEnd
+    ? lastPageIndex - PAGES_TO_SHOW_COUNT
+    : activePageIndex - halfOfPagesCount;
+
+  const maxPageIndexToShow = isActivePageInStart
+    ? PAGES_TO_SHOW_COUNT
+    : activePageIndex + halfOfPagesCount;
+
+  const handlePageChange = (pageIndex: number) => {
+    onChange(pageIndex);
+    setActivePageIndex(pageIndex);
+  };
+
+  return {
+    pagesCount,
+    activePageIndex,
+    itemsCountString,
+    goToPageByIndex: handlePageChange,
+    pagesArray,
+    minPageIndexToShow,
+    maxPageIndexToShow,
+  };
+}
