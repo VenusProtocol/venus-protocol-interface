4b87a601e79fb02b83e207309d6e9ab6b7ea72e5
therealemjy <maxime.julian.creations@gmail.com>
2022-06-02 18:22:04 +0100

    optimize useGetVaults

--- src/clients/api/queries/useGetVaults/constants.ts
+++ src/clients/api/queries/useGetVaults/constants.ts
@@ -2,4 +2,4 @@ import { getToken, getContractAddress } from 'utilities';
 
 export const XVS_TOKEN_ID = 'xvs';
 export const XVS_TOKEN_ADDRESS = getToken(XVS_TOKEN_ID).address;
-export const XVS_VAULT_ADDRESS = getContractAddress('xvsVault');
+export const XVS_VAULT_PROXY_CONTRACT_ADDRESS = getContractAddress('xvsVaultProxy');
