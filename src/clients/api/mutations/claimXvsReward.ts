e70e1d0aee803a6c7505ac1800c9708e4be1edc4
Corey Rice <coreyarice@gmail.com>
2022-06-06 11:51:24 -0300

    Claim rewards for all address
    Filtering claimed address by balances leaves out rewards for withdrawn or repaid balances.
    We can claims rewards on all addresses until we upgrade venusLens to return pending rewards by address

--- src/clients/api/mutations/claimXvsReward.ts
+++ src/clients/api/mutations/claimXvsReward.ts
@@ -1,14 +1,11 @@
-import BigNumber from 'bignumber.js';
 import type { TransactionReceipt } from 'web3-core/types';
 
 import { VBEP_TOKENS } from 'constants/tokens';
-import { Comptroller, VenusLens } from 'types/contracts';
+import { Comptroller } from 'types/contracts';
 import { checkForComptrollerTransactionError } from 'errors';
-import getVTokenBalancesAll from '../queries/getVTokenBalancesAll';
 
 export interface IClaimXvsRewardInput {
   comptrollerContract: Comptroller;
-  venusLensContract: VenusLens;
   fromAccountAddress: string;
 }
 
@@ -16,31 +13,16 @@ export type ClaimXvsRewardOutput = TransactionReceipt;
 
 const claimXvsReward = async ({
   comptrollerContract,
-  venusLensContract,
   fromAccountAddress,
 }: IClaimXvsRewardInput): Promise<ClaimXvsRewardOutput> => {
   // Fetch list of tokens for which user have a positive balance, since these
   // are the tokens susceptible to have generated XVS rewards
   const vTokenAddresses = Object.values(VBEP_TOKENS).map(vToken => vToken.address);
-
-  const vTokenBalances = await getVTokenBalancesAll({
-    venusLensContract,
-    vTokenAddresses,
-    account: fromAccountAddress,
-  });
-
-  const filteredVTokenAddresses = vTokenBalances
-    .filter(
-      vTokenBalance =>
-        new BigNumber(vTokenBalance.borrowBalanceCurrent).isGreaterThan(0) ||
-        new BigNumber(vTokenBalance.balanceOfUnderlying).isGreaterThan(0),
-    )
-    .map(vTokenBalance => vTokenBalance.vToken);
-
+  // @TODO [VEN-198] - use venus lens to fetch rewards by addresses once it is upgraded with this functionality
   // Send query to claim XVS reward
   const resp = await comptrollerContract.methods['claimVenus(address,address[])'](
     fromAccountAddress,
-    filteredVTokenAddresses,
+    vTokenAddresses,
   ).send({
     from: fromAccountAddress,
   });
