714bf42593f495a502fa8b4f832aa30a5b3a6a6f
therealemjy <maxime.julian.creations@gmail.com>
2022-06-06 10:48:20 +0100

    fix Market page table

--- src/pages/Market/MarketTable/index.tsx
+++ src/pages/Market/MarketTable/index.tsx
@@ -151,7 +151,7 @@ export const MarketTableUi: React.FC<IMarketTableProps> = ({ markets, getRowHref
       cardColumns={cardColumns}
       data={rows}
       initialOrder={{
-        orderBy: 'asset',
+        orderBy: 'market',
         orderDirection: 'desc',
       }}
       rowKeyIndex={0}
