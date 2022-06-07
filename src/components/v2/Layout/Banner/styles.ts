f6193f094443f17373ad03c559d78b85fe7a916b
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-06-07 14:23:45 -0300

    Add UST / LUNA deprecation warning (#612)

--- src/components/v2/Layout/Banner/styles.ts
+++ src/components/v2/Layout/Banner/styles.ts
@@ -4,15 +4,6 @@ import { useTheme } from '@mui/material';
 export const useStyles = () => {
   const theme = useTheme();
   return {
-    layout: css`
-      display: flex;
-      flex: 1;
-      flex-direction: row;
-      height: 100vh;
-      ${theme.breakpoints.down('md')} {
-        flex-direction: column;
-      }
-    `,
     banner: css`
       margin: ${theme.spacing(4, 10, 0)};
       ${theme.breakpoints.down('lg')} {
--- src/components/v2/Layout/index.tsx
+++ src/components/v2/Layout/index.tsx
@@ -1,23 +1,22 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import Box from '@mui/material/Box';
+import { useTranslation } from 'translation';
 import Sidebar from './Sidebar';
 import Header from './Header';
 import { PageContainer } from './PageContainer';
+import { Banner } from './Banner';
 import { useStyles } from './styles';
 
-export interface ILayoutProps {
-  children: React.ReactNode;
-}
-
-export const Layout: React.FC<ILayoutProps> = ({ children }) => {
+export const Layout: React.FC = ({ children }) => {
   const styles = useStyles();
-
+  const { t } = useTranslation();
   return (
     <div css={styles.layout}>
       <Sidebar />
 
       <Box display="flex" flexDirection="column" flex="1">
+        <Banner showBanner bannerText={t('layout.banner')} />
         <Header />
         <PageContainer>{children}</PageContainer>
       </Box>
