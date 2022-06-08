1478a9624c0451246d9eca4dc84904d892764b4e
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-06-08 19:44:06 +0400

    Feature: Add VoteSummary component

---
+++ src/components/v2/VoteSummary/index.stories.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import BigNumber from 'bignumber.js';
+import noop from 'noop-ts';
+import { ComponentMeta } from '@storybook/react';
+import { withThemeProvider, withCenterStory } from 'stories/decorators';
+import { VoteSummary } from '.';
+
+export default {
+  title: 'Components/VoteSummary',
+  component: VoteSummary,
+  decorators: [withThemeProvider, withCenterStory({ width: 500 })],
+  parameters: {
+    backgrounds: {
+      default: 'Default',
+    },
+  },
+} as ComponentMeta<typeof VoteSummary>;
+
+const votes = [
+  {
+    address: '0x33AAb7ED8C71C6910Fb4A9bc41de2391b74c2976',
+    voteWeightWei: new BigNumber('1000'),
+    comment: 'comment text from storybook',
+  },
+  {
+    address: '0x33AAb7ED8C71C6910Fb4A9bc41de2391b74c2977',
+    voteWeightWei: new BigNumber('1271'),
+  },
+];
+
+export const VoteFor = () => (
+  <VoteSummary
+    votedForWei={new BigNumber('100000000000000000')}
+    votedTotalWei={new BigNumber('200000000000000000')}
+    votesFrom={votes}
+    onClick={noop}
+  />
+);
+
+export const VoteAgainst = () => (
+  <VoteSummary
+    votedAgainstWei={new BigNumber('100000000000000000')}
+    votedTotalWei={new BigNumber('200000000000000000')}
+    votesFrom={votes}
+    onClick={noop}
+  />
+);
+
+export const Empty = () => (
+  <VoteSummary
+    votedForWei={new BigNumber('100000000000000000')}
+    votedTotalWei={new BigNumber('200000000000000000')}
+    onClick={noop}
+  />
+);
