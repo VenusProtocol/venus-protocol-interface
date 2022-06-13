885b416e5bcbd31521bffdc70b202b9c0d9d8cd1
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-06-13 18:11:35 +0100

    [VEN-282] Add modal to warn users to decollateralize LUNA and UST (#640)
    add modal to warn users to decollateralize LUNA and UST

--- src/pages/Dashboard/useLunaUstWarningModal.ts
+++ src/pages/Dashboard/useLunaUstWarningModal.ts
@@ -1,22 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import { Asset } from 'types';
 
 const SESSION_STORAGE_KEY = 'has-seen-modal-this-session';
 
 const useLunaUstWarningModal = (assets: Asset[]): [boolean, () => void] => {
-  const [shouldShowModal, setShouldShowModal] = useState(false);
+  const hasDeprecatedCollateral = useMemo(
+    () => assets.some(asset => (asset.id === 'luna' || asset.id === 'ust') && asset.collateral),
+    [JSON.stringify(assets)],
+  );
 
-  useEffect(() => {
-    const hasDeprecatedCollateralEnabled = assets.some(
-      asset => (asset.id === 'luna' || asset.id === 'ust') && asset.collateral,
-    );
-    const hasSeenModalThisSession = !!sessionStorage.getItem(SESSION_STORAGE_KEY);
+  const hasSeenModalThisSession = !!sessionStorage.getItem(SESSION_STORAGE_KEY);
 
-    if (hasDeprecatedCollateralEnabled && !hasSeenModalThisSession && !shouldShowModal) {
-      setShouldShowModal(true);
-    }
-  }, [JSON.stringify(assets)]);
+  const [shouldShowModal, setShouldShowModal] = useState(
+    !!hasDeprecatedCollateral && !hasSeenModalThisSession,
+  );
 
   const closeModal = () => {
     // Mark modal as seen in session storage
