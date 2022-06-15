3a09f99cefadfc33bb5156a3640cbb09e4a9d35e
Corey Rice <coreyarice@gmail.com>
2022-06-15 15:07:55 -0300

    Add Vote Summary to proposal detail page

--- src/pages/Proposal/styles.ts
+++ src/pages/Proposal/styles.ts
@@ -13,15 +13,18 @@ export const useStyles = () => {
     `,
     votes: css`
       display: flex;
-      flex-direction: row;
       margin-bottom: ${theme.spacing(8)};
     `,
     vote: css`
       display: flex;
+      flex-direction: column;
       flex: 1;
     `,
     middleVote: css`
       margin: 0 ${theme.spacing(8)};
     `,
+    successColor: theme.palette.interactive.success,
+    againstColor: theme.palette.interactive.error,
+    abstainColor: theme.palette.text.secondary,
   };
 };
