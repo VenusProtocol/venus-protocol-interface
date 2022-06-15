bc21b281454a3608919a1f96486d26be9194463b
Corey Rice <coreyarice@gmail.com>
2022-06-15 15:07:55 -0300

    Add Vote Summary to proposal detail page

--- src/pages/Proposal/index.tsx
+++ src/pages/Proposal/index.tsx
@@ -1,19 +1,46 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
-import Votes from './Votes';
+import BigNumber from 'bignumber.js';
+import { useTranslation } from 'translation';
+import VoteSummary from './VoteSummary';
 import ProposalSummary from './ProposalSummary';
 import Description from './Description';
 import { useStyles } from './styles';
 
 export const Vote: React.FC = () => {
   const styles = useStyles();
+  const { t } = useTranslation();
   return (
     <div css={styles.root}>
       <ProposalSummary css={styles.summary} />
       <div css={styles.votes}>
-        <Votes css={styles.vote} />
-        <Votes css={[styles.votes, styles.middleVote]} />
-        <Votes css={styles.vote} />
+        <VoteSummary
+          css={styles.vote}
+          label={t('vote.for')}
+          votedValueWei={new BigNumber('100000000000000000')}
+          votedTotalWei={new BigNumber('200000000000000000')}
+          votesFrom={[]}
+          onClick={() => {}}
+          progressBarColor={styles.successColor}
+        />
+        <VoteSummary
+          css={[styles.vote, styles.middleVote]}
+          label={t('vote.against')}
+          votedValueWei={new BigNumber('100000000000000000')}
+          votedTotalWei={new BigNumber('200000000000000000')}
+          votesFrom={[]}
+          onClick={() => {}}
+          progressBarColor={styles.againstColor}
+        />
+        <VoteSummary
+          css={styles.vote}
+          label={t('vote.abstain')}
+          votedValueWei={new BigNumber('100000000000000000')}
+          votedTotalWei={new BigNumber('200000000000000000')}
+          votesFrom={[]}
+          onClick={() => {}}
+          progressBarColor={styles.abstainColor}
+        />
       </div>
       <Description />
     </div>
