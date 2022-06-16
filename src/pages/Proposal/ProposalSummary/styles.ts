dcadbf5b747375d0c96f720e22924966f8f1e9bc
Corey Rice <coreyarice@gmail.com>
2022-06-16 15:19:33 -0300

    Add ProposalSummary base

--- src/pages/Proposal/ProposalSummary/styles.ts
+++ src/pages/Proposal/ProposalSummary/styles.ts
@@ -1,8 +1,46 @@
 import { css } from '@emotion/react';
+import { useTheme } from '@mui/material';
 
-export const useStyles = () => ({
-  root: css`
-    display: flex;
-    flex-direction: row;
-  `,
-});
+export const useStyles = () => {
+  const theme = useTheme();
+  return {
+    root: css`
+      display: flex;
+      flex-direction: row;
+    `,
+    leftSection: css`
+      display: flex;
+      flex-direction: column;
+      flex: 3;
+      border-right: ${theme.spacing(0.25)} solid ${theme.palette.text.secondary};
+      padding-right: ${theme.spacing(6)};
+    `,
+    topRow: css`
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin-bottom: ${theme.spacing(6)};
+    `,
+    rightSection: css`
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+      margin-left: ${theme.spacing(6)};
+    `,
+    chipSpace: css`
+      padding-right: ${theme.spacing(2)};
+    `,
+    title: css`
+      margin-bottom: ${theme.spacing(2)};
+    `,
+    content: css`
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      justify-content: space-between;
+    `,
+    rightTitle: css`
+      margin-bottom: ${theme.spacing(6)};
+    `,
+  };
+};
