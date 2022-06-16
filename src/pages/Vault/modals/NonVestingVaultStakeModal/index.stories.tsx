2c9ac873f37631a4fc42dfd872720cc060a742b7
therealemjy <maxime.julian.creations@gmail.com>
2022-06-16 18:49:31 +0100

    add basic NonVestingVaultStakeModal

--- src/pages/Vault/modals/NonVestingVaultStakeModal/index.stories.tsx
+++ src/pages/Vault/modals/NonVestingVaultStakeModal/index.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import noop from 'noop-ts';
 
 import { ComponentMeta, Story } from '@storybook/react';
-import { withCenterStory } from 'stories/decorators';
+import { withCenterStory, withEnabledToken, withAuthContext } from 'stories/decorators';
 import NonVestingVaultStakeModal, { NonVestingVaultStakeModalProps } from '.';
 
 export default {
@@ -15,8 +15,39 @@ const Template: Story<NonVestingVaultStakeModalProps> = args => (
   <NonVestingVaultStakeModal {...args} />
 );
 
+const authContext = {
+  login: noop,
+  logOut: noop,
+  openAuthModal: noop,
+  closeAuthModal: noop,
+  account: {
+    address: '0x0000000000000000000000000000000000000000',
+  },
+};
+
 export const Default = Template.bind({});
 Default.args = {
   tokenId: 'vai',
   handleClose: noop,
 };
+Default.decorators = [withAuthContext(authContext), withEnabledToken('vai')];
+
+export const WithoutConnectedAccount = Template.bind({});
+WithoutConnectedAccount.args = {
+  tokenId: 'vai',
+  handleClose: noop,
+};
+
+export const WithDisabledToken = Template.bind({});
+WithDisabledToken.args = {
+  tokenId: 'vai',
+  handleClose: noop,
+};
+WithDisabledToken.decorators = [withAuthContext(authContext)];
+
+export const WithIsInitialLoading = Template.bind({});
+WithIsInitialLoading.args = {
+  tokenId: 'vai',
+  handleClose: noop,
+};
+WithIsInitialLoading.decorators = [withAuthContext(authContext), withEnabledToken('vai')];
--- src/pages/Vault/modals/NonVestingVaultStakeModal/index.tsx
+++ src/pages/Vault/modals/NonVestingVaultStakeModal/index.tsx
@@ -2,58 +2,19 @@
 import React from 'react';
 import BigNumber from 'bignumber.js';
 
-import { Modal, IModalProps, Spinner } from 'components';
-import TransactionForm, { ITransactionFormProps } from '../../TransactionForm';
+import { getToken } from 'utilities';
+import { useTranslation } from 'translation';
+import ActionModal, { IActionModalProps } from '../ActionModal';
 
-export interface INonVestingVaultStakeModalUiProps
-  extends Pick<IModalProps, 'handleClose'>,
-    Pick<ITransactionFormProps, 'tokenId' | 'availableTokensWei' | 'onSubmit' | 'isSubmitting'> {
-  isInitialLoading: boolean;
-}
-
-const NonVestingVaultStakeModalUi: React.FC<INonVestingVaultStakeModalUiProps> = ({
-  handleClose,
-  tokenId,
-  isInitialLoading,
-  availableTokensWei,
-  onSubmit,
-  isSubmitting,
-}) => {
-  // TODO: internationalize and update content
-  const title = 'Title';
-  const availableTokensLabel = 'Available VAI';
-  const submitButtonLabel = 'Stake';
-  const submitButtonDisabledLabel = 'Enter a valid amount to stake';
-
-  return (
-    <Modal isOpen title={title} handleClose={handleClose}>
-      {isInitialLoading ? (
-        <Spinner />
-      ) : (
-        // TODO: wrap with EnableToken
-        <TransactionForm
-          tokenId={tokenId}
-          availableTokensWei={availableTokensWei}
-          availableTokensLabel={availableTokensLabel}
-          submitButtonLabel={submitButtonLabel}
-          submitButtonDisabledLabel={submitButtonDisabledLabel}
-          onSubmit={onSubmit}
-          isSubmitting={isSubmitting}
-        />
-      )}
-    </Modal>
-  );
-};
-
-export type NonVestingVaultStakeModalProps = Pick<
-  INonVestingVaultStakeModalUiProps,
-  'tokenId' | 'handleClose'
->;
+export type NonVestingVaultStakeModalProps = Pick<IActionModalProps, 'tokenId' | 'handleClose'>;
 
 const NonVestingVaultStakeModal: React.FC<NonVestingVaultStakeModalProps> = ({
   tokenId,
   handleClose,
 }) => {
+  const { t } = useTranslation();
+  const tokenSymbol = getToken(tokenId).symbol;
+
   // TODO: wire up
   const availableTokensWei = new BigNumber('193871256231321312312');
   const isInitialLoading = true;
@@ -61,13 +22,20 @@ const NonVestingVaultStakeModal: React.FC<NonVestingVaultStakeModalProps> = ({
   const isSubmitting = true;
 
   return (
-    <NonVestingVaultStakeModalUi
-      handleClose={handleClose}
+    <ActionModal
+      title={t('nonVestingVaultStakeModal.title', { tokenSymbol })}
       tokenId={tokenId}
+      handleClose={handleClose}
       availableTokensWei={availableTokensWei}
       isInitialLoading={isInitialLoading}
       onSubmit={onSubmit}
       isSubmitting={isSubmitting}
+      connectWalletMessage={t('nonVestingVaultStakeModal.connectWalletMessage', { tokenSymbol })}
+      tokenNeedsToBeEnabled
+      enableTokenMessage={t('nonVestingVaultStakeModal.enableTokenMessage', { tokenSymbol })}
+      availableTokensLabel={t('nonVestingVaultStakeModal.availableTokensLabel', { tokenSymbol })}
+      submitButtonLabel={t('nonVestingVaultStakeModal.submitButtonLabel')}
+      submitButtonDisabledLabel={t('nonVestingVaultStakeModal.submitButtonDisabledLabel')}
     />
   );
 };
