d4b969bb397089de34e4243b6ff0c935e3b5ee12
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-06-20 13:04:40 +0400

    remove serviceWorker.ts and commented code that references it

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'pages/App';
-// import * as serviceWorker from 'serviceWorker';
 
 import 'antd/dist/antd.css';
 import 'assets/styles/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// serviceWorker.register();
