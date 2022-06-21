22e4f17f607790ec6faded925bba22328944b234
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-06-21 15:00:41 +0400

    fix ios menu

--- src/components/v2/Layout/Sidebar/index.tsx
+++ src/components/v2/Layout/Sidebar/index.tsx
@@ -6,10 +6,10 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
+import { useLocation } from 'react-router-dom';
 
 import { ReactComponent as LogoDesktop } from 'assets/img/v2/venusLogoWithText.svg';
 import { ReactComponent as LogoNoText } from 'assets/img/v2/venusLogoPure.svg';
-import { ReactComponent as LogoMobile } from 'assets/img/v2/venusLogoMobile.svg';
 import { useTranslation } from 'translation';
 import { Toolbar } from '../Toolbar';
 import ClaimXvsRewardButton from '../ClaimXvsRewardButton';
@@ -24,6 +24,7 @@ export const SidebarUi: React.FC = () => {
   const open = Boolean(anchorEl);
   const { t } = useTranslation();
   const styles = useStyles();
+  const { pathname } = useLocation();
 
   const openMenu = (event: React.MouseEvent) => {
     setAnchorEl(event.currentTarget);
@@ -68,7 +69,7 @@ export const SidebarUi: React.FC = () => {
 
       <div css={styles.mobileMenuBox}>
         <div css={styles.flexRow}>
-          <LogoMobile css={styles.mobileLogo} />
+          <Icon name="logoMobile" key={pathname} css={styles.mobileLogo} />
 
           <ConnectButton small fullWidth css={styles.mobileConnectButton} />
 
@@ -91,7 +92,7 @@ export const SidebarUi: React.FC = () => {
           anchorPosition={{ top: 0, left: 0 }}
         >
           <div css={[styles.flexRow, styles.doublePadding]}>
-            <LogoMobile css={styles.mobileLogo} />
+            <Icon name="logoMobile" key={pathname} css={styles.mobileLogo} />
 
             <ConnectButton small fullWidth css={styles.mobileConnectButton} />
 
