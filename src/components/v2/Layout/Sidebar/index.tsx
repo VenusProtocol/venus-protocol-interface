769380293b3c512f577d2c4b7caed6e3983b0378
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-06-21 17:49:34 +0400

    fix logoMobile gradient id generation

--- src/components/v2/Layout/Sidebar/index.tsx
+++ src/components/v2/Layout/Sidebar/index.tsx
@@ -6,8 +6,6 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import { useLocation } from 'react-router-dom';
-import { useUID } from 'react-uid';
 
 import { ReactComponent as LogoDesktop } from 'assets/img/v2/venusLogoWithText.svg';
 import { ReactComponent as LogoNoText } from 'assets/img/v2/venusLogoPure.svg';
@@ -25,8 +23,6 @@ export const SidebarUi: React.FC = () => {
   const open = Boolean(anchorEl);
   const { t } = useTranslation();
   const styles = useStyles();
-  const { pathname } = useLocation();
-  const logoKey = pathname || useUID();
 
   const openMenu = (event: React.MouseEvent) => {
     setAnchorEl(event.currentTarget);
@@ -71,7 +67,7 @@ export const SidebarUi: React.FC = () => {
 
       <div css={styles.mobileMenuBox}>
         <div css={styles.flexRow}>
-          <Icon name="logoMobile" key={logoKey} css={styles.mobileLogo} />
+          <Icon name="logoMobile" css={styles.mobileLogo} />
 
           <ConnectButton small fullWidth css={styles.mobileConnectButton} />
 
@@ -94,7 +90,7 @@ export const SidebarUi: React.FC = () => {
           anchorPosition={{ top: 0, left: 0 }}
         >
           <div css={[styles.flexRow, styles.doublePadding]}>
-            <Icon name="logoMobile" key={logoKey} css={styles.mobileLogo} />
+            <Icon name="logoMobile" css={styles.mobileLogo} />
 
             <ConnectButton small fullWidth css={styles.mobileConnectButton} />
 
