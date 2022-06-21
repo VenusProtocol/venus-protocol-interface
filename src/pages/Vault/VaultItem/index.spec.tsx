44d1531302d37e0db46253b727d91f33fb2f65c2
therealemjy <maxime.julian.creations@gmail.com>
2022-06-21 12:35:21 +0100

    add tests

--- src/pages/Vault/VaultItem/index.spec.tsx
+++ src/pages/Vault/VaultItem/index.spec.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import BigNumber from 'bignumber.js';
 import { waitFor, fireEvent } from '@testing-library/react';
 
-import { TokenId } from 'types';
 import fakeTransactionReceipt from '__mocks__/models/transactionReceipt';
-import { TOKENS } from 'constants/tokens';
 import TEST_IDS from 'constants/testIds';
 import renderComponent from 'testUtils/renderComponent';
 import { vaults as fakeVaults } from '__mocks__/models/vaults';
@@ -12,7 +10,6 @@ import useSuccessfulTransactionModal from 'hooks/useSuccessfulTransactionModal';
 import fakeAddress from '__mocks__/models/address';
 import en from 'translation/translations/en.json';
 import useClaimVaultReward from 'hooks/useClaimVaultReward';
-import { withdrawFromVrtVault } from 'clients/api';
 import VaultItem, { VaultItemProps } from '.';
 
 jest.mock('clients/api');
@@ -51,18 +48,18 @@ describe('pages/Vault/VaultItem', () => {
     );
   });
 
-  it('sends the correct request then displays a successful transaction modal on success when clicking on the claim reward button', async () => {
+  it('clicking on claim reward button sends the correct request then displays a successful transaction modal on success', async () => {
     const { openSuccessfulTransactionModal } = useSuccessfulTransactionModal();
     const { claimReward } = useClaimVaultReward();
 
     (claimReward as jest.Mock).mockImplementationOnce(() => fakeTransactionReceipt);
 
-    const { getByText } = renderComponent(<VaultItem {...baseProps} />, {
+    const { getByTestId } = renderComponent(<VaultItem {...baseProps} />, {
       authContextValue: { account: { address: fakeAddress } },
     });
 
     // Click on claim reward button
-    const claimRewardButton = getByText(en.vaultItem.claimButton);
+    const claimRewardButton = getByTestId(TEST_IDS.vault.vaultItem.claimRewardButton);
     fireEvent.click(claimRewardButton);
 
     await waitFor(() => expect(claimReward).toHaveBeenCalledTimes(1));
@@ -76,39 +73,8 @@ describe('pages/Vault/VaultItem', () => {
     await waitFor(() => expect(openSuccessfulTransactionModal).toHaveBeenCalledTimes(1));
     expect(openSuccessfulTransactionModal).toHaveBeenCalledWith({
       transactionHash: fakeTransactionReceipt.transactionHash,
-      content: en.vaultItem.successfulClaimRewardTransactionModal.description,
-      title: en.vaultItem.successfulClaimRewardTransactionModal.title,
-    });
-  });
-
-  it('sends the correct request then displays a successful transaction modal on success when clicking on the withdraw button of the VRT non-vesting vault', async () => {
-    const { openSuccessfulTransactionModal } = useSuccessfulTransactionModal();
-
-    (withdrawFromVrtVault as jest.Mock).mockImplementationOnce(() => fakeTransactionReceipt);
-
-    const customBaseProps: VaultItemProps = {
-      ...baseProps,
-      stakedTokenId: TOKENS.vrt.id as TokenId,
-    };
-
-    const { getByText } = renderComponent(<VaultItem {...customBaseProps} />, {
-      authContextValue: { account: { address: fakeAddress } },
-    });
-
-    // Click on withdraw button
-    const withdrawButton = getByText(en.vaultItem.withdrawButton);
-    fireEvent.click(withdrawButton);
-
-    await waitFor(() => expect(withdrawFromVrtVault).toHaveBeenCalledTimes(1));
-    expect(withdrawFromVrtVault).toHaveBeenCalledWith({
-      fromAccountAddress: fakeAddress,
-    });
-
-    await waitFor(() => expect(openSuccessfulTransactionModal).toHaveBeenCalledTimes(1));
-    expect(openSuccessfulTransactionModal).toHaveBeenCalledWith({
-      transactionHash: fakeTransactionReceipt.transactionHash,
-      content: en.vaultItem.successfulWithdrawVrtTransactionModal.description,
-      title: en.vaultItem.successfulWithdrawVrtTransactionModal.title,
+      content: en.vaultItem.successfulTransactionModal.description,
+      title: en.vaultItem.successfulTransactionModal.title,
     });
   });
 });
