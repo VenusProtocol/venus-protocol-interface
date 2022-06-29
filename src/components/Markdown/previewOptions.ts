f156edef935681eb3766977d6c72289e605cb78d
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-06-29 06:31:12 -0300

    Render italic in markdown preview (#705)

--- src/components/Markdown/previewOptions.ts
+++ src/components/Markdown/previewOptions.ts
@@ -2,7 +2,7 @@ import rehypeSanitize from 'rehype-sanitize';
 
 const previewOptions = {
   skipHtml: true,
-  allowedElements: ['h1', 'h2', 'h3', 'h4', 'p', 'a', 'ul', 'li', 'strong', 'italic'],
+  allowedElements: ['h1', 'h2', 'h3', 'h4', 'p', 'a', 'ul', 'li', 'strong', 'em'],
   rehypePlugins: [rehypeSanitize],
 };
 
