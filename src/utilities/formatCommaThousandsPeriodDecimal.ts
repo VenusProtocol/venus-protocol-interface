cdbaf018ee68af98db0463598f16d0144ada9cf9
Corey Rice <coreyarice@gmail.com>
2022-06-30 11:42:30 -0300

    Remove comma number

--- src/utilities/formatCommaThousandsPeriodDecimal.ts
+++
@@ -1,5 +0,0 @@
-import commaNumber from 'comma-number';
-
-// TODO: remove once old component have been removed (see VEN-320)
-export const formatCommaThousandsPeriodDecimal = commaNumber.bindWith(',', '.');
-export default formatCommaThousandsPeriodDecimal;
