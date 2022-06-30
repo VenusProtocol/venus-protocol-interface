c28f120a1d58fe6b22c6736c1f302c783f8dd2f7
Corey Rice <coreyarice@gmail.com>
2022-06-30 10:39:16 -0300

    Setup new vault and vote pages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -10,17 +10,12 @@ import { SuccessfulTransactionModalProvider } from 'context/SuccessfulTransactio
 import { store } from 'core';
 import { Layout, ResetScrollOnRouteChange } from 'components';
 import Dashboard from 'pages/Dashboard';
-import VoteV1 from 'containers/Main/Vote';
 import Vote from 'pages/Vote';
 import Xvs from 'pages/Xvs';
 import Market from 'pages/Market';
 import Vault from 'pages/Vault';
-import VaultV1 from 'containers/Main/Vault';
-import VoteOverview from 'containers/Main/VoteOverview';
-import VoterDetails from 'pages/VoterDetails';
 import Proposal from 'pages/Proposal';
-import ProposerDetail from 'containers/Main/ProposerDetail';
-import VoterLeaderboardV1 from 'containers/Main/VoterLeaderboard';
+import VoterDetails from 'pages/VoterDetails';
 import VoterLeaderboard from 'pages/VoterLeaderboard';
 import ConvertVrt from 'pages/ConvertVrt';
 import MarketDetails from 'pages/MarketDetails';
@@ -54,38 +49,18 @@ const App = () => (
                             <Route exact path={Path.MARKET} component={Market} />
                             <Route exact path={Path.MARKET_DETAILS} component={MarketDetails} />
 
-                            <Route
-                              exact
-                              path={Path.VAULT}
-                              component={process.env.REACT_APP_RUN_V2 ? Vault : VaultV1}
-                            />
+                            <Route exact path={Path.VAULT} component={Vault} />
 
                             <Route exact path={Path.HISTORY} component={History} />
 
-                            <Route
-                              exact
-                              path={Path.VOTE}
-                              component={process.env.REACT_APP_RUN_V2 ? Vote : VoteV1}
-                            />
+                            <Route exact path={Path.VOTE} component={Vote} />
                             <Route
                               exact
                               path={Path.VOTE_LEADER_BOARD}
-                              component={
-                                process.env.REACT_APP_RUN_V2 ? VoterLeaderboard : VoterLeaderboardV1
-                              }
+                              component={VoterLeaderboard}
                             />
-                            <Route
-                              exact
-                              path={Path.VOTE_ADDRESS}
-                              component={
-                                process.env.REACT_APP_RUN_V2 ? VoterDetails : ProposerDetail
-                              }
-                            />
-                            <Route
-                              exact
-                              path={Path.VOTE_PROPOSAL_DETAILS}
-                              component={process.env.REACT_APP_RUN_V2 ? Proposal : VoteOverview}
-                            />
+                            <Route exact path={Path.VOTE_ADDRESS} component={VoterDetails} />
+                            <Route exact path={Path.VOTE_PROPOSAL_DETAILS} component={Proposal} />
 
                             <Route exact path={Path.XVS} component={Xvs} />
 
