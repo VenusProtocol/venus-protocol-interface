08452670acb45c29869ce972c83f1c7af4544e16
Corey Rice <7258308+coreyar@users.noreply.github.com>
2022-07-04 12:50:30 -0300

    [VEN-297] Remove redux (#709)
    Remove redux

--- src/pages/VoterLeaderboard/index.stories.tsx
+++ src/pages/VoterLeaderboard/index.stories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ComponentMeta, Story } from '@storybook/react';
-import { withRouter, withProvider } from 'stories/decorators';
+import { withRouter } from 'stories/decorators';
 import VoterLeaderboard from '.';
 
 export default {
   title: 'Pages/VoterLeaderboard',
   component: VoterLeaderboard,
-  decorators: [withRouter, withProvider],
+  decorators: [withRouter],
   parameters: {
     backgrounds: {
       default: 'White',
