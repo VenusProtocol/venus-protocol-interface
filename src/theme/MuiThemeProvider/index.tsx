24c602750ed3fb73dffa2ec0208f1f584208597e
Corey Rice <coreyarice@gmail.com>
2022-07-04 11:17:34 -0300

    Add normalize css

--- src/theme/MuiThemeProvider/index.tsx
+++ src/theme/MuiThemeProvider/index.tsx
@@ -3,6 +3,8 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import mainTheme from './muiTheme';
 
+import 'normalize.css';
+
 export function MuiThemeProvider({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider theme={mainTheme}>
