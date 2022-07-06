1f9660270240bd33583424c1c0d09b5afbbdf714
therealemjy <maxime.julian.creations@gmail.com>
2022-07-06 11:46:40 +0100

    remove MarketContext

--- src/hooks/useMarkets.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { MarketContext } from '../context/MarketContext';
-
-export const useMarkets = () => {
-  const { markets, dailyVenus, treasuryTotalUSDBalance } = useContext(MarketContext);
-  return { markets, dailyVenus, treasuryTotalUSDBalance };
-};
