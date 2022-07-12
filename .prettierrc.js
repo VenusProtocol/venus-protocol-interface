1746aa19e54d13bfba617d11c51e2edd584dfbdc
therealemjy <maxime.julian.creations@gmail.com>
2022-07-12 14:08:59 +0100

    add @trivago/prettier-plugin-sort-imports + untrack contract types

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   ...require('prettier-airbnb-config'),
-  "trailingComma": "all",
-  "bracketSpacing": true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  importOrder: ['<THIRD_PARTY_MODULES>', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
