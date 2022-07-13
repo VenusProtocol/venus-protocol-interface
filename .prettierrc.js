e6e51612edab451f01c4b9cc0695bd8392a32f68
therealemjy <maxime.julian.creations@gmail.com>
2022-07-13 14:11:58 +0100

    add sorting rules to Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,10 @@ module.exports = {
   ...require('prettier-airbnb-config'),
   trailingComma: 'all',
   bracketSpacing: true,
-  importOrder: ['<THIRD_PARTY_MODULES>', '^[./]'],
+  importOrder: [
+    '(__mocks__|assets|clients|components|config|constants|containers|context|errors|hooks|pages|stories|testUtils|theme|translation|types|utilities)/(.*)$',
+    '^[./]',
+  ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
 };
