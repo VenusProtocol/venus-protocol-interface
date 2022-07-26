bfd78fbfd71a205bec61c6d9a018f8e7399fd7f4
therealemjy <maxime.julian.creations@gmail.com>
2022-07-26 09:30:38 +0100

    update return type of getAssetsInAccount

--- src/clients/api/queries/getAssetsInAccount/index.spec.ts
+++ src/clients/api/queries/getAssetsInAccount/index.spec.ts
@@ -1,3 +1,7 @@
+import fakeAddress from '__mocks__/models/address';
+import { TOKENS } from 'constants/tokens';
+import { Comptroller } from 'types/contracts';
+
 import getAssetsInAccount from '.';
 
 describe('api/queries/getAssetsInAccount', () => {
@@ -10,13 +14,40 @@ describe('api/queries/getAssetsInAccount', () => {
           },
         }),
       },
-    } as any;
+    } as unknown as Comptroller;
 
     try {
-      await getAssetsInAccount({ comptrollerContract: fakeContract, account: '0x2398' });
+      await getAssetsInAccount({ comptrollerContract: fakeContract, accountAddress: fakeAddress });
       throw new Error('getAssetsInAccount should have thrown an error but did not');
     } catch (error) {
       expect(error).toMatchInlineSnapshot('[Error: Fake error message]');
     }
+  });
+
+  test('returns addresses of assets in account on success', async () => {
+    const fakeTokenAddresses = [TOKENS.aave.address, TOKENS.ada.address];
+
+    const callMock = jest.fn(async () => fakeTokenAddresses);
+    const getAssetsInMock = jest.fn(() => ({
+      call: callMock,
+    }));
+
+    const fakeContract = {
+      methods: {
+        getAssetsIn: getAssetsInMock,
+      },
+    } as unknown as Comptroller;
+
+    const response = await getAssetsInAccount({
+      comptrollerContract: fakeContract,
+      accountAddress: fakeAddress,
+    });
+
+    expect(getAssetsInMock).toHaveBeenCalledTimes(1);
+    expect(callMock).toHaveBeenCalledTimes(1);
+    expect(getAssetsInMock).toHaveBeenCalledWith(fakeAddress);
+    expect(response).toEqual({
+      tokenAddresses: fakeTokenAddresses,
+    });
   });
 });
