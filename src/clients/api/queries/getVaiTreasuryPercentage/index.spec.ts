7eb9f18976bb6eb2471d52fe35cd2a99ca46465c
therealemjy <maxime.julian.creations@gmail.com>
2022-07-26 10:54:38 +0100

    update return type of getVaiTreasuryPercentage

--- src/clients/api/queries/getVaiTreasuryPercentage/index.spec.ts
+++ src/clients/api/queries/getVaiTreasuryPercentage/index.spec.ts
@@ -1,5 +1,7 @@
 import BigNumber from 'bignumber.js';
 
+import { VaiUnitroller } from 'types/contracts';
+
 import getVaiTreasuryPercentage from '.';
 
 describe('api/queries/getVaiTreasuryPercentage', () => {
