30037c525383e069fd2f2ff9e845a3ed36f67f1b
therealemjy <maxime.julian.creations@gmail.com>
2022-07-26 10:21:33 +0100

    update return type of getLatestProposalIdByProposer

--- src/clients/api/queries/getLatestProposalIdByProposer/index.ts
+++ src/clients/api/queries/getLatestProposalIdByProposer/index.ts
@@ -5,12 +5,19 @@ export interface GetLatestProposalIdByProposerInput {
   accountAddress: string;
 }
 
-export type GetLatestProposalIdByProposerOutput = string;
+export type GetLatestProposalIdByProposerOutput = {
+  proposalId: string;
+};
 
 const getLatestProposalIdByProposer = async ({
   governorBravoContract,
   accountAddress,
-}: GetLatestProposalIdByProposerInput): Promise<GetLatestProposalIdByProposerOutput> =>
-  governorBravoContract.methods.latestProposalIds(accountAddress).call();
+}: GetLatestProposalIdByProposerInput): Promise<GetLatestProposalIdByProposerOutput> => {
+  const res = await governorBravoContract.methods.latestProposalIds(accountAddress).call();
+
+  return {
+    proposalId: res,
+  };
+};
 
 export default getLatestProposalIdByProposer;
