17ba579da0e0a4811787c496d415bd63fd7127f4
therealemjy <maxime.julian.creations@gmail.com>
2022-07-27 11:17:51 +0100

    update return type of getXvsVaultTotalAllocationPoints

--- src/clients/api/queries/useGetVaults/useGetVestingVaults/index.ts
+++ src/clients/api/queries/useGetVaults/useGetVestingVaults/index.ts
@@ -40,7 +40,7 @@ const useGetVestingVaults = ({
     });
 
   const {
-    data: xvsVaultTotalAllocationPoints,
+    data: xvsVaultTotalAllocationPointsData,
     isLoading: isGetXvsVaultTotalAllocationPointsLoading,
   } = useGetXvsVaultTotalAllocationPoints({
     tokenAddress: XVS_TOKEN_ADDRESS,
@@ -140,11 +140,11 @@ const useGetVestingVaults = ({
 
           const poolRewardWeiPerBlock =
             xvsVaultRewardWeiPerBlock?.rewardPerBlockWei &&
-            xvsVaultTotalAllocationPoints &&
+            xvsVaultTotalAllocationPointsData?.totalAllocationPoints &&
             poolData[poolIndex]?.poolInfos.allocationPoint &&
             xvsVaultRewardWeiPerBlock.rewardPerBlockWei
               .multipliedBy(poolData[poolIndex]?.poolInfos.allocationPoint)
-              .div(xvsVaultTotalAllocationPoints);
+              .div(xvsVaultTotalAllocationPointsData.totalAllocationPoints);
 
           const dailyEmissionWei =
             poolRewardWeiPerBlock && poolRewardWeiPerBlock.multipliedBy(BLOCKS_PER_DAY);
@@ -189,7 +189,7 @@ const useGetVestingVaults = ({
       JSON.stringify(poolData),
       JSON.stringify(poolBalances),
       xvsVaultRewardWeiPerBlock?.rewardPerBlockWei.toFixed(),
-      xvsVaultTotalAllocationPoints,
+      xvsVaultTotalAllocationPointsData?.totalAllocationPoints,
     ],
   );
 
