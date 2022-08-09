beeb74cbc9a99d4c1a97bec9b5f71cb59b1db6e4
therealemjy <maxime.julian.creations@gmail.com>
2022-08-09 11:54:21 +0200

    clean up dashboard in preparation for isolated lending

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -24,109 +24,17 @@ interface DashboardUiProps {
   assets: Asset[];
 }
 
-const DashboardUi: React.FC<DashboardUiProps> = ({
-  accountAddress,
-  assets,
-  areHigherRiskTokensDisplayed,
-  onHigherRiskTokensToggleChange,
-  searchValue,
-  onSearchInputChange,
-}) => {
-  const { t } = useTranslation();
-  const styles = useStyles();
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
-
-  const showXlDownCss = useShowXlDownCss();
-  const hideXlDownCss = useHideXlDownCss();
-
-  // TODO: add all options
-  const mobileSelectOptions = [
-    {
-      value: 'riskLevel',
-      label: 'Risk level',
-    },
-  ];
-
-  const handleSearchInputChange: InputHTMLAttributes<HTMLInputElement>['onChange'] = changeEvent =>
-    onSearchInputChange(changeEvent.currentTarget.value);
-
-  return (
-    <>
-      <HigherRiskTokensNotice />
-
-      <div css={styles.header}>
-        <TextField
-          css={[styles.tabletSearchTextField, showXlDownCss]}
-          isSmall
-          value={searchValue}
-          onChange={handleSearchInputChange}
-          placeholder={t('dashboard.searchInput.placeholder')}
-          leftIconName="magnifier"
-        />
-
-        <ButtonGroup
-          css={[styles.tabletButtonGroup, showXlDownCss]}
-          fullWidth
-          buttonLabels={[t('dashboard.supplyMarketTabTitle'), t('dashboard.borrowMarketTabTitle')]}
-          activeButtonIndex={activeTabIndex}
-          onButtonClick={setActiveTabIndex}
-        />
-
-        <div css={styles.headerBottomRow}>
-          <Select
-            css={[styles.mobileSelect, showXlDownCss]}
-            label={t('dashboard.mobileSelect.label')}
-            title={t('dashboard.mobileSelect.title')}
-            // TODO: wire up
-            value={mobileSelectOptions[0].value}
-            onChange={console.log}
-            options={mobileSelectOptions}
-            ariaLabel={t('dashboard.mobileSelect.ariaLabelFor')}
-          />
-
-          <ButtonGroup
-            css={hideXlDownCss}
-            buttonLabels={[
-              t('dashboard.supplyMarketTabTitle'),
-              t('dashboard.borrowMarketTabTitle'),
-            ]}
-            activeButtonIndex={activeTabIndex}
-            onButtonClick={setActiveTabIndex}
-          />
-
-          <div css={styles.rightColumn}>
-            <Toggle
-              tooltip={t('dashboard.riskyTokensToggleTooltip')}
-              label={t('dashboard.riskyTokensToggleLabel')}
-              isLight
-              value={areHigherRiskTokensDisplayed}
-              onChange={event => onHigherRiskTokensToggleChange(event.currentTarget.checked)}
-            />
-
-            <TextField
-              css={[styles.desktopSearchTextField, hideXlDownCss]}
-              isSmall
-              value={searchValue}
-              onChange={handleSearchInputChange}
-              placeholder={t('dashboard.searchInput.placeholder')}
-              leftIconName="magnifier"
-            />
-          </div>
-        </div>
-      </div>
-
-      {activeTabIndex === 0 ? (
-        // TODO: get isXvsEnabled from context
-        // TODO: handle sorting on mobile
-        <SupplyMarket isXvsEnabled accountAddress={accountAddress} assets={assets} />
-      ) : (
-        // TODO: get isXvsEnabled from context
-        // TODO: handle sorting on mobile
-        <BorrowMarket isXvsEnabled assets={assets} />
-      )}
-    </>
-  );
-};
+const DashboardUi: React.FC<DashboardUiProps> = ({ accountAddress, assets }) => (
+  <>
+    <Markets
+      isXvsEnabled
+      accountAddress={accountAddress}
+      // TODO: refactor to pass just one assets prop
+      supplyMarketAssets={assets}
+      borrowMarketAssets={assets}
+    />
+  </>
+);
 
 const Dashboard: React.FC = () => {
   const { account } = useContext(AuthContext);
