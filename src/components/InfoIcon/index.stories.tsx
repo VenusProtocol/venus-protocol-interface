4911b4b136a31a2c803caecd9530180aa4829e42
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-08-23 10:33:05 +0100

    [VEN-493] Create CellGroup and InfoIcon components (#814)
    * add label to Select component
    
    * add label and tooltip to Toggle component
    
    * add basic CellGroup component
    
    * add InfoIcon component
    
    * complete CellGroup component

---
+++ src/components/InfoIcon/index.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { withCenterStory } from 'stories/decorators';
+
+import { InfoIcon } from '.';
+
+export default {
+  title: 'Components/InfoIcon',
+  component: InfoIcon,
+  decorators: [withCenterStory({ width: 100 })],
+} as ComponentMeta<typeof InfoIcon>;
+
+export const Default = () => <InfoIcon tooltip="This is a fake tooltip" />;
