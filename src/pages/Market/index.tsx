b19c931ac555c0006b95ddb3c178cae742a8f8e1
therealemjy <maxime.julian.creations@gmail.com>
2022-08-30 11:17:03 +0100

    add CellGroup

--- src/pages/Market/index.tsx
+++ src/pages/Market/index.tsx
@@ -1,5 +1,68 @@
-import React from 'react';
+import { Cell, CellGroup } from 'components';
+import React, { useMemo } from 'react';
+import { useTranslation } from 'translation';
+import { Asset } from 'types';
+import { formatCentsToReadableValue } from 'utilities';
 
-const Market: React.FC = () => <>Market</>;
+import { assetData } from '__mocks__/models/asset';
+
+export interface MarketUiProps {
+  assets: Asset[];
+  totalSupplyCents: number;
+  totalBorrowCents: number;
+}
+
+export const MarketUi: React.FC<MarketUiProps> = ({
+  assets,
+  totalSupplyCents,
+  totalBorrowCents,
+}) => {
+  const { t } = useTranslation();
+
+  const cells: Cell[] = useMemo(
+    () => [
+      {
+        label: t('market.header.totalSupplyLabel'),
+        value: formatCentsToReadableValue({
+          value: totalSupplyCents,
+        }),
+      },
+      {
+        label: t('market.header.totalBorrowLabel'),
+        value: formatCentsToReadableValue({
+          value: totalBorrowCents,
+        }),
+      },
+      {
+        label: t('market.header.availableLiquidityLabel'),
+        value: formatCentsToReadableValue({
+          value: totalSupplyCents - totalBorrowCents,
+        }),
+      },
+      {
+        label: t('market.header.assetsLabel'),
+        value: assets.length,
+      },
+    ],
+    [totalSupplyCents, totalBorrowCents, assets.length],
+  );
+
+  return <CellGroup cells={cells} />;
+};
+
+const Market: React.FC = () => {
+  // TODO: fetch actual values (see https://jira.toolsfdg.net/browse/VEN-546)
+  const assets = assetData;
+  const totalSupplyCents = 1000000000;
+  const totalBorrowCents = 100000000;
+
+  return (
+    <MarketUi
+      assets={assets}
+      totalSupplyCents={totalSupplyCents}
+      totalBorrowCents={totalBorrowCents}
+    />
+  );
+};
 
 export default Market;
