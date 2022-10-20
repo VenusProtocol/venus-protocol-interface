331ccf79500fdfcc1bf70ffbb6505e163e02c87e
therealemjy <maxime.julian.creations@gmail.com>
2022-10-20 18:22:08 +0100

    add switch button

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import Paper from '@mui/material/Paper';
-import { SelectTokenTextField } from 'components';
+import { Icon, SelectTokenTextField, TertiaryButton } from 'components';
 import React, { useState } from 'react';
+import { useTranslation } from 'translation';
 import { TokenId } from 'types';
 
 import { TOKENS } from 'constants/tokens';
@@ -28,13 +29,26 @@ const initialFormValues: FormValues = {
 
 const SwapUi: React.FC = () => {
   const styles = useStyles();
+  const { t } = useTranslation();
 
   const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
+
+  const switchTokens = () =>
+    setFormValues(currentFormValues => ({
+      ...currentFormValues,
+      fromTokenId: currentFormValues.toTokenId,
+      fromTokenAmount: currentFormValues.toTokenAmount,
+      toTokenId: currentFormValues.fromTokenId,
+      toTokenAmount: currentFormValues.fromTokenAmount,
+      direction:
+        currentFormValues.direction === 'exactAmountIn' ? 'exactAmountOut' : 'exactAmountIn',
+    }));
 
   return (
     <Paper css={styles.container}>
       <SelectTokenTextField
         name="fromTokenAmount"
+        label={t('swapPage.fromTokenAmountField.label')}
         selectedTokenId={formValues.fromTokenId as TokenId}
         value={formValues.fromTokenAmount}
         onChange={amount =>
@@ -57,10 +71,16 @@ const SwapUi: React.FC = () => {
           }))
         }
         tokenIds={tokenIds.filter(tokenId => tokenId !== formValues.fromTokenId)}
+        css={styles.selectTokenTextField}
       />
+
+      <TertiaryButton css={styles.switchButton} onClick={switchTokens}>
+        <Icon name="convert" css={styles.switchButtonIcon} />
+      </TertiaryButton>
 
       <SelectTokenTextField
         name="toTokenAmount"
+        label={t('swapPage.toTokenAmountField.label')}
         selectedTokenId={formValues.toTokenId as TokenId}
         value={formValues.toTokenAmount}
         onChange={amount =>
@@ -83,6 +103,7 @@ const SwapUi: React.FC = () => {
           }))
         }
         tokenIds={tokenIds.filter(tokenId => tokenId !== formValues.toTokenId)}
+        css={styles.selectTokenTextField}
       />
     </Paper>
   );
