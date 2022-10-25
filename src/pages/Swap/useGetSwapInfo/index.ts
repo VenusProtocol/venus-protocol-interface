40647ffaf65b6ce66e2427a487fd2f5000717abc
therealemjy <maxime.julian.creations@gmail.com>
2022-10-25 17:16:50 +0100

    fix issue with infinite render loop

--- src/pages/Swap/useGetSwapInfo/index.ts
+++ src/pages/Swap/useGetSwapInfo/index.ts
@@ -6,6 +6,7 @@ import {
   TradeType as PSTradeType,
 } from '@pancakeswap/sdk/dist/index.js';
 import BigNumber from 'bignumber.js';
+import { useMemo } from 'react';
 import { Token } from 'types';
 import { convertTokensToWei } from 'utilities';
 
@@ -24,69 +25,75 @@ export interface UseGetSwapInfoInput {
 }
 
 const useGetSwapInfo = (input: UseGetSwapInfoInput): Swap | undefined => {
-  console.log('rendered');
-
   // Determine all possible pairs based on input tokens
-  // const tokenCombinations = useGetTokenCombinations({
-  //   fromToken: input.fromToken,
-  //   toToken: input.toToken,
-  // });
+  const tokenCombinations = useGetTokenCombinations({
+    fromToken: input.fromToken,
+    toToken: input.toToken,
+  });
 
-  // const pairs = useGetPairs([]);
+  const pairs = useGetPairs(tokenCombinations);
 
   // TODO: fetch pair data
 
   // TODO: determine best swap using pair data
 
-  // TODO: get from fetched swap info or calculate using swap info
-  const exchangeRate = new BigNumber('1.126783');
+  return useMemo(() => {
+    // TODO: get from fetched swap info or calculate using swap info
+    const exchangeRate = new BigNumber('1.126783');
 
-  if (input.direction === 'exactAmountIn' && !!input.fromTokenAmountTokens) {
-    // TODO: get from fetched swap info
-    const expectedToTokenAmountReceivedWei = new BigNumber('190287638578');
-    // Calculate minimum received accepted according to slippage
-    const minimumToTokenAmountReceivedWei = expectedToTokenAmountReceivedWei
-      .multipliedBy(1 - SLIPPAGE_TOLERANCE_PERCENTAGE / 100)
-      .dp(0);
+    if (input.direction === 'exactAmountIn' && !!input.fromTokenAmountTokens) {
+      // TODO: get from fetched swap info
+      const expectedToTokenAmountReceivedWei = new BigNumber('190287638578');
+      // Calculate minimum received accepted according to slippage
+      const minimumToTokenAmountReceivedWei = expectedToTokenAmountReceivedWei
+        .multipliedBy(1 - SLIPPAGE_TOLERANCE_PERCENTAGE / 100)
+        .dp(0);
 
-    return {
-      fromToken: input.fromToken,
-      toToken: input.toToken,
-      exchangeRate,
-      fromTokenAmountSoldWei: convertTokensToWei({
-        value: new BigNumber(input.fromTokenAmountTokens),
-        tokenId: input.fromToken.id,
-      }),
-      expectedToTokenAmountReceivedWei,
-      minimumToTokenAmountReceivedWei,
-      direction: 'exactAmountIn',
-    };
-  }
+      return {
+        fromToken: input.fromToken,
+        toToken: input.toToken,
+        exchangeRate,
+        fromTokenAmountSoldWei: convertTokensToWei({
+          value: new BigNumber(input.fromTokenAmountTokens),
+          tokenId: input.fromToken.id,
+        }),
+        expectedToTokenAmountReceivedWei,
+        minimumToTokenAmountReceivedWei,
+        direction: 'exactAmountIn',
+      };
+    }
 
-  if (input.direction === 'exactAmountOut' && !!input.toTokenAmountTokens) {
-    // TODO: get from fetched swap info
-    const expectedFromTokenAmountSoldWei = new BigNumber('467312321');
-    // Calculate maximum sold accepted according to slippage
-    const maximumFromTokenAmountSoldWei = expectedFromTokenAmountSoldWei
-      .multipliedBy(1 + SLIPPAGE_TOLERANCE_PERCENTAGE / 100)
-      .dp(0);
+    if (input.direction === 'exactAmountOut' && !!input.toTokenAmountTokens) {
+      // TODO: get from fetched swap info
+      const expectedFromTokenAmountSoldWei = new BigNumber('467312321');
+      // Calculate maximum sold accepted according to slippage
+      const maximumFromTokenAmountSoldWei = expectedFromTokenAmountSoldWei
+        .multipliedBy(1 + SLIPPAGE_TOLERANCE_PERCENTAGE / 100)
+        .dp(0);
 
-    return {
-      fromToken: input.fromToken,
-      toToken: input.toToken,
-      exchangeRate,
-      expectedFromTokenAmountSoldWei,
-      maximumFromTokenAmountSoldWei,
-      toTokenAmountReceivedWei: convertTokensToWei({
-        value: new BigNumber(input.toTokenAmountTokens),
-        tokenId: input.toToken.id,
-      }),
-      direction: 'exactAmountOut',
-    };
-  }
+      return {
+        fromToken: input.fromToken,
+        toToken: input.toToken,
+        exchangeRate,
+        expectedFromTokenAmountSoldWei,
+        maximumFromTokenAmountSoldWei,
+        toTokenAmountReceivedWei: convertTokensToWei({
+          value: new BigNumber(input.toTokenAmountTokens),
+          tokenId: input.toToken.id,
+        }),
+        direction: 'exactAmountOut',
+      };
+    }
 
-  // Return undefined if a mandatory prop is missing
-  return undefined;
+    // Return undefined if a mandatory prop is missing
+    return undefined;
+  }, [
+    input.direction,
+    input.fromToken,
+    input.toToken,
+    input.fromTokenAmountTokens,
+    input.toTokenAmountTokens,
+  ]);
 };
 
 export default useGetSwapInfo;
