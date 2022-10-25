4eb3b8e666c4e1de003a36427fd578218fcbe32e
therealemjy <maxime.julian.creations@gmail.com>
2022-10-25 15:04:17 +0100

    generate pair addresses

--- src/pages/Swap/useGetSwapInfo/useGetPairs.ts
+++ src/pages/Swap/useGetSwapInfo/useGetPairs.ts
@@ -1,7 +1,27 @@
-import { Token as PSToken } from '@pancakeswap/sdk/dist/index.js';
+import { Pair as PSPair, Token as PSToken } from '@pancakeswap/sdk/dist/index.js';
+import { useMemo } from 'react';
 
 const useGetPairs = (tokenCombinations: [PSToken, PSToken][]) => {
-  console.log('tokenCombinations', tokenCombinations);
+  // Generate pair addresses
+  const pairAddresses = useMemo(
+    () =>
+      tokenCombinations
+        .map(([tokenA, tokenB]) => {
+          try {
+            return tokenA && tokenB && !tokenA.equals(tokenB)
+              ? PSPair.getAddress(tokenA, tokenB)
+              : undefined;
+          } catch {
+            // Do nothing
+            return undefined;
+          }
+        })
+        // Remove undefined values
+        .filter(address => !!address),
+    [tokenCombinations],
+  );
+
+  console.log('pairAddresses', pairAddresses);
 
   return [];
 };
