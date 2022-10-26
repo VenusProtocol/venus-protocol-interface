7d57097e0c1fc01639a8e3280793d20f29262665
therealemjy <maxime.julian.creations@gmail.com>
2022-10-26 15:22:40 +0100

    update list of tokens

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -18,12 +18,12 @@ import { SLIPPAGE_TOLERANCE_PERCENTAGE } from 'constants/swap';
 import { AuthContext } from 'context/AuthContext';
 
 import { useStyles } from './styles';
-import tokenList from './tokenList';
+import { PANCAKE_SWAP_TOKENS } from './tokenList';
 import { Swap, SwapDirection } from './types';
 import useGetSwapInfo from './useGetSwapInfo';
 
 // TODO: fix (TokenId type is incorrect) (see https://jira.toolsfdg.net/browse/VEN-712)
-const tokenIds = Object.keys(tokenList) as TokenId[];
+const tokenIds = Object.keys(PANCAKE_SWAP_TOKENS) as TokenId[];
 
 const readableSlippageTolerancePercentage = formatToReadablePercentage(
   SLIPPAGE_TOLERANCE_PERCENTAGE,
@@ -38,9 +38,9 @@ interface FormValues {
 }
 
 const initialFormValues: FormValues = {
-  fromToken: getToken('bnb'),
+  fromToken: getToken('eth'),
   fromTokenAmountTokens: '',
-  toToken: getToken('xvs'),
+  toToken: getToken('usdt'),
   toTokenAmountTokens: '',
   direction: 'exactAmountIn',
 };
