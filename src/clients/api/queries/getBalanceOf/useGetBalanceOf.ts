9ad9bcd5575570dfd5110b6b42888fcac66bcc3e
therealemjy <maxime.julian.creations@gmail.com>
2022-10-28 09:20:58 +0100

    update getBalanceOf to support BNB

--- src/clients/api/queries/getBalanceOf/useGetBalanceOf.ts
+++ src/clients/api/queries/getBalanceOf/useGetBalanceOf.ts
@@ -2,7 +2,7 @@ import { QueryObserverOptions, useQuery } from 'react-query';
 import { TokenId } from 'types';
 
 import { GetBalanceOfInput, GetBalanceOfOutput, getBalanceOf } from 'clients/api';
-import { useTokenContract } from 'clients/contracts/hooks';
+import { useWeb3 } from 'clients/web3';
 import FunctionKey from 'constants/functionKey';
 
 type Options = QueryObserverOptions<
@@ -14,14 +14,14 @@ type Options = QueryObserverOptions<
 >;
 
 const useGetBalanceOf = (
-  { accountAddress, tokenId }: Omit<GetBalanceOfInput, 'tokenContract'> & { tokenId: TokenId },
+  { accountAddress, tokenId }: Omit<GetBalanceOfInput, 'web3'> & { tokenId: TokenId },
   options?: Options,
 ) => {
-  const tokenContract = useTokenContract(tokenId);
+  const web3 = useWeb3();
 
   return useQuery(
     [FunctionKey.GET_BALANCE_OF, accountAddress, tokenId],
-    () => getBalanceOf({ tokenContract, accountAddress }),
+    () => getBalanceOf({ web3, accountAddress, tokenId }),
     options,
   );
 };
