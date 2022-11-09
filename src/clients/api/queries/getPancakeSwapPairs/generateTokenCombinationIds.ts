3105458918b6256f9a0dfdc6a6e6e713066c482f
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-11-09 17:00:44 +0000

    [VEN-726] Add tests to cover Swap (Part 1) (#857)
    * add tests for wrapToken function
    
    * add tests for useGetTokenCombinations
    
    * add tests for getPancakeSwapPairs
    
    * add tests for useGetSwapInfo
    
    * fix generateTokenCombinationIds

--- src/clients/api/queries/getPancakeSwapPairs/generateTokenCombinationIds.ts
+++ src/clients/api/queries/getPancakeSwapPairs/generateTokenCombinationIds.ts
@@ -1,8 +1,8 @@
-import { TokenCombination } from './types';
+import { PSTokenCombination } from 'types';
 
-const generateTokenCombinationIds = (tokenCombinations: TokenCombination[]) =>
+const generateTokenCombinationIds = (tokenCombinations: PSTokenCombination[]) =>
   tokenCombinations
-    .map(tokenCombination => `${tokenCombination[0].address}-${tokenCombination[0].address}`)
+    .map(tokenCombination => `${tokenCombination[0].address}-${tokenCombination[1].address}`)
     // Sort generated IDs to output the same data when providing the same token
     // combinations in a different order. This prevents unnecessary processing
     // when using the IDs as query key
--- src/pages/Swap/useGetSwapInfo/types.ts
+++ src/pages/Swap/useGetSwapInfo/types.ts
@@ -5,6 +5,8 @@ import {
 } from '@pancakeswap/sdk/dist/index.js';
 import { Swap, SwapDirection, Token } from 'types';
 
+export type SwapError = 'INSUFFICIENT_LIQUIDITY' | 'WRAPPING_UNWRAPPING_UNSUPPORTED';
+
 export interface UseGetSwapInfoInput {
   fromToken: Token;
   toToken: Token;
@@ -13,7 +15,10 @@ export interface UseGetSwapInfoInput {
   toTokenAmountTokens?: string;
 }
 
-export type SwapError = 'INSUFFICIENT_LIQUIDITY' | 'WRAPPING_UNWRAPPING_UNSUPPORTED';
+export interface UseGetSwapInfoOutput {
+  swap: Swap | undefined;
+  error: SwapError | undefined;
+}
 
 export interface FormatToSwapInput {
   input: UseGetSwapInfoInput;
