2afc27ab3f7595e9959707aec84e52d7916d6bbf
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2022-11-30 18:06:56 +0000

    Fix Market Details charts (develop) (#872)
    fix Market Details charts + add vTokenId to query key of useGetMarketHistory

--- src/clients/api/queries/getMarketHistory/useGetMarketHistory.ts
+++ src/clients/api/queries/getMarketHistory/useGetMarketHistory.ts
@@ -11,12 +11,12 @@ type Options = QueryObserverOptions<
   Error,
   GetMarketHistoryOutput,
   GetMarketHistoryOutput,
-  [FunctionKey.GET_MARKET_HISTORY, { vTokenId: string }]
+  [FunctionKey.GET_MARKET_HISTORY, { vTokenAddress: string }]
 >;
 
 const useGetMarketHistory = (input: GetMarketHistoryInput, options?: Options) =>
   useQuery(
-    [FunctionKey.GET_MARKET_HISTORY, { vTokenId: input.vTokenId }],
+    [FunctionKey.GET_MARKET_HISTORY, { vTokenAddress: input.vToken.address }],
     () => getMarketHistory(input),
     options,
   );
