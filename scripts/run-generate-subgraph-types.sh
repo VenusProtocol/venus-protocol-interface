2c217261ef545a2d91c7184e92bfd84996c117f7
Gleiser Oliveira <gleiser.dev@gmail.com>
2022-12-01 14:16:28 -0600

    Check REACT_APP_CHAIN_ID and set SUBGRAPH_GRAPHQL_ENDPOINT

--- scripts/run-generate-subgraph-types.sh
+++ scripts/run-generate-subgraph-types.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [[ -z "${SUBGRAPH_GRAPHQL_ENDPOINT}" ]]; then
+if [[ "${REACT_APP_CHAIN_ID}" == "56" ]]; then
+  export SUBGRAPH_GRAPHQL_ENDPOINT="https://api.thegraph.com/subgraphs/name/venusprotocol/venus-isolated-pools"
+else
   export SUBGRAPH_GRAPHQL_ENDPOINT="https://api.thegraph.com/subgraphs/name/venusprotocol/venus-isolated-pools-chapel"
 fi
 
