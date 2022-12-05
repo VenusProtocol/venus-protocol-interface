4446fd546169b11b1be8f9b37206e502804672d8
Gleiser Oliveira <52756735+gleiser-oliveira@users.noreply.github.com>
2022-12-05 10:36:41 -0600

    [VEN-813] Added a max button to the swap from balance input (#878)
    * Added a max button to the swap from balance input
    
    * PR comments

--- src/pages/Swap/testIds.ts
+++ src/pages/Swap/testIds.ts
@@ -1,5 +1,6 @@
 const TEST_IDS = {
   fromTokenSelectTokenTextField: 'from-token-select-token-text-field',
+  fromTokenMaxButton: 'from-token-max-button',
   toTokenSelectTokenTextField: 'to-token-select-token-text-field',
   switchTokensButton: 'switch-tokens-button',
   swapDetails: 'swap-details',
