33e7f2b32bbdb1f7419629d297aba06b6c890d05
therealemjy <maxime.julian.creations@gmail.com>
2022-12-23 19:22:32 +0100

    start wiring up Pools and Pool pages

--- src/pages/Pools/PoolTable/index.spec.tsx
+++ src/pages/Pools/PoolTable/index.spec.tsx
@@ -1,8 +1,5 @@
-import BigNumber from 'bignumber.js';
 import React from 'react';
 
-import { assetData } from '__mocks__/models/asset';
-import { useGetMainAssets } from 'clients/api';
 import renderComponent from 'testUtils/renderComponent';
 
 import PoolTable from '.';
@@ -10,18 +7,6 @@ import PoolTable from '.';
 jest.mock('clients/api');
 
 describe('pages/Pools/PoolTable', () => {
-  beforeEach(() => {
-    (useGetMainAssets as jest.Mock).mockImplementation(() => ({
-      data: {
-        assets: assetData,
-        userTotalBorrowLimitCents: new BigNumber('111'),
-        userTotalBorrowBalanceCents: new BigNumber('91'),
-        userTotalSupplyBalanceCents: new BigNumber('910'),
-      },
-      isLoading: false,
-    }));
-  });
-
   it('renders without crashing', async () => {
     renderComponent(<PoolTable />);
   });
