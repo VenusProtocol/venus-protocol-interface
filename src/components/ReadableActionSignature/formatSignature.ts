8c636c49c5da4f059d9ebcc518f764429985bd8d
therealemjy <maxime.julian.creations@gmail.com>
2023-04-19 10:41:20 +0100

    update ReadableActionSignature to handle tuples

--- src/components/ReadableActionSignature/formatSignature.ts
+++ src/components/ReadableActionSignature/formatSignature.ts
@@ -1,32 +1,56 @@
 import type { Result } from '@ethersproject/abi';
+import BigNumber from 'bignumber.js';
 import { ethers } from 'ethers';
+import { ParamType } from 'ethers/lib/utils';
 import { ProposalAction } from 'types';
 
 import { FormValues } from 'pages/Governance/ProposalList/CreateProposalModal/proposalSchema';
 
+const formatParamType = (paramType: ParamType): string => {
+  if (paramType.type === 'tuple') {
+    return `tuple(${paramType.components.map(formatParamType).join(', ')})`;
+  }
+
+  return paramType.type;
+};
+
+const formatArgToReadableFormat = (argument: Result): string => {
+  if (Array.isArray(argument)) {
+    return `[${argument.map(formatArgToReadableFormat).join(', ')}]`;
+  }
+
+  if (typeof argument === 'string') {
+    return `"${argument}"`;
+  }
+
+  // eslint-disable-next-line no-underscore-dangle
+  if (argument._isBigNumber) {
+    // eslint-disable-next-line no-underscore-dangle
+    return new BigNumber(argument._hex).toString();
+  }
+
+  return argument.toString();
+};
+
 const formatSignature = (action: FormValues['actions'][number] | ProposalAction) => {
   try {
-    const fragment = ethers.utils.FunctionFragment.from(action.signature || '');
-    let args: Result = [];
+    const fragment = ethers.utils.FunctionFragment.from(action.signature);
+    let args: string[] = [];
 
     if (Array.isArray(action.callData)) {
-      args = fragment.inputs.map((i, idx) => {
-        if (i.baseType === 'string' || i.baseType === 'address') {
-          return `"${action.callData[idx]}"`;
-        }
-        return action.callData[idx];
-      });
+      args = fragment.inputs
+        .map((i, idx) => {
+          const res = action.callData[idx];
+          return i.baseType === 'string' || i.baseType === 'address' ? `"${res}"` : res;
+        })
+        .filter((item): item is string => !!item);
     } else {
       const unformattedArgs = ethers.utils.defaultAbiCoder.decode(
-        fragment.inputs.map(input => input.type),
+        fragment.inputs.map(formatParamType),
         action.callData,
       );
-      args = fragment.inputs.map((i, idx) => {
-        if (i.baseType === 'string' || i.baseType === 'address') {
-          return `"${unformattedArgs[idx]}"`;
-        }
-        return unformattedArgs[idx];
-      });
+
+      args = unformattedArgs.map(formatArgToReadableFormat);
     }
 
     return `.${fragment.name}(${args.join(', ')})`;
