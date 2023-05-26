e84e63ce8989879a52b88e52f92ffca59d3a398e
therealemjy <maxime.julian.creations@gmail.com>
2023-05-26 11:45:35 +0100

    display 7 assets on Pools page

--- src/pages/Pools/PoolTable/index.tsx
+++ src/pages/Pools/PoolTable/index.tsx
@@ -53,7 +53,7 @@ export const PoolTableUi: React.FC<PoolTableProps> = ({ pools, isFetchingPools }
         key: 'assets',
         label: t('pools.poolTable.columns.assets'),
         renderCell: ({ pool }) => (
-          <TokenGroup tokens={pool.assets.map(asset => asset.vToken.underlyingToken)} limit={4} />
+          <TokenGroup tokens={pool.assets.map(asset => asset.vToken.underlyingToken)} limit={7} />
         ),
       },
       {
