a76e007c0138895bdbc457df300527f586fdbbae
Gleiser Oliveira <52756735+gleiser-oliveira@users.noreply.github.com>
2023-06-02 07:49:05 -0600

    Added preview env var and API URL (develop/IL deployment) (#1064)
    feat: add preview env and URL

--- src/constants/dAppHosts.ts
+++ src/constants/dAppHosts.ts
@@ -1,5 +1,5 @@
 export const DAPP_HOSTS = {
   testnet: ['testnet-cf.venus.io', 'testnet.venus.io'],
-  'app-preview': ['app-preview-cf.venus.io', 'app-preview.venus.io'],
+  preview: ['app-preview-cf.venus.io', 'app-preview.venus.io'],
   mainnet: 'app.venus.io',
 };
