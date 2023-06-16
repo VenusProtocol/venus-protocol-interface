eabcb059cab816a6d0d79c6975db88306bba233e
Gleiser Oliveira <gleiser.dev@gmail.com>
2023-06-16 12:58:53 -0600

    fix: add disabled tokens in testnet

--- src/constants/disabledTokens.ts
+++ src/constants/disabledTokens.ts
@@ -1,6 +1,7 @@
+import config from 'config';
 import { Token } from 'types';
 
-import { MAINNET_TOKENS } from 'constants/tokens';
+import { MAINNET_TOKENS, TESTNET_TOKENS } from 'constants/tokens';
 
 export type Action = 'borrow' | 'repay' | 'supply' | 'withdraw';
 
@@ -9,7 +10,30 @@ interface DisabledToken {
   disabledActions: Action[];
 }
 
-export const DISABLED_TOKENS: DisabledToken[] = [
+export const DISABLED_TOKENS_TESTNET: DisabledToken[] = [
+  {
+    token: TESTNET_TOKENS.ust,
+    disabledActions: ['borrow', 'supply'],
+  },
+  {
+    token: TESTNET_TOKENS.luna,
+    disabledActions: ['borrow', 'supply'],
+  },
+  {
+    token: TESTNET_TOKENS.sxp,
+    disabledActions: ['borrow', 'supply'],
+  },
+  {
+    token: TESTNET_TOKENS.trxold,
+    disabledActions: ['borrow', 'supply'],
+  },
+  {
+    token: TESTNET_TOKENS.tusdold,
+    disabledActions: ['borrow', 'supply'],
+  },
+];
+
+export const DISABLED_TOKENS_MAINNET: DisabledToken[] = [
   {
     token: MAINNET_TOKENS.ust,
     disabledActions: ['borrow', 'supply'],
@@ -35,3 +59,7 @@ export const DISABLED_TOKENS: DisabledToken[] = [
     disabledActions: ['borrow'],
   },
 ];
+
+export const DISABLED_TOKENS = config.isOnTestnet
+  ? DISABLED_TOKENS_TESTNET
+  : DISABLED_TOKENS_MAINNET;
