4d02aa472607df7064c95ceb2d0a9f84758bd0c8
therealemjy <maxime.julian.creations@gmail.com>
2023-06-28 16:52:32 +0800

    add ProgressCircle component

---
+++ src/components/ProgressCircle/styles.ts
@@ -0,0 +1,21 @@
+import { css, useTheme } from '@mui/material';
+
+export const useStyles = () => {
+  const theme = useTheme();
+
+  return {
+    container: css`
+      width: ${theme.spacing(4)};
+      height: ${theme.spacing(4)};
+    `,
+    getCircle: ({ circumference, offset }: { circumference: number; offset: number }) => css`
+      transform: rotate(-90deg);
+      transform-origin: 50% 50%;
+      stroke-dasharray: ${circumference} ${circumference};
+      stroke-dashoffset: ${offset};
+      stroke-width: 2px;
+    `,
+  };
+};
+
+export default useStyles;
