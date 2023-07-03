78b28b991eca0de5029786a854b25f576b8a02b6
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2023-07-03 14:28:06 +0100

    Update amount formatting to handle negative values (#1141)

--- src/utilities/calculateDailyEarningsCents.spec.ts
+++ src/utilities/calculateDailyEarningsCents.spec.ts
@@ -5,7 +5,7 @@ import calculateDailyEarningsCents from './calculateDailyEarningsCents';
 describe('utilities/calculateDailyEarningsCentss', () => {
   test('calculates daily Earnings for a single asset', () => {
     expect(calculateDailyEarningsCents(new BigNumber('1924.21991227022443813375')).toString()).toBe(
-      '5.27183537608280667982',
+      '5.27183537608280667981',
     );
   });
 });
