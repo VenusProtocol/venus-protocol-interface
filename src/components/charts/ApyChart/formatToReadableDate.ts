d88a7025dd6fece7c5b9e3b1dafc464b6882b2a4
therealemjy <maxime.julian.creations@gmail.com>
2023-07-05 11:12:45 +0200

    add date to graph tooltips + add year to x axis

--- src/components/charts/ApyChart/formatToReadableDate.ts
+++ src/components/charts/ApyChart/formatToReadableDate.ts
@@ -1,6 +1,6 @@
 import formatDate from 'date-fns/format';
 
-const READABLE_DATE_FORMAT = 'MM.dd';
+const READABLE_DATE_FORMAT = 'MM.dd.yy';
 
 const formatToReadableDate = (timestampMs: number) =>
   formatDate(new Date(timestampMs), READABLE_DATE_FORMAT);
