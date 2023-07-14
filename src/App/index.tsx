4538869ad0c2ed6bc10a3743673c213f3fa4141b
therealemjy <maxime.julian.creations@gmail.com>
2023-07-14 19:19:07 +0200

    temporarily disable Sentry and PostHog

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -7,41 +7,35 @@ import { ToastContainer } from 'react-toastify';
 import 'assets/styles/App.scss';
 import { queryClient } from 'clients/api';
 import { Web3Wrapper } from 'clients/web3';
-import { AnalyticsProvider } from 'context/Analytics';
 import { AuthProvider } from 'context/AuthContext';
 import { DisableLunaUstWarningProvider } from 'context/DisableLunaUstWarning';
-import { ErrorLoggerProvider } from 'context/ErrorLogger';
 import { SuccessfulTransactionModalProvider } from 'context/SuccessfulTransactionModalContext';
 import { MuiThemeProvider } from 'theme/MuiThemeProvider';
 
 import Switch from './Switch';
 
 const App = () => (
-  <ErrorLoggerProvider>
-    <Web3Wrapper>
-      <QueryClientProvider client={queryClient}>
-        <MuiThemeProvider>
-          <AuthProvider>
-            <AnalyticsProvider>
-              <SuccessfulTransactionModalProvider>
-                <DisableLunaUstWarningProvider>
-                  <BrowserRouter>
-                    <ToastContainer />
+  <Web3Wrapper>
+    <QueryClientProvider client={queryClient}>
+      <MuiThemeProvider>
+        <AuthProvider>
+          <SuccessfulTransactionModalProvider>
+            <DisableLunaUstWarningProvider>
+              <BrowserRouter>
+                <ToastContainer />
 
-                    <Layout>
-                      <ResetScrollOnRouteChange />
+                <Layout>
+                  <ResetScrollOnRouteChange />
 
-                      <Switch />
-                    </Layout>
-                  </BrowserRouter>
-                </DisableLunaUstWarningProvider>
-              </SuccessfulTransactionModalProvider>
-            </AnalyticsProvider>
-          </AuthProvider>
-        </MuiThemeProvider>
-      </QueryClientProvider>
-    </Web3Wrapper>
-  </ErrorLoggerProvider>
+                  <Switch />
+                </Layout>
+              </BrowserRouter>
+            </DisableLunaUstWarningProvider>
+          </SuccessfulTransactionModalProvider>
+        </AuthProvider>
+      </MuiThemeProvider>
+    </QueryClientProvider>
+  </Web3Wrapper>
 );
 
 export default App;
