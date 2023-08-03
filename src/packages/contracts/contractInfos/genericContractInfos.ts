d56f71952fe41199e6f70da378dd1b546f7afa39
therealemjy <maxime.julian.creations@gmail.com>
2023-08-03 12:01:50 +0100

    refactor: remove unused ABIs + add contracts

--- src/packages/contracts/contractInfos/genericContractInfos.ts
+++ src/packages/contracts/contractInfos/genericContractInfos.ts
@@ -1,18 +1,18 @@
 import type { JsonFragment } from '@ethersproject/abi';
 import { abi as isolatedPoolComptrollerAbi } from '@venusprotocol/isolated-pools/artifacts/contracts/Comptroller.sol/Comptroller.json';
-import { abi as interestRateModelAbi } from '@venusprotocol/isolated-pools/artifacts/contracts/InterestRateModel.sol/InterestRateModel.json';
-import { abi as JumpRateModelV2Abi } from '@venusprotocol/isolated-pools/artifacts/contracts/JumpRateModelV2.sol/JumpRateModelV2.json';
+import { abi as jumpRateModelV2Abi } from '@venusprotocol/isolated-pools/artifacts/contracts/JumpRateModelV2.sol/JumpRateModelV2.json';
 import { abi as rewardsDistributorAbi } from '@venusprotocol/isolated-pools/artifacts/contracts/Rewards/RewardsDistributor.sol/RewardsDistributor.json';
 import { abi as vTokenAbi } from '@venusprotocol/isolated-pools/artifacts/contracts/VToken.sol/VToken.json';
+import { abi as jumpRateModelAbi } from '@venusprotocol/venus-protocol/artifacts/contracts/InterestRateModels/JumpRateModel.sol/JumpRateModel.json';
 
 import {
-  InterestRateModel,
   Comptroller as IsolatedPoolComptroller,
   JumpRateModelV2,
   RewardsDistributor,
   VToken,
 } from '../types/contracts/isolatedPools';
 import { Bep20, PancakePairV2 } from '../types/contracts/others';
+import { JumpRateModel } from '../types/contracts/venusProtocol';
 
 import bep20Abi from './externalAbis/bep20.json';
 import pancakePairV2Abi from './externalAbis/pancakePairV2.json';
@@ -25,12 +25,12 @@ const isolatedPoolComptroller: GenericContractInfo = {
   abi: isolatedPoolComptrollerAbi,
 };
 
-const interestRateModel: GenericContractInfo = {
-  abi: interestRateModelAbi,
+const jumpRateModel: GenericContractInfo = {
+  abi: jumpRateModelAbi,
 };
 
 const jumpRateModelV2: GenericContractInfo = {
-  abi: JumpRateModelV2Abi,
+  abi: jumpRateModelV2Abi,
 };
 
 const rewardsDistributor: GenericContractInfo = {
@@ -51,7 +51,7 @@ const pancakePairV2: GenericContractInfo = {
 
 const genericContractInfos = {
   isolatedPoolComptroller,
-  interestRateModel,
+  jumpRateModel,
   jumpRateModelV2,
   rewardsDistributor,
   vToken,
@@ -63,7 +63,7 @@ export type GenericContractName = keyof typeof genericContractInfos;
 
 export type GenericContractTypes = {
   isolatedPoolComptroller: IsolatedPoolComptroller;
-  interestRateModel: InterestRateModel;
+  jumpRateModel: JumpRateModel;
   jumpRateModelV2: JumpRateModelV2;
   rewardsDistributor: RewardsDistributor;
   vToken: VToken;
