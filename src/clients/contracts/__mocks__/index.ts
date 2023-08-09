c1d04b19452bea055055cb54c4fcfe4c23d045a1
therealemjy <maxime.julian.creations@gmail.com>
2023-08-09 19:44:17 +0200

    refactor: remove contracts client

--- src/clients/contracts/__mocks__/index.ts
+++
@@ -1,7 +0,0 @@
-export const getTokenContract = vi.fn();
-export const useTokenContract = () => getTokenContract;
-
-export const getVTokenContract = vi.fn();
-export const useVTokenContract = () => getVTokenContract;
-
-export const getMaximillionContract = vi.fn();
