801bab396ba011ca46f8d51042520b2cf8b4a7df
therealemjy <maxime.julian.creations@gmail.com>
2023-08-25 11:00:04 +0100

    feat: only display value changes on ValueUpdate component

--- src/components/ValueUpdate/index.stories.tsx
+++ src/components/ValueUpdate/index.stories.tsx
@@ -14,3 +14,5 @@ export default {
 export const Increase = () => <ValueUpdate original={200000} update={300000} />;
 
 export const Decrease = () => <ValueUpdate original={500000} update={300000} />;
+
+export const Same = () => <ValueUpdate original={500000} update={500000} />;
