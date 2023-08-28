38f156490dd000977197903fb25d83e2da90bb6b
therealemjy <maxime.julian.creations@gmail.com>
2023-08-28 10:04:32 +0100

    chore: upgrade to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import App from 'App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import 'assets/styles/index.scss';
 
@@ -8,4 +8,6 @@ import initializeLibraries from './initializeLibraries';
 
 initializeLibraries();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
