aad6e85fbd271007340b335303ff92c847e77572
Gleiser Oliveira <gleiser.dev@gmail.com>
2023-09-07 14:59:20 -0600

    feat: update markets/core-pool to new enpoint

--- src/clients/api/queries/getMainMarkets/index.ts
+++ src/clients/api/queries/getMainMarkets/index.ts
@@ -4,16 +4,15 @@ import { restService } from 'utilities';
 
 export interface ApiMarket {
   address: string;
-  totalDistributed: string;
+  totalDistributedMantissa: string;
+  underlyingDecimal: number;
   borrowerCount: number;
   supplierCount: number;
 }
 
 export interface GetMainMarketsResponse {
-  dailyVenus: number;
-  markets: ApiMarket[];
+  result: ApiMarket[];
   request: { addresses: string[] };
-  venusRate: string;
 }
 
 export interface GetMainMarketsOutput {
@@ -21,21 +20,31 @@ export interface GetMainMarketsOutput {
 }
 
 const getMainMarkets = async (): Promise<GetMainMarketsOutput> => {
-  const response = await restService<GetMainMarketsResponse, 'v1'>({
+  const response = await restService<GetMainMarketsResponse, 'v2'>({
     endpoint: '/markets/core-pool',
     method: 'GET',
+    next: true,
+    params: {
+      limit: 50,
+    },
   });
 
   if ('result' in response && response.result === 'error') {
     throw new Error(response.message);
   }
 
-  const markets: Market[] = (response?.data?.data.markets || []).map(apiMarket => ({
-    address: apiMarket.address,
-    borrowerCount: apiMarket.borrowerCount,
-    supplierCount: apiMarket.supplierCount,
-    totalXvsDistributedTokens: new BigNumber(apiMarket.totalDistributed),
-  }));
+  const markets: Market[] = (response?.data?.result || []).map(apiMarket => {
+    const decimalPlaces = new BigNumber(10).pow(apiMarket.underlyingDecimal);
+    const totalXvsDistributedTokens = new BigNumber(apiMarket.totalDistributedMantissa).dividedBy(
+      decimalPlaces,
+    );
+    return {
+      address: apiMarket.address,
+      borrowerCount: apiMarket.borrowerCount,
+      supplierCount: apiMarket.supplierCount,
+      totalXvsDistributedTokens,
+    };
+  });
 
   return { markets };
 };
