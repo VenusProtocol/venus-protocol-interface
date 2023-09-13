368d30ed3324e543c0ea4a602bb6656a7d9783a7
therealemjy <maxime.julian.creations@gmail.com>
2023-09-13 10:28:21 +0100

    feat: add mocks and index file to tokens package

--- src/packages/tokens/index.ts
+++ src/packages/tokens/index.ts
@@ -1,2 +1,3 @@
+export * from './tokenInfos';
 export * from './getTokens';
 export * from './getPancakeSwapTokens';
---
+++ src/packages/tokens/tokenInfos/index.ts
@@ -0,0 +1,5 @@
+export { default as commonTokens } from './common';
+export * from './common';
+
+export { default as pancakeSwapTokens } from './pancakeSwap';
+export * from './pancakeSwap';
