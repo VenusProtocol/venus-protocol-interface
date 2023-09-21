cfb59874e7bda10e1303f77af36a4ebf616e9a44
therealemjy <maxime.julian.creations@gmail.com>
2023-09-21 13:04:04 +0100

    feat: start adding elements to PrimeStatusBanner container

--- src/containers/PrimeStatusBanner/index.tsx
+++ src/containers/PrimeStatusBanner/index.tsx
@@ -1,26 +1,116 @@
 /** @jsxImportSource @emotion/react */
+import { Typography } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import BigNumber from 'bignumber.js';
-import React from 'react';
+import formatDistanceStrict from 'date-fns/formatDistanceStrict';
+import React, { useMemo } from 'react';
+import { useTranslation } from 'translation';
+import { Token } from 'types';
+
+import { ReactComponent as PrimeLogo } from 'assets/img/primeLogo.svg';
+import useFormatPercentageToReadableValue from 'hooks/useFormatPercentageToReadableValue';
+import useConvertWeiToReadableTokenString from 'hooks/useFormatTokensToReadableValue';
+import useGetToken from 'hooks/useGetToken';
 
 import { useStyles } from './styles';
 
 export interface PrimeStatusBannerUiProps {
   isUserPrime: boolean;
-  userStakedXvsTokens: BigNumber;
-  minXvsToStakeForEligibilityTokens: BigNumber;
-  highestHypotheticalPrimeApyBoostPercentage: BigNumber;
-  haveAllTokensBeenClaimed?: boolean;
-  secondsUntilEligible?: number;
+  xvs: Token;
+  isLoading: boolean;
+  userStakedXvsTokens?: BigNumber;
+  minXvsToStakeForPrimeTokens?: BigNumber;
+  highestHypotheticalPrimeApyBoostPercentage?: BigNumber;
+  haveAllPrimeTokensBeenClaimed?: boolean;
+  primeClaimWaitingPeriodSeconds?: number;
   className?: string;
 }
 
-export const PrimeStatusBannerUi: React.FC<PrimeStatusBannerUiProps> = ({ className }) => {
+export const PrimeStatusBannerUi: React.FC<PrimeStatusBannerUiProps> = ({
+  className,
+  isUserPrime,
+  isLoading,
+  xvs,
+  highestHypotheticalPrimeApyBoostPercentage,
+  primeClaimWaitingPeriodSeconds,
+  minXvsToStakeForPrimeTokens,
+  userStakedXvsTokens,
+}) => {
   const styles = useStyles();
+  const { Trans } = useTranslation();
+
+  const readableApyBoostPercentage = useFormatPercentageToReadableValue({
+    value: highestHypotheticalPrimeApyBoostPercentage,
+  });
+
+  const readableClaimWaitingPeriod = useMemo(
+    () =>
+      primeClaimWaitingPeriodSeconds
+        ? formatDistanceStrict(
+            new Date(),
+            new Date().getTime() + primeClaimWaitingPeriodSeconds * 1000,
+            { unit: 'day' },
+          )
+        : undefined,
+    [primeClaimWaitingPeriodSeconds],
+  );
+
+  const stakeDeltaTokens =
+    minXvsToStakeForPrimeTokens &&
+    userStakedXvsTokens &&
+    minXvsToStakeForPrimeTokens.minus(userStakedXvsTokens);
+
+  const readableStakeDeltaTokens = useConvertWeiToReadableTokenString({
+    value: stakeDeltaTokens,
+    token: xvs,
+  });
+
+  if (isLoading || isUserPrime) {
+    return null;
+  }
 
   return (
     <Paper css={styles.container} className={className}>
-      Test
+      <div css={styles.column}>
+        <div css={styles.header}>
+          <PrimeLogo css={styles.primeLogo} />
+
+          <div>
+            <Typography variant="h3" css={styles.title}>
+              <Trans
+                i18nKey="primeStatusBanner.title"
+                components={{
+                  GreenText: <span css={styles.greenText} />,
+                }}
+                values={{
+                  percentage: readableApyBoostPercentage,
+                }}
+              />
+            </Typography>
+
+            <Typography>
+              <Trans
+                i18nKey="primeStatusBanner.description"
+                components={{
+                  WhiteText: <span css={styles.whiteText} />,
+                  Link: (
+                    // eslint-disable-next-line jsx-a11y/anchor-has-content
+                    <a
+                      // TODO: add correct link
+                      href="https://google.com"
+                      rel="noreferrer"
+                    />
+                  ),
+                }}
+                values={{
+                  stakeDelta: readableStakeDeltaTokens,
+                  claimWaitingPeriod: readableClaimWaitingPeriod,
+                }}
+              />
+            </Typography>
+          </div>
+        </div>
+      </div>
     </Paper>
   );
 };
@@ -28,22 +118,29 @@ export const PrimeStatusBannerUi: React.FC<PrimeStatusBannerUiProps> = ({ classN
 export type PrimeStatusBannerProps = Pick<PrimeStatusBannerUiProps, 'className'>;
 
 const PrimeStatusBanner: React.FC<PrimeStatusBannerProps> = props => {
+  const xvs = useGetToken({
+    symbol: 'XVS',
+  });
+
   // TODO: fetch these values
+  const isLoading = false;
   const isUserPrime = false;
-  const secondsUntilEligible = 0;
-  const userStakedXvsTokens = new BigNumber('1000');
-  const minXvsToStakeForEligibilityTokens = new BigNumber('1000');
+  const primeClaimWaitingPeriodSeconds = 90 * 24 * 60 * 60; // 9 days in seconds
+  const userStakedXvsTokens = new BigNumber('100');
+  const minXvsToStakeForPrimeTokens = new BigNumber('1000');
   const highestHypotheticalPrimeApyBoostPercentage = new BigNumber('3.14');
-  const haveAllTokensBeenClaimed = false;
+  const haveAllPrimeTokensBeenClaimed = false;
 
   return (
     <PrimeStatusBannerUi
+      isLoading={isLoading}
       isUserPrime={isUserPrime}
-      secondsUntilEligible={secondsUntilEligible}
+      primeClaimWaitingPeriodSeconds={primeClaimWaitingPeriodSeconds}
+      xvs={xvs!}
       userStakedXvsTokens={userStakedXvsTokens}
-      minXvsToStakeForEligibilityTokens={minXvsToStakeForEligibilityTokens}
+      minXvsToStakeForPrimeTokens={minXvsToStakeForPrimeTokens}
       highestHypotheticalPrimeApyBoostPercentage={highestHypotheticalPrimeApyBoostPercentage}
-      haveAllTokensBeenClaimed={haveAllTokensBeenClaimed}
+      haveAllPrimeTokensBeenClaimed={haveAllPrimeTokensBeenClaimed}
       {...props}
     />
   );
--- src/containers/PrimeStatusBanner/styles.ts
+++ src/containers/PrimeStatusBanner/styles.ts
@@ -9,5 +9,27 @@ export const useStyles = () => {
       display: flex;
       justify-content: space-between;
     `,
+    column: css`
+      :not(:last-child) {
+        margin-right: ${theme.spacing(6)};
+      }
+    `,
+    header: css`
+      display: flex;
+    `,
+    primeLogo: css`
+      display: block;
+      width: ${theme.spacing(10)};
+      margin-right: ${theme.spacing(4)};
+    `,
+    title: css`
+      margin-bottom: ${theme.spacing(2)};
+    `,
+    greenText: css`
+      color: ${theme.palette.interactive.success};
+    `,
+    whiteText: css`
+      color: ${theme.palette.text.primary};
+    `,
   };
 };
