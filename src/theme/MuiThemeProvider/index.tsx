34b4382890f3b9d4e9563ca31204a29af539c44b
therealemjy <maxime.julian.creations@gmail.com>
2023-09-26 18:39:14 +0200

    feat: add support for tailwind.css

--- src/theme/MuiThemeProvider/index.tsx
+++ src/theme/MuiThemeProvider/index.tsx
@@ -2,8 +2,6 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import React, { ReactNode } from 'react';
 
-import 'assets/styles/normalize.scss';
-
 import mainTheme from './muiTheme';
 
 export function MuiThemeProvider({ children }: { children: ReactNode }) {
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
