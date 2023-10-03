4108047a5d2f7116773d432631c0438b975a01c6
Gleiser Oliveira <gleiser.dev@gmail.com>
2023-10-03 11:26:46 -0600

    feat: use next auth endpoint

--- src/pages/Dashboard/ConnectWalletBanner/index.tsx
+++ src/pages/Dashboard/ConnectWalletBanner/index.tsx
@@ -31,17 +31,17 @@ export const ConnectWalletBannerUi: React.FC<ConnectWalletBannerUiProps> = ({
       <div className="sm:flex-4 px-4 py-6 text-center sm:px-6 sm:text-left">
         <p className="mb-3 text-lg">{t('dashboard.connectWalletBanner.title')}</p>
 
-        <p className="text-grey mb-8">{t('dashboard.connectWalletBanner.description')}</p>
+        <p className="mb-8 text-grey">{t('dashboard.connectWalletBanner.description')}</p>
 
         <PrimaryButton className="w-full sm:w-auto" onClick={openAuthModal}>
           {t('dashboard.connectWalletBanner.buttonLabel')}
         </PrimaryButton>
       </div>
 
-      <div className="h-50 sm:flex-4 lg:flex-5 sm:bg-transparent relative -order-1 overflow-hidden bg-lightGrey sm:order-none sm:h-full sm:overflow-visible">
+      <div className="sm:flex-4 lg:flex-5 relative -order-1 h-50 overflow-hidden bg-lightGrey sm:order-none sm:h-full sm:overflow-visible sm:bg-transparent">
         <img
           src={illustration}
-          className="w-84 -mr-46 sm:-right-26 xl:w-100 xl:-mr-50 absolute -top-7 right-[50%] max-w-none sm:-top-3 sm:mr-0 md:-right-10 lg:-right-6 xl:-top-10 xl:right-[50%]"
+          className="absolute -top-7 right-[50%] -mr-46 w-84 max-w-none sm:-right-26 sm:-top-3 sm:mr-0 md:-right-10 lg:-right-6 xl:-top-10 xl:right-[50%] xl:-mr-50 xl:w-100"
           alt={t('dashboard.connectWalletBanner.illustration.alt')}
         />
       </div>
