4f1a344b3fba197d31244e738c47d9522fd2569d
therealemjy <maxime.julian.creations@gmail.com>
2023-10-09 20:09:34 +0900

    refactor: update Layout to use Tailwind

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,28 +1,19 @@
-/** @jsxImportSource @emotion/react */
-import Box from '@mui/material/Box';
-import React from 'react';
-
 import Header from './Header';
 import { PageContainer } from './PageContainer';
 import Sidebar from './Sidebar';
-import { useStyles } from './styles';
 
 export interface LayoutProps {
   children?: React.ReactNode;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const styles = useStyles();
+export const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <div className="flex h-screen flex-col lg:flex-row">
+    <Sidebar />
 
-  return (
-    <div css={styles.layout}>
-      <Sidebar />
+    <div className="flex flex-1 flex-col">
+      <Header />
 
-      <Box display="flex" flexDirection="column" flex="1">
-        <Header />
-
-        <PageContainer>{children}</PageContainer>
-      </Box>
+      <PageContainer>{children}</PageContainer>
     </div>
-  );
-};
+  </div>
+);
