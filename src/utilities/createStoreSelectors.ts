62bb00ad200d709712b38f21daa5aed8e12ea630
therealemjy <maxime.julian.creations@gmail.com>
2023-10-09 11:26:37 +0900

    feat: handle closing banner

---
+++ src/utilities/createStoreSelectors.ts
@@ -0,0 +1,16 @@
+import { StoreApi, UseBoundStore } from 'zustand';
+
+type WithSelectors<S> = S extends { getState: () => infer T }
+  ? S & { use: { [K in keyof T]: () => T[K] } }
+  : never;
+
+export const createStoreSelectors = <S extends UseBoundStore<StoreApi<object>>>(_store: S) => {
+  const store = _store as WithSelectors<typeof _store>;
+  store.use = {};
+
+  Object.keys(store.getState()).forEach(k => {
+    (store.use as any)[k] = () => store(s => s[k as keyof typeof s]);
+  });
+
+  return store;
+};
