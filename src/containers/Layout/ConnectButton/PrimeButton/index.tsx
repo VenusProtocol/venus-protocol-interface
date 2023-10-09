b67122ce13d12fa246e4a1abb1196d75384d2924
therealemjy <maxime.julian.creations@gmail.com>
2023-10-09 11:53:18 +0900

    refactor: split Prime button from Connect button

---
+++ src/containers/Layout/ConnectButton/PrimeButton/index.tsx
@@ -0,0 +1,35 @@
+/** @jsxImportSource @emotion/react */
+import { ButtonProps, SecondaryButton } from 'components';
+import React from 'react';
+import { useTranslation } from 'translation';
+import { cn, truncateAddress } from 'utilities';
+
+import primeLogoSrc from 'assets/img/primeLogo.svg';
+
+export interface PrimeButtonProps extends ButtonProps {
+  accountAddress: string;
+}
+
+export const PrimeButton: React.FC<PrimeButtonProps> = ({
+  accountAddress,
+  className,
+  ...otherProps
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <SecondaryButton
+      className={cn(
+        'relative border-transparent bg-background bg-clip-padding before:absolute before:inset-0 before:-z-[1] before:-m-[1px] before:rounded-lg before:bg-gradient-to-r before:from-[#805c4e] before:to-[#e3cdc3] before:content-[""] hover:border-transparent hover:bg-cards active:border-transparent active:bg-cards',
+        className,
+      )}
+      {...otherProps}
+    >
+      <>
+        <img className="mr-2 w-5" src={primeLogoSrc} alt={t('PrimeButton.primeLogoAlt')} />
+
+        {truncateAddress(accountAddress)}
+      </>
+    </SecondaryButton>
+  );
+};
--- src/containers/Layout/ConnectButton/index.tsx
+++ src/containers/Layout/ConnectButton/index.tsx
@@ -1,50 +1,38 @@
-/** @jsxImportSource @emotion/react */
 import { Button, ButtonProps } from 'components';
 import React from 'react';
 import { useTranslation } from 'translation';
-import { cn, isFeatureEnabled, truncateAddress } from 'utilities';
+import { isFeatureEnabled, truncateAddress } from 'utilities';
 
-import primeLogoSrc from 'assets/img/primeLogo.svg';
 import { useAuth } from 'context/AuthContext';
 
-export interface ConnectButtonProps extends ButtonProps {
+import { PrimeButton } from './PrimeButton';
+
+export interface ConnectButtonUiProps extends ButtonProps {
   isPrime: boolean;
   accountAddress?: string;
 }
 
-export const ConnectButtonUi: React.FC<ConnectButtonProps> = ({
+export const ConnectButtonUi: React.FC<ConnectButtonUiProps> = ({
   accountAddress,
   isPrime,
-  className,
   ...otherProps
 }) => {
   const { t } = useTranslation();
 
+  if (accountAddress && isPrime) {
+    return <PrimeButton accountAddress={accountAddress} {...otherProps} />;
+  }
+
   return (
-    <Button
-      variant={accountAddress ? 'secondary' : 'primary'}
-      className={cn(
-        isPrime &&
-          'relative border-transparent bg-background bg-clip-padding before:absolute before:inset-0 before:-z-[1] before:-m-[1px] before:rounded-lg before:bg-gradient-to-r before:from-[#805c4e] before:to-[#e3cdc3] before:content-[""] hover:border-transparent hover:bg-cards active:border-transparent active:bg-cards',
-        className,
-      )}
-      {...otherProps}
-    >
-      {accountAddress ? (
-        <>
-          {isPrime && (
-            <img className="mr-2 w-5" src={primeLogoSrc} alt={t('connectButton.primeLogoAlt')} />
-          )}
-          {truncateAddress(accountAddress)}
-        </>
-      ) : (
-        t('connectButton.title')
-      )}
+    <Button variant={accountAddress ? 'secondary' : 'primary'} {...otherProps}>
+      {accountAddress ? <>{truncateAddress(accountAddress)}</> : t('connectButton.title')}
     </Button>
   );
 };
 
-export const ConnectButton: React.FC<ButtonProps> = props => {
+export const ConnectButton: React.FC<
+  Omit<ConnectButtonUiProps, 'isPrime' | 'accountAddress'>
+> = props => {
   const { accountAddress, openAuthModal, isPrime } = useAuth();
 
   return (
