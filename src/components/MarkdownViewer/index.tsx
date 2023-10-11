f5a9de67a8533a55f2482b22d4254ff621cb261b
therealemjy <maxime.julian.creations@gmail.com>
2023-10-11 18:10:14 +0900

    feat: style MarkdownViewer component

--- src/components/MarkdownViewer/index.tsx
+++ src/components/MarkdownViewer/index.tsx
@@ -1,17 +1,49 @@
 import Markdown from 'react-markdown';
 import { cn } from 'utilities';
 
+import { Link } from '../Link';
+
 export interface MarkdownViewerProps {
   className?: string;
   content: string;
 }
 
+const TITLE_CLASSES = cn('mb-2 mt-4 font-semibold text-grey first:mt-0');
+
 export const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ className, content }) => (
-  // TODO: add style and options
   <Markdown
-    className={cn('flex-1', className)}
+    className={className}
     allowedElements={['h1', 'h2', 'h3', 'h4', 'p', 'a', 'ul', 'li', 'strong', 'em']}
     skipHtml
+    components={{
+      h1: ({ children, ...otherProps }) => (
+        <h1 {...otherProps} className={TITLE_CLASSES}>
+          {children}
+        </h1>
+      ),
+      h2: ({ children, ...otherProps }) => (
+        <h2 {...otherProps} className={TITLE_CLASSES}>
+          {children}
+        </h2>
+      ),
+      h4: ({ children, ...otherProps }) => (
+        <h4 {...otherProps} className={TITLE_CLASSES}>
+          {children}
+        </h4>
+      ),
+      p: ({ children, ...otherProps }) => (
+        <p {...otherProps} className="mb-4">
+          {children}
+        </p>
+      ),
+      a: ({ children, ...otherProps }) => <Link {...otherProps}>{children}</Link>,
+      ul: ({ children, ...otherProps }) => (
+        <ul {...otherProps} className="mb-4 pl-6">
+          {children}
+        </ul>
+      ),
+      li: ({ children, ...otherProps }) => <li {...otherProps}>{children}</li>,
+    }}
   >
     {content}
   </Markdown>
