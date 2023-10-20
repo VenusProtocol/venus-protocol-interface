b81a403f6c0b3e34dfcb025889d0365a460a2c67
therealemjy <maxime.julian.creations@gmail.com>
2023-10-20 15:57:13 +0900

    refactor: rewrite Spinner with Tailwind + fix SpendingLimit component styling

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,9 +1,8 @@
-/** @jsxImportSource @emotion/react */
 import React from 'react';
 import { useTranslation } from 'translation';
+import { cn } from 'utilities';
 
 import spinnerAnimation from './spinnerAnimation.gif';
-import { useStyles } from './styles';
 import TEST_IDS from './testIds';
 
 interface SpinnerProps {
@@ -12,12 +11,22 @@ interface SpinnerProps {
 }
 
 export const Spinner: React.FC<SpinnerProps> = ({ variant = 'large', className }) => {
-  const styles = useStyles({ variant });
   const { t } = useTranslation();
 
   return (
-    <div css={styles.container} className={className} data-testid={TEST_IDS.spinner}>
-      <img css={styles.spinner} src={spinnerAnimation} alt={t('spinner.altText')} />
+    <div
+      className={cn(
+        'flex items-center justify-center',
+        variant === 'large' && 'h-full w-full',
+        className,
+      )}
+      data-testid={TEST_IDS.spinner}
+    >
+      <img
+        className={cn(variant === 'large' ? 'h-7 w-7' : 'h-5 w-5')}
+        src={spinnerAnimation}
+        alt={t('spinner.altText')}
+      />
     </div>
   );
 };
