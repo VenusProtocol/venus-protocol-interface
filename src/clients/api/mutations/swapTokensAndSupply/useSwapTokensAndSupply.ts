22a99c44f43c4125817712929529b92813b28750
therealemjy <maxime.julian.creations@gmail.com>
2023-10-26 12:29:57 +0100

    refactor: rewrite swapTokensAndSupply with useSendTransaction

--- src/clients/api/mutations/swapTokensAndSupply/useSwapTokensAndSupply.ts
+++ src/clients/api/mutations/swapTokensAndSupply/useSwapTokensAndSupply.ts
@@ -1,27 +1,18 @@
 import { useAnalytics } from 'packages/analytics';
 import { useGetSwapRouterContract } from 'packages/contracts';
-import { MutationObserverOptions, useMutation } from 'react-query';
 import { VToken } from 'types';
 import { callOrThrow, convertWeiToTokens } from 'utilities';
 
-import {
-  SwapTokensAndSupplyInput,
-  SwapTokensAndSupplyOutput,
-  queryClient,
-  swapTokensAndSupply,
-} from 'clients/api';
+import { SwapTokensAndSupplyInput, queryClient, swapTokensAndSupply } from 'clients/api';
 import FunctionKey from 'constants/functionKey';
 import { SLIPPAGE_TOLERANCE_PERCENTAGE } from 'constants/swap';
+import { UseSendTransactionOptions, useSendTransaction } from 'hooks/useSendTransaction';
 
 type TrimmedSwapTokensAndSupplyInput = Omit<
   SwapTokensAndSupplyInput,
   'swapRouterContract' | 'vToken'
 >;
-type Options = MutationObserverOptions<
-  SwapTokensAndSupplyOutput,
-  Error,
-  TrimmedSwapTokensAndSupplyInput
->;
+type Options = UseSendTransactionOptions<TrimmedSwapTokensAndSupplyInput>;
 
 const useSwapTokensAndSupply = (
   {
@@ -37,9 +28,9 @@ const useSwapTokensAndSupply = (
   });
   const { captureAnalyticEvent } = useAnalytics();
 
-  return useMutation(
-    FunctionKey.SWAP_TOKENS_AND_SUPPLY,
-    (input: TrimmedSwapTokensAndSupplyInput) =>
+  return useSendTransaction({
+    fnKey: FunctionKey.SWAP_TOKENS_AND_SUPPLY,
+    fn: (input: TrimmedSwapTokensAndSupplyInput) =>
       callOrThrow({ swapRouterContract }, params =>
         swapTokensAndSupply({
           vToken,
@@ -47,79 +38,71 @@ const useSwapTokensAndSupply = (
           ...params,
         }),
       ),
-    {
-      ...options,
-      onSuccess: async (...onSuccessParams) => {
-        const { swap } = onSuccessParams[1];
+    onConfirmed: async ({ input }) => {
+      captureAnalyticEvent('Tokens swapped and supplied', {
+        poolName,
+        fromTokenSymbol: input.swap.fromToken.symbol,
+        fromTokenAmountTokens: convertWeiToTokens({
+          token: input.swap.fromToken,
+          valueWei:
+            input.swap.direction === 'exactAmountIn'
+              ? input.swap.fromTokenAmountSoldWei
+              : input.swap.expectedFromTokenAmountSoldWei,
+        }).toNumber(),
+        toTokenSymbol: input.swap.toToken.symbol,
+        toTokenAmountTokens: convertWeiToTokens({
+          token: input.swap.toToken,
+          valueWei:
+            input.swap.direction === 'exactAmountIn'
+              ? input.swap.expectedToTokenAmountReceivedWei
+              : input.swap.toTokenAmountReceivedWei,
+        }).toNumber(),
+        priceImpactPercentage: input.swap.priceImpactPercentage,
+        slippageTolerancePercentage: SLIPPAGE_TOLERANCE_PERCENTAGE,
+        exchangeRate: input.swap.exchangeRate.toNumber(),
+        routePath: input.swap.routePath,
+      });
 
-        captureAnalyticEvent('Tokens swapped and supplied', {
-          poolName,
-          fromTokenSymbol: swap.fromToken.symbol,
-          fromTokenAmountTokens: convertWeiToTokens({
-            token: swap.fromToken,
-            valueWei:
-              swap.direction === 'exactAmountIn'
-                ? swap.fromTokenAmountSoldWei
-                : swap.expectedFromTokenAmountSoldWei,
-          }).toNumber(),
-          toTokenSymbol: swap.toToken.symbol,
-          toTokenAmountTokens: convertWeiToTokens({
-            token: swap.toToken,
-            valueWei:
-              swap.direction === 'exactAmountIn'
-                ? swap.expectedToTokenAmountReceivedWei
-                : swap.toTokenAmountReceivedWei,
-          }).toNumber(),
-          priceImpactPercentage: swap.priceImpactPercentage,
-          slippageTolerancePercentage: SLIPPAGE_TOLERANCE_PERCENTAGE,
-          exchangeRate: swap.exchangeRate.toNumber(),
-          routePath: swap.routePath,
-        });
+      const accountAddress = await swapRouterContract?.signer.getAddress();
 
-        const accountAddress = await swapRouterContract?.signer.getAddress();
+      queryClient.invalidateQueries([
+        FunctionKey.GET_BALANCE_OF,
+        {
+          accountAddress,
+          tokenAddress: input.swap.fromToken.address,
+        },
+      ]);
 
-        queryClient.invalidateQueries([
-          FunctionKey.GET_BALANCE_OF,
-          {
-            accountAddress,
-            tokenAddress: swap.fromToken.address,
-          },
-        ]);
+      queryClient.invalidateQueries([
+        FunctionKey.GET_TOKEN_ALLOWANCE,
+        {
+          tokenAddress: input.swap.fromToken.address,
+          accountAddress,
+          spenderAddress: swapRouterContract?.address,
+        },
+      ]);
 
-        queryClient.invalidateQueries([
-          FunctionKey.GET_TOKEN_ALLOWANCE,
-          {
-            tokenAddress: swap.fromToken.address,
-            accountAddress,
-            spenderAddress: swapRouterContract?.address,
-          },
-        ]);
+      queryClient.invalidateQueries([
+        FunctionKey.GET_BALANCE_OF,
+        {
+          accountAddress,
+          tokenAddress: input.swap.toToken.address,
+        },
+      ]);
 
-        queryClient.invalidateQueries([
-          FunctionKey.GET_BALANCE_OF,
-          {
-            accountAddress,
-            tokenAddress: swap.toToken.address,
-          },
-        ]);
+      queryClient.invalidateQueries([
+        FunctionKey.GET_TOKEN_BALANCES,
+        {
+          accountAddress,
+        },
+      ]);
 
-        queryClient.invalidateQueries([
-          FunctionKey.GET_TOKEN_BALANCES,
-          {
-            accountAddress,
-          },
-        ]);
-
-        queryClient.invalidateQueries(FunctionKey.GET_V_TOKEN_BALANCES_ALL);
-        queryClient.invalidateQueries(FunctionKey.GET_MAIN_MARKETS);
-        queryClient.invalidateQueries(FunctionKey.GET_ISOLATED_POOLS);
-
-        if (options?.onSuccess) {
-          options.onSuccess(...onSuccessParams);
-        }
-      },
+      queryClient.invalidateQueries(FunctionKey.GET_V_TOKEN_BALANCES_ALL);
+      queryClient.invalidateQueries(FunctionKey.GET_MAIN_MARKETS);
+      queryClient.invalidateQueries(FunctionKey.GET_ISOLATED_POOLS);
     },
-  );
+    options,
+  });
 };
 
 export default useSwapTokensAndSupply;
