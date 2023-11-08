9d568f23a72a1501453feea28e08016f8dd5cfba
therealemjy <maxime.julian.creations@gmail.com>
2023-11-08 09:05:22 +0000

    refactor: update setVoteDelegate mutation to use new transaction flow

--- src/clients/api/mutations/setVoteDelegate/index.ts
+++ src/clients/api/mutations/setVoteDelegate/index.ts
@@ -1,4 +1,3 @@
-import { checkForXvsVaultProxyTransactionError } from 'errors';
 import { ContractReceipt } from 'ethers';
 import { XvsVault } from 'packages/contracts';
 
@@ -9,11 +8,7 @@ export interface SetVoteDelegateInput {
 
 export type SetVoteDelegateOutput = ContractReceipt;
 
-const setVoteDelegate = async ({ xvsVaultContract, delegateAddress }: SetVoteDelegateInput) => {
-  const transaction = await xvsVaultContract.delegate(delegateAddress);
-  const receipt = await transaction.wait(1);
-  // TODO: remove check once this function has been refactored to use useSendTransaction hook
-  return checkForXvsVaultProxyTransactionError(receipt);
-};
+const setVoteDelegate = async ({ xvsVaultContract, delegateAddress }: SetVoteDelegateInput) =>
+  xvsVaultContract.delegate(delegateAddress);
 
 export default setVoteDelegate;
