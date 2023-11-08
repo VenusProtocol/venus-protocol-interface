691eefab9d3df8a88b6c23b210f3de98c9865030
therealemjy <maxime.julian.creations@gmail.com>
2023-11-08 08:57:16 +0000

    refactor: update requestWithdrawalFromXvsVault mutation to use new transaction flow

--- src/clients/api/mutations/requestWithdrawalFromXvsVault/useRequestWithdrawalFromXvsVault.ts
+++ src/clients/api/mutations/requestWithdrawalFromXvsVault/useRequestWithdrawalFromXvsVault.ts
@@ -1,26 +1,21 @@
 import { useAnalytics } from 'packages/analytics';
 import { useGetXvsVaultContract } from 'packages/contracts';
 import { useGetToken } from 'packages/tokens';
-import { MutationObserverOptions, useMutation } from 'react-query';
 import { callOrThrow, convertWeiToTokens } from 'utilities';
 
 import {
   RequestWithdrawalFromXvsVaultInput,
-  RequestWithdrawalFromXvsVaultOutput,
   queryClient,
   requestWithdrawalFromXvsVault,
 } from 'clients/api';
 import FunctionKey from 'constants/functionKey';
+import { UseSendTransactionOptions, useSendTransaction } from 'hooks/useSendTransaction';
 
 type TrimmedRequestWithdrawalFromXvsVaultInput = Omit<
   RequestWithdrawalFromXvsVaultInput,
   'xvsVaultContract'
 >;
-type Options = MutationObserverOptions<
-  RequestWithdrawalFromXvsVaultOutput,
-  Error,
-  TrimmedRequestWithdrawalFromXvsVaultInput
->;
+type Options = UseSendTransactionOptions<TrimmedRequestWithdrawalFromXvsVaultInput>;
 
 const useRequestWithdrawalFromXvsVault = (options?: Options) => {
   const xvsVaultContract = useGetXvsVaultContract({
@@ -33,51 +28,45 @@ const useRequestWithdrawalFromXvsVault = (options?: Options) => {
 
   const { captureAnalyticEvent } = useAnalytics();
 
-  return useMutation(
-    FunctionKey.REQUEST_WITHDRAWAL_FROM_XVS_VAULT,
-    (input: TrimmedRequestWithdrawalFromXvsVaultInput) =>
+  return useSendTransaction({
+    fnKey: FunctionKey.REQUEST_WITHDRAWAL_FROM_XVS_VAULT,
+    fn: (input: TrimmedRequestWithdrawalFromXvsVaultInput) =>
       callOrThrow({ xvsVaultContract }, params =>
         requestWithdrawalFromXvsVault({
           ...params,
           ...input,
         }),
       ),
-    {
-      ...options,
-      onSuccess: async (...onSuccessParams) => {
-        const { poolIndex, amountWei } = onSuccessParams[1];
+    onConfirmed: async ({ input }) => {
+      const { poolIndex, amountWei } = input;
 
-        if (xvs) {
-          captureAnalyticEvent('Token withdrawal requested from XVS vault', {
-            poolIndex,
-            rewardTokenSymbol: xvs.symbol,
-            tokenAmountTokens: convertWeiToTokens({
-              token: xvs,
-              valueWei: amountWei,
-            }).toNumber(),
-          });
+      if (xvs) {
+        captureAnalyticEvent('Token withdrawal requested from XVS vault', {
+          poolIndex,
+          rewardTokenSymbol: xvs.symbol,
+          tokenAmountTokens: convertWeiToTokens({
+            token: xvs,
+            valueWei: amountWei,
+          }).toNumber(),
+        });
 
-          const accountAddress = await xvsVaultContract?.signer.getAddress();
+        const accountAddress = await xvsVaultContract?.signer.getAddress();
 
-          // Invalidate cached user info
-          queryClient.invalidateQueries([
-            FunctionKey.GET_XVS_VAULT_USER_INFO,
-            { accountAddress, rewardTokenAddress: xvs.address, poolIndex },
-          ]);
+        // Invalidate cached user info
+        queryClient.invalidateQueries([
+          FunctionKey.GET_XVS_VAULT_USER_INFO,
+          { accountAddress, rewardTokenAddress: xvs.address, poolIndex },
+        ]);
 
-          // Invalidate cached user withdrawal requests
-          queryClient.invalidateQueries([
-            FunctionKey.GET_XVS_VAULT_WITHDRAWAL_REQUESTS,
-            { accountAddress, rewardTokenAddress: xvs.address, poolIndex },
-          ]);
-        }
-
-        if (options?.onSuccess) {
-          options.onSuccess(...onSuccessParams);
-        }
-      },
+        // Invalidate cached user withdrawal requests
+        queryClient.invalidateQueries([
+          FunctionKey.GET_XVS_VAULT_WITHDRAWAL_REQUESTS,
+          { accountAddress, rewardTokenAddress: xvs.address, poolIndex },
+        ]);
+      }
     },
-  );
+    options,
+  });
 };
 
 export default useRequestWithdrawalFromXvsVault;
