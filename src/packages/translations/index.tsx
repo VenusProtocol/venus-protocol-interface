70f9cdc95ed611a1290d232ec3a24579117a4dee
therealemjy <maxime.julian.creations@gmail.com>
2023-11-15 09:28:05 +0000

    fix: typing issue

--- src/packages/translations/index.tsx
+++ src/packages/translations/index.tsx
@@ -1,6 +1,6 @@
 import { format as formatDate, formatDistanceToNowStrict, isDate } from 'date-fns';
 import { enUS } from 'date-fns/locale';
-import i18next, { TFunctionKeys } from 'i18next';
+import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import EnLocales from './translations/en.json';
@@ -42,7 +42,6 @@ const i18NextInstance = init();
 // Only use this function when you need to render a string from outside a
 // component. Otherwise, use the t function or Trans component returned by the
 // useTranslation hook.
-export const t = (params: TFunctionKeys, values?: Record<string, unknown>) =>
-  i18NextInstance.t(params, values);
+export const { t } = i18NextInstance;
 
 export * from './useTranslation';
