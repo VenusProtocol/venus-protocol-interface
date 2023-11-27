091227dae9c92d084101b97bc44b648d3256eefb
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2023-11-27 11:28:06 +0000

    chore: clean up stories (#1856)

--- src/components/Card/index.stories.tsx
+++ src/components/Card/index.stories.tsx
@@ -1,14 +1,13 @@
 import { Meta } from '@storybook/react';
-import React from 'react';
 
-import { withCenterStory, withRouter } from 'stories/decorators';
+import { withCenterStory } from 'stories/decorators';
 
 import { Card } from '.';
 
 export default {
   title: 'Components/Card',
   component: Card,
-  decorators: [withRouter, withCenterStory({ width: '100%' })],
+  decorators: [withCenterStory({ width: '100%' })],
 } as Meta<typeof Card>;
 
 export const Default = () => <Card>Some content</Card>;
