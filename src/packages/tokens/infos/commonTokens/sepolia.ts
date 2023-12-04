7b3bb8157243b833d64542e8352ab57b102093ac
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2023-12-04 11:19:02 +0000

    feat: add token records for the Sepolia network (#1896)

---
+++ src/packages/tokens/infos/commonTokens/sepolia.ts
@@ -0,0 +1,52 @@
+import crvLogo from 'packages/tokens/img/crv.svg';
+import usdcLogo from 'packages/tokens/img/usdc.svg';
+import usdtLogo from 'packages/tokens/img/usdt.svg';
+import wbtcLogo from 'packages/tokens/img/wbtc.svg';
+import wethLogo from 'packages/tokens/img/weth.svg';
+import xvsLogo from 'packages/tokens/img/xvs.svg';
+import { Token } from 'types';
+
+export const tokens: Token[] = [
+  {
+    address: '0x92A2928f5634BEa89A195e7BeCF0f0FEEDAB885b',
+    decimals: 8,
+    symbol: 'WBTC',
+    asset: wbtcLogo,
+  },
+  {
+    address: '0x700868CAbb60e90d77B6588ce072d9859ec8E281',
+    decimals: 18,
+    symbol: 'WETH',
+    asset: wethLogo,
+  },
+  {
+    address: '0x8d412FD0bc5d826615065B931171Eed10F5AF266',
+    decimals: 6,
+    symbol: 'USDT',
+    asset: usdtLogo,
+  },
+  {
+    address: '0x772d68929655ce7234C8C94256526ddA66Ef641E',
+    decimals: 6,
+    symbol: 'USDC',
+    asset: usdcLogo,
+  },
+  {
+    address: '0x2c78EF7eab67A6e0C9cAa6f2821929351bdDF3d3',
+    decimals: 18,
+    symbol: 'CRV',
+    asset: crvLogo,
+  },
+  {
+    address: '0x36421d873abCa3E2bE6BB3c819C0CF26374F63b6',
+    decimals: 18,
+    symbol: 'crvUSD',
+    asset: crvLogo, // TODO: add correct icon
+  },
+  {
+    address: '0xdb633c11d3f9e6b8d17ac2c972c9e3b05da59bf9',
+    decimals: 18,
+    symbol: 'XVS',
+    asset: xvsLogo,
+  },
+];
