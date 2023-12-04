6353ee8843c5e705e6cd60a3dfb8a9174b94cf23
therealemjy <maxime.julian.creations@gmail.com>
2023-12-04 11:07:03 +0000

    feat: add Sepolia tokens

--- src/packages/tokens/infos/commonTokens/index.ts
+++ src/packages/tokens/infos/commonTokens/index.ts
@@ -3,10 +3,12 @@ import { ChainId } from 'types';
 import { TokenMapping } from '../../types';
 import { tokens as bscMainnetTokens } from './bscMainnet';
 import { tokens as bscTestnetTokens } from './bscTestnet';
+import { tokens as ethereumTokens } from './ethereum';
+import { tokens as sepoliaTokens } from './sepolia';
 
 export const tokens: TokenMapping = {
   [ChainId.BSC_MAINNET]: bscMainnetTokens,
   [ChainId.BSC_TESTNET]: bscTestnetTokens,
-  [ChainId.SEPOLIA]: [], // TODO: add records (see VEN-2121)
-  [ChainId.ETHEREUM]: [], // TODO: add records (see VEN-2120)
+  [ChainId.SEPOLIA]: sepoliaTokens,
+  [ChainId.ETHEREUM]: ethereumTokens,
 };
