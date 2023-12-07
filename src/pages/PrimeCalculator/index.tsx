0aac84e893f819be4a2adcbd430b404c12ce4c7c
therealemjy <maxime.julian.creations@gmail.com>
2023-12-07 15:27:52 +0100

    feat: add top notice

--- src/pages/PrimeCalculator/index.tsx
+++ src/pages/PrimeCalculator/index.tsx
@@ -1,9 +1,15 @@
+import { NoticeInfo } from 'components';
+import { PRIME_DOC_URL } from 'constants/prime';
+import { Link } from 'containers/Link';
 import { useGetToken } from 'packages/tokens';
+import { useTranslation } from 'packages/translations';
 
 import { Form } from './Form';
 import { RewardDetails } from './RewardDetails';
 
 const PrimeCalculator: React.FC = () => {
+  const { Trans } = useTranslation();
+
   const usdt = useGetToken({
     symbol: 'USDT',
   });
@@ -13,19 +19,32 @@ const PrimeCalculator: React.FC = () => {
   }
 
   return (
-    <div className="space-y-4 lg:grid lg:grid-cols-2 lg:gap-6 lg:space-y-0">
-      <Form />
-
-      {/* TODO: wire up RewardDetails */}
-      <RewardDetails
-        primeBorrowApy="-"
-        primeSupplyApy="-"
-        token={usdt}
-        totalYearlyRewards="-"
-        userYearlyRewards="-"
-        userSuppliedTokens="-"
-        userBorrowedTokens="-"
+    <div>
+      <NoticeInfo
+        className="mb-6"
+        description={
+          <Trans
+            i18nKey="primeCalculator.topMessage"
+            components={{
+              Link: <Link href={PRIME_DOC_URL} />,
+            }}
+          />
+        }
       />
+
+      <div className="space-y-4 lg:grid lg:grid-cols-2 lg:gap-6 lg:space-y-0">
+        <Form />
+
+        <RewardDetails
+          primeBorrowApy="-"
+          primeSupplyApy="-"
+          token={usdt}
+          totalYearlyRewards="-"
+          userYearlyRewards="-"
+          userSuppliedTokens="-"
+          userBorrowedTokens="-"
+        />
+      </div>
     </div>
   );
 };
