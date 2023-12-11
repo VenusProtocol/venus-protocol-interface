399d41b509a4de3311aba959de689530061fe81a
Gleiser Oliveira <gleiser.dev@gmail.com>
2023-12-11 06:57:00 -0600

    feat: set isIsolatedPoolMarket for non BNB core pools

--- src/pages/Market/CorePoolMarket/index.tsx
+++ src/pages/Market/CorePoolMarket/index.tsx
@@ -2,18 +2,23 @@
 import { useParams } from 'react-router-dom';
 
 import { useGetChainMetadata } from 'hooks/useGetChainMetadata';
+import { useChainId } from 'packages/wallet';
+import { ChainId } from 'types';
 
 import Market from '..';
 import MarketLoader from '../MarketLoader';
 
 const CorePoolMarket: React.FC = () => {
   const { vTokenAddress } = useParams();
+  const { chainId } = useChainId();
+  const isBnbChain = chainId === ChainId.BSC_MAINNET || chainId === ChainId.BSC_TESTNET;
   const { corePoolComptrollerContractAddress } = useGetChainMetadata();
 
   return (
     <MarketLoader
       poolComptrollerAddress={corePoolComptrollerContractAddress}
       vTokenAddress={vTokenAddress}
+      isIsolatedPoolMarket={!isBnbChain}
     >
       {marketProps => <Market {...marketProps} />}
     </MarketLoader>
