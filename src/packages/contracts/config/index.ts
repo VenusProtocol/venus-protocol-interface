e79e5a34c44e95811b9d39447def240f763be402
therealemjy <maxime.julian.creations@gmail.com>
2023-12-14 10:52:28 +0100

    feat: add Multicall3 Sepolia record

--- src/packages/contracts/config/index.ts
+++ src/packages/contracts/config/index.ts
@@ -193,6 +193,7 @@ export const contracts: ContractConfig[] = [
     address: {
       [ChainId.BSC_TESTNET]: '0xca11bde05977b3631167028862be2a173976ca11',
       [ChainId.BSC_MAINNET]: '0xca11bde05977b3631167028862be2a173976ca11',
+      [ChainId.SEPOLIA]: '0xca11bde05977b3631167028862be2a173976ca11',
     },
   },
   {
