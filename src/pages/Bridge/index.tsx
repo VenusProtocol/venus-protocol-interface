54cc437b30e6c00a824996ff77528a6b2df39025
therealemjy <maxime.julian.creations@gmail.com>
2023-12-18 13:14:55 +0100

    feat: add footer

--- src/pages/Bridge/index.tsx
+++ src/pages/Bridge/index.tsx
@@ -11,6 +11,7 @@ import {
   TextButton,
   TokenTextField,
 } from 'components';
+import { Link } from 'containers/Link';
 import useTokenApproval from 'hooks/useTokenApproval';
 import { useGetMaximillionContract } from 'packages/contracts';
 import { useGetToken } from 'packages/tokens';
@@ -18,6 +19,9 @@ import { useTranslation } from 'packages/translations';
 import { chains, useAccountAddress, useAuthModal } from 'packages/wallet';
 
 import { ChainSelect } from './ChainSelect';
+import { ReactComponent as LayerZeroLogo } from './layerZeroLogo.svg';
+
+const BRIDGE_DOC_URL = ''; // TODO: add link
 
 const BridgePage: React.FC = () => {
   const { t } = useTranslation();
@@ -65,100 +69,120 @@ const BridgePage: React.FC = () => {
   }, [t, isFormValid]);
 
   return (
-    <Card className="mx-auto w-full space-y-6 md:max-w-[544px]">
-      <div className="md:flex md:items-end md:justify-between md:space-x-4">
-        <ChainSelect
-          // TODO: wire up
-          onChange={() => {}}
-          value={formValues.fromChainId}
-          label={t('bridgePage.fromChainSelect.label')}
-          className="mb-4 w-full min-w-0 grow md:mb-0"
-        />
+    <div className="mx-auto w-full space-y-6 md:max-w-[544px]">
+      <Card>
+        <div className="mb-6 md:flex md:items-end md:justify-between md:space-x-4">
+          <ChainSelect
+            // TODO: wire up
+            onChange={() => {}}
+            value={formValues.fromChainId}
+            label={t('bridgePage.fromChainSelect.label')}
+            className="mb-4 w-full min-w-0 grow md:mb-0"
+          />
 
-        <TextButton
-          className="mx-auto mb-2 flex h-auto flex-none p-2 md:mb-[3px]"
-          // TODO: wire up
-          onClick={() => {}}
-          disabled={isSubmitting}
-        >
-          <Icon name="convert" className="h-6 w-6 rotate-90 text-blue md:rotate-0" />
-        </TextButton>
+          <TextButton
+            className="mx-auto mb-2 flex h-auto flex-none p-2 md:mb-[3px]"
+            // TODO: wire up
+            onClick={() => {}}
+            disabled={isSubmitting}
+          >
+            <Icon name="convert" className="h-6 w-6 rotate-90 text-blue md:rotate-0" />
+          </TextButton>
 
-        <ChainSelect
-          // TODO: wire up
-          onChange={() => {}}
-          value={formValues.toChainId}
-          menuPosition="right"
-          className="w-full min-w-0 grow"
-          label={t('bridgePage.toChainSelect.label')}
-        />
-      </div>
+          <ChainSelect
+            // TODO: wire up
+            onChange={() => {}}
+            value={formValues.toChainId}
+            menuPosition="right"
+            className="w-full min-w-0 grow"
+            label={t('bridgePage.toChainSelect.label')}
+          />
+        </div>
 
-      <TokenTextField
-        name="amountTokens"
-        token={xvs!}
-        label={t('bridgePage.amountInput.label')}
-        value="" // TODO: wire up
-        onChange={() => {}} // TODO: wire up
-        disabled={isSubmitting || isApproveXvsLoading}
-        rightMaxButton={{
-          label: t('bridgePage.amountInput.maxButtonLabel'),
-          onClick: () => {}, // TODO: wire up
-        }}
-      />
-
-      <div className="space-y-3">
-        <LabeledInlineContent label={t('bridgePage.walletBalance.label')}>
-          {readableWalletBalance}
-        </LabeledInlineContent>
-
-        <SpendingLimit
+        <TokenTextField
+          name="amountTokens"
           token={xvs!}
-          walletBalanceTokens={new BigNumber(100)} // TODO: wire up
-          walletSpendingLimitTokens={xvsWalletSpendingLimitTokens}
-          onRevoke={revokeXvsWalletSpendingLimit}
-          isRevokeLoading={isRevokeXvsWalletSpendingLimitLoading}
+          label={t('bridgePage.amountInput.label')}
+          value="" // TODO: wire up
+          onChange={() => {}} // TODO: wire up
+          className="mb-6"
+          disabled={isSubmitting || isApproveXvsLoading}
+          rightMaxButton={{
+            label: t('bridgePage.amountInput.maxButtonLabel'),
+            onClick: () => {}, // TODO: wire up
+          }}
         />
+
+        <div className="mb-6 space-y-3">
+          <LabeledInlineContent label={t('bridgePage.walletBalance.label')}>
+            {readableWalletBalance}
+          </LabeledInlineContent>
+
+          <SpendingLimit
+            token={xvs!}
+            walletBalanceTokens={new BigNumber(100)} // TODO: wire up
+            walletSpendingLimitTokens={xvsWalletSpendingLimitTokens}
+            onRevoke={revokeXvsWalletSpendingLimit}
+            isRevokeLoading={isRevokeXvsWalletSpendingLimitLoading}
+          />
+        </div>
 
         <LabeledInlineContent
           label={t('bridgePage.bridgeGasFee.label')}
           tooltip={t('bridgePage.bridgeGasFee.tooltip')}
+          className="mb-10"
         >
           -
         </LabeledInlineContent>
-      </div>
 
-      <ApproveTokenSteps
-        token={xvs!}
-        hideTokenEnablingStep={!isUserConnected || !isFormValid}
-        isTokenApproved={isApproveXvsLoading}
-        approveToken={approveXvs}
-        isApproveTokenLoading={isApproveXvsLoading}
-        isWalletSpendingLimitLoading={isXvsWalletSpendingLimitLoading}
-      >
-        {isUserConnected ? (
-          <PrimaryButton
-            type="submit"
-            loading={isSubmitting}
-            disabled={
-              !isFormValid ||
-              isSubmitting ||
-              isApproveXvsLoading ||
-              isXvsWalletSpendingLimitLoading ||
-              isRevokeXvsWalletSpendingLimitLoading ||
-              !isXvsApproved
-            }
-            className="w-full"
-          >
-            {submitButtonLabel}
-          </PrimaryButton>
-        ) : (
-          <PrimaryButton className="w-full" onClick={openAuthModal}>
-            {t('bridgePage.connectWalletButton.label')}
-          </PrimaryButton>
-        )}
-      </ApproveTokenSteps>
-    </Card>
+        <ApproveTokenSteps
+          className="mt-10"
+          token={xvs!}
+          hideTokenEnablingStep={!isUserConnected || !isFormValid}
+          isTokenApproved={isApproveXvsLoading}
+          approveToken={approveXvs}
+          isApproveTokenLoading={isApproveXvsLoading}
+          isWalletSpendingLimitLoading={isXvsWalletSpendingLimitLoading}
+        >
+          {isUserConnected ? (
+            <PrimaryButton
+              type="submit"
+              loading={isSubmitting}
+              disabled={
+                !isFormValid ||
+                isSubmitting ||
+                isApproveXvsLoading ||
+                isXvsWalletSpendingLimitLoading ||
+                isRevokeXvsWalletSpendingLimitLoading ||
+                !isXvsApproved
+              }
+              className="w-full"
+            >
+              {submitButtonLabel}
+            </PrimaryButton>
+          ) : (
+            <PrimaryButton className="w-full" onClick={openAuthModal}>
+              {t('bridgePage.connectWalletButton.label')}
+            </PrimaryButton>
+          )}
+        </ApproveTokenSteps>
+      </Card>
+
+      <div className="flex items-center justify-center space-x-3">
+        <div className="flex items-center">
+          <span className="mr-1 text-sm text-grey">{t('bridgePage.footer.poweredBy')}</span>
+          <LayerZeroLogo className="w-[80px]" />
+        </div>
+
+        <div className="h-5 w-[1px] bg-lightGrey" />
+
+        <Link href={BRIDGE_DOC_URL} className="flex items-center text-sm">
+          {t('bridgePage.footer.bridgeDocLink')}
+
+          <Icon name="open" className="ml-1 text-inherit" />
+        </Link>
+      </div>
+    </div>
   );
 };
 
