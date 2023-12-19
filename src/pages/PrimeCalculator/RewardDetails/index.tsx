84dcdd47cbe0f7de88175001c3ddb30ffa1e263f
Gleiser Oliveira <gleiser.dev@gmail.com>
2023-12-18 22:54:21 -0600

    feat: add RewardDetails component

--- src/pages/PrimeCalculator/RewardDetails/index.tsx
+++ src/pages/PrimeCalculator/RewardDetails/index.tsx
@@ -1,3 +1,99 @@
-import { Card } from 'components';
+import { Card, Delimiter, Icon, LabeledInlineContent, Tooltip } from 'components';
+import { useTranslation } from 'packages/translations';
+import { Token } from 'types';
 
-export const RewardDetails: React.FC = () => <Card>RewardDetails</Card>;
+interface RewardDetailsProps {
+  token: Token;
+  totalYearlyRewards: string;
+  userYearlyRewards: string;
+  userSuppliedTokens: string;
+  userBorrowedTokens: string;
+  primeSupplyApy: string;
+  primeBorrowApy: string;
+}
+
+export const RewardDetails: React.FC<RewardDetailsProps> = ({
+  primeBorrowApy,
+  primeSupplyApy,
+  token,
+  totalYearlyRewards,
+  userBorrowedTokens,
+  userSuppliedTokens,
+  userYearlyRewards,
+}: RewardDetailsProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <Card>
+      <h4 className="text-lg font-semibold">{t('primeCalculator.rewardDetails.title')}</h4>
+      <div className="mb-3 mt-6 flex justify-between">
+        <div className="flex w-full">
+          <LabeledInlineContent
+            className="flex-1"
+            iconSrc={token}
+            tooltip={t('primeCalculator.rewardDetails.totalYearlyRewards.tooltip')}
+            label={t('primeCalculator.rewardDetails.totalYearlyRewards.title')}
+          >
+            {totalYearlyRewards}
+          </LabeledInlineContent>
+        </div>
+      </div>
+      <div className="mb-6 flex justify-between">
+        <div className="flex w-full">
+          <LabeledInlineContent
+            className="flex-1"
+            iconSrc={token}
+            label={t('primeCalculator.rewardDetails.yourYearlyRewards')}
+          >
+            {userYearlyRewards}
+          </LabeledInlineContent>
+        </div>
+      </div>
+
+      <Delimiter />
+
+      <div className="mb-8 mt-6 flex lg:mb-6">
+        <div className="flex flex-col w-[110px] sm:w-35 md:w-[110px] lg:w-40">
+          <span className="text-grey">{t('primeCalculator.rewardDetails.fromSuppliedTokens')}</span>
+          <span>{userSuppliedTokens}</span>
+        </div>
+        <Icon name="blueArrowRight" className="mx-6 self-center lg:mx-3" />
+        <div className="flex flex-col">
+          <div className="flex">
+            <span className="mr-2 text-grey">
+              {t('primeCalculator.rewardDetails.primeSupplyApy.title')}
+            </span>
+            <Tooltip
+              className="flex flex-col justify-center"
+              title={t('primeCalculator.rewardDetails.primeSupplyApy.tooltip')}
+            >
+              <Icon name="info" />
+            </Tooltip>
+          </div>
+          <span>{primeSupplyApy}</span>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex flex-col w-[110px] sm:w-35 md:w-[110px] lg:w-40">
+          <span className="text-grey">{t('primeCalculator.rewardDetails.fromBorrowedTokens')}</span>
+          <span>{userBorrowedTokens}</span>
+        </div>
+        <Icon name="blueArrowRight" className="mx-6 self-center lg:mx-3" />
+        <div className="flex flex-col">
+          <div className="flex">
+            <span className="mr-2 text-grey">
+              {t('primeCalculator.rewardDetails.primeBorrowApy.title')}
+            </span>
+            <Tooltip
+              className="flex flex-col justify-center"
+              title={t('primeCalculator.rewardDetails.primeBorrowApy.tooltip')}
+            >
+              <Icon name="info" />
+            </Tooltip>
+          </div>
+          <span>{primeBorrowApy}</span>
+        </div>
+      </div>
+    </Card>
+  );
+};
--- src/pages/PrimeCalculator/index.tsx
+++ src/pages/PrimeCalculator/index.tsx
@@ -1,12 +1,33 @@
+import { useGetToken } from 'packages/tokens';
+
 import { Form } from './Form';
 import { RewardDetails } from './RewardDetails';
 
-const PrimeCalculator: React.FC = () => (
-  <div className="space-y-4 lg:grid lg:grid-cols-2 lg:gap-6 lg:space-y-0">
-    <Form />
+const PrimeCalculator: React.FC = () => {
+  const usdt = useGetToken({
+    symbol: 'USDT',
+  });
 
-    <RewardDetails />
-  </div>
-);
+  if (!usdt) {
+    return null;
+  }
+
+  return (
+    <div className="space-y-4 lg:grid lg:grid-cols-2 lg:gap-6 lg:space-y-0">
+      <Form />
+
+      {/* TODO: wire up RewardDetails */}
+      <RewardDetails
+        primeBorrowApy="-"
+        primeSupplyApy="-"
+        token={usdt}
+        totalYearlyRewards="-"
+        userYearlyRewards="-"
+        userSuppliedTokens="-"
+        userBorrowedTokens="-"
+      />
+    </div>
+  );
+};
 
 export default PrimeCalculator;
