55c7ddf95e644a923e42dbe7f09507756e1e28b5
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2024-01-12 12:52:13 +0000

    fix: chain ID detection from URL in getChainId function (#2048)

--- src/packages/wallet/utilities/getChainId/index.tsx
+++ src/packages/wallet/utilities/getChainId/index.tsx
@@ -1,6 +1,6 @@
 import { getChainIdFromSearchParams } from 'packages/wallet/utilities/getChainIdFromSearchParams';
 
 export const getChainId = () => {
-  const searchParams = new URLSearchParams(document.location.search);
+  const searchParams = new URLSearchParams(`?${window.location.hash.split('?')[1]}`);
   return getChainIdFromSearchParams({ searchParams });
 };
