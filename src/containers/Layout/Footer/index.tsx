c8496172e884f8db2986f0a4994e07826955bc57
therealemjy <maxime.julian.creations@gmail.com>
2024-01-15 09:57:07 +0000

    feat: remove block number from footer

--- src/containers/Layout/Footer/index.tsx
+++ src/containers/Layout/Footer/index.tsx
@@ -1,8 +1,4 @@
-import { useGetBlockNumber } from 'clients/api';
-import { Link } from 'containers/Link';
-import { useGetChainMetadata } from 'hooks/useGetChainMetadata';
 import { useGetToken } from 'packages/tokens';
-import { useTranslation } from 'packages/translations';
 import { useChainId } from 'packages/wallet';
 import { generateChainExplorerUrl } from 'utilities';
 
@@ -17,48 +13,29 @@ import {
 
 export const Footer: React.FC = () => {
   const { chainId } = useChainId();
-  const { data: getBlockNumberData } = useGetBlockNumber();
 
-  const { t } = useTranslation();
-  const currentBlockNumber = getBlockNumberData?.blockNumber;
   const xvs = useGetToken({
     symbol: 'XVS',
   });
 
-  const { explorerUrl } = useGetChainMetadata();
-
   return (
     <footer className="flex h-14 flex-none items-center justify-between bg-background px-4 sm:justify-end md:px-6 xl:px-10">
-      {!!currentBlockNumber && (
-        <Link
-          className="text-sm text-grey hover:no-underline sm:mr-6"
-          href={explorerUrl}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {t('footer.latestNumber')}
-          <br className="sm:hidden" /> <span className="text-offWhite">{currentBlockNumber}</span>
-        </Link>
-      )}
-
-      <div className="flex">
-        <IconLink
-          href={
-            xvs &&
-            generateChainExplorerUrl({
-              hash: xvs.address,
-              urlType: 'token',
-              chainId,
-            })
-          }
-          iconName="venus"
-        />
-        <IconLink iconName="discord" href={VENUS_DISCORD_URL} />
-        <IconLink iconName="telegram" href={VENUS_TELEGRAM_URL} />
-        <IconLink iconName="twitter" href={VENUS_TWITTER_URL} />
-        <IconLink iconName="medium" href={VENUS_MEDIUM_URL} />
-        <IconLink iconName="github" href={VENUS_GITHUB_URL} />
-      </div>
+      <IconLink
+        href={
+          xvs &&
+          generateChainExplorerUrl({
+            hash: xvs.address,
+            urlType: 'token',
+            chainId,
+          })
+        }
+        iconName="venus"
+      />
+      <IconLink iconName="discord" href={VENUS_DISCORD_URL} />
+      <IconLink iconName="telegram" href={VENUS_TELEGRAM_URL} />
+      <IconLink iconName="twitter" href={VENUS_TWITTER_URL} />
+      <IconLink iconName="medium" href={VENUS_MEDIUM_URL} />
+      <IconLink iconName="github" href={VENUS_GITHUB_URL} />
     </footer>
   );
 };
