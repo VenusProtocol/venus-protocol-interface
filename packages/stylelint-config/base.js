ddd5f3a7d6c6514ab274a0153bbf380fdaa22c27
therealemjy <maxime.julian.creations@gmail.com>
2024-01-22 13:47:21 +0100

    refactor: add internal stylelint-config package

---
+++ packages/stylelint-config/base.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss'],
+  customSyntax: '@stylelint/postcss-css-in-js',
+  rules: {
+    'at-rule-empty-line-before': null,
+    'function-name-case': null,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['function', 'if', 'else', 'return', 'each', 'include', 'mixin'],
+      },
+    ],
+    'block-closing-brace-newline-after': [
+      'always',
+      {
+        ignoreAtRules: ['if', 'else'],
+      },
+    ],
+    'declaration-empty-line-before': null,
+    'rule-empty-line-before': null,
+    'selector-list-comma-newline-after': null,
+    'no-descending-specificity': null,
+  },
+};
