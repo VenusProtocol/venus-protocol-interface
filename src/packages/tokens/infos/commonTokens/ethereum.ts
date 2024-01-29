cb2ff365efca75205d924faddc557db7da04d556
Gleiser Oliveira <gleiser.dev@gmail.com>
2024-01-29 10:10:29 -0600

    feat: add Ethereum tokens

--- src/packages/tokens/infos/commonTokens/ethereum.ts
+++ src/packages/tokens/infos/commonTokens/ethereum.ts
@@ -1,4 +1,10 @@
+import crvLogo from 'packages/tokens/img/crv.svg';
+import crvUsdLogo from 'packages/tokens/img/crvUsd.svg';
 import ethLogo from 'packages/tokens/img/eth.svg';
+import usdcLogo from 'packages/tokens/img/usdc.svg';
+import usdtLogo from 'packages/tokens/img/usdt.svg';
+import wBtcLogo from 'packages/tokens/img/wbtc.svg';
+import wEthLogo from 'packages/tokens/img/weth.svg';
 import wstEthLogo from 'packages/tokens/img/wstEth.svg';
 import xvsLogo from 'packages/tokens/img/xvs.svg';
 import { Token } from 'types';
@@ -22,5 +28,41 @@ export const tokens: Token[] = [
     decimals: 18,
     symbol: 'wstETH',
     asset: wstEthLogo,
+  },
+  {
+    address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+    decimals: 8,
+    symbol: 'wBTC',
+    asset: wBtcLogo,
+  },
+  {
+    address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    decimals: 18,
+    symbol: 'wETH',
+    asset: wEthLogo,
+  },
+  {
+    address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    decimals: 6,
+    symbol: 'USDC',
+    asset: usdcLogo,
+  },
+  {
+    address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    decimals: 6,
+    symbol: 'USDT',
+    asset: usdtLogo,
+  },
+  {
+    address: '0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E',
+    decimals: 18,
+    symbol: 'crvUSD',
+    asset: crvUsdLogo,
+  },
+  {
+    address: '0xD533a949740bb3306d119CC777fa900bA034cd52',
+    decimals: 18,
+    symbol: 'CRV',
+    asset: crvLogo,
   },
 ];
