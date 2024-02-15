f221f9d640c483ebbcec86892ae78d56dd83eb6a
Gleiser Oliveira <gleiser.dev@gmail.com>
2024-02-15 08:37:27 -0600

    test: add tests for wrapUnwrapNativeToken in the RepayForm

--- apps/evm/src/hooks/useOperationModal/Modal/RepayForm/__testUtils__/fakeData.ts
+++ apps/evm/src/hooks/useOperationModal/Modal/RepayForm/__testUtils__/fakeData.ts
@@ -1,8 +1,9 @@
 import BigNumber from 'bignumber.js';
 
 import { poolData } from '__mocks__/models/pools';
+import { eth, weth } from '__mocks__/models/tokens';
 
-import { Asset, Pool } from 'types';
+import { Asset, Pool, TokenBalance } from 'types';
 
 export const fakePool: Pool = {
   ...poolData[0],
@@ -15,4 +16,22 @@ export const fakeAsset: Asset = {
   userBorrowBalanceTokens: new BigNumber(1000),
   userWalletBalanceTokens: new BigNumber(10000000),
   tokenPriceCents: new BigNumber(100),
+};
+
+export const fakeWethAsset: Asset = {
+  ...fakeAsset,
+  vToken: {
+    ...fakeAsset.vToken,
+    underlyingToken: weth,
+  },
+};
+
+export const fakeEthTokenBalance: TokenBalance = {
+  balanceMantissa: new BigNumber('1000000000000000000'),
+  token: eth,
+};
+
+export const fakeWEthTokenBalance: TokenBalance = {
+  balanceMantissa: new BigNumber('1000000000000000000'),
+  token: weth,
 };
