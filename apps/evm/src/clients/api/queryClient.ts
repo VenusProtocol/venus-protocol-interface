c4e5e18df2cb1a6705433454d25da86386adfc25
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2024-02-20 13:28:22 +0000

    feat: stop logging query errors (#2226)

--- apps/evm/src/clients/api/queryClient.ts
+++ apps/evm/src/clients/api/queryClient.ts
@@ -1,19 +1,5 @@
 import { QueryClient } from 'react-query';
 
-import { logError } from 'libs/errors';
-import { getChainId } from 'libs/wallet';
-import { ChainId } from 'types';
-
-const onError = (error: unknown) => {
-  const { chainId } = getChainId();
-
-  // Log errors happening on the Ethereum and Sepolia network only. This is temporary so we can
-  // track errors while the multichain feature is being tested by users
-  if (chainId === ChainId.ETHEREUM || chainId === ChainId.SEPOLIA) {
-    logError(error);
-  }
-};
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -21,10 +7,6 @@ const queryClient = new QueryClient({
       // the cache instantly after their hook unmounts (see documentation for
       // more info: https://react-query.tanstack.com/guides/important-defaults)
       staleTime: 10000,
-      onError,
-    },
-    mutations: {
-      onError,
     },
   },
 });
