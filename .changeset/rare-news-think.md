e4d9b0a07f8d05a27c3c2a5f7451d278f91dc3ba
therealemjy <maxime.julian.creations@gmail.com>
2024-03-26 17:25:40 +0100

    fix: always fetch proposal previews from Governance chain

---
+++ .changeset/rare-news-think.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": patch
+---
+
+always fetch governance previews using governance chain provider
--- apps/evm/src/clients/api/queries/getBlockNumber/useGetBlockNumber.ts
+++ apps/evm/src/clients/api/queries/getBlockNumber/useGetBlockNumber.ts
@@ -1,6 +1,6 @@
 import { type QueryObserverOptions, useQuery } from 'react-query';
 
-import { getBlockNumber } from 'clients/api/';
+import getBlockNumber, { type GetBlockNumberOutput } from 'clients/api/queries/getBlockNumber';
 import FunctionKey from 'constants/functionKey';
 import { useGetChainMetadata } from 'hooks/useGetChainMetadata';
 import { useChainId, useProvider } from 'libs/wallet';
@@ -8,8 +8,8 @@ import type { ChainId } from 'types';
 
 export type UseGetBlockNumberQueryKey = [FunctionKey.GET_BLOCK_NUMBER, { chainId: ChainId }];
 
-interface GetBlockNumberOutput {
-  blockNumber: number;
+interface Input {
+  chainId?: ChainId;
 }
 
 type Options = QueryObserverOptions<
@@ -20,9 +20,10 @@ type Options = QueryObserverOptions<
   UseGetBlockNumberQueryKey
 >;
 
-const useGetBlockNumber = (options?: Options) => {
-  const { provider } = useProvider();
-  const { chainId } = useChainId();
+const useGetBlockNumber = (input?: Input, options?: Options) => {
+  const { chainId: currentChainId } = useChainId();
+  const chainId = input?.chainId ?? currentChainId;
+  const { provider } = useProvider({ chainId });
   const { blockTimeMs } = useGetChainMetadata();
 
   return useQuery([FunctionKey.GET_BLOCK_NUMBER, { chainId }], () => getBlockNumber({ provider }), {
