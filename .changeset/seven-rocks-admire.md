6c6dfeb5079b9244199bd7ab4f9d87145174fdbf
Gleiser Oliveira <52756735+gleiser-oliveira@users.noreply.github.com>
2024-03-26 02:40:04 -0600

    feat: activate wrapUnwrapNativeToken on BSC_MAINNET (#2425)
    * feat: activate wrapUnwrapNativeToken on BSC_MAINNET
    
    * chore: add changeset

---
+++ .changeset/seven-rocks-admire.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": minor
+---
+
+activate wrapUnwrapNativeToken on BSC_MAINNET
--- apps/evm/src/hooks/useIsFeatureEnabled/index.tsx
+++ apps/evm/src/hooks/useIsFeatureEnabled/index.tsx
@@ -35,6 +35,7 @@ export const featureFlags = {
     ChainId.OPBNB_TESTNET,
   ],
   wrapUnwrapNativeToken: [
+    ChainId.BSC_MAINNET,
     ChainId.BSC_TESTNET,
     ChainId.ETHEREUM,
     ChainId.SEPOLIA,
