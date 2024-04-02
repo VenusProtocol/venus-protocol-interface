4b3ccbbf500362a7259c6d834c6a2bb590097cb1
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2024-04-02 15:43:49 +0200

    feat: add error state on Governance page (#2478)

---
+++ .changeset/ten-coins-learn.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": minor
+---
+
+Add error state to Governance page
--- apps/evm/src/components/Notice/types.ts
+++ apps/evm/src/components/Notice/types.ts
@@ -3,9 +3,8 @@ import type { ReactElement } from 'react';
 export type NoticeVariant = 'info' | 'loading' | 'error' | 'success' | 'warning';
 
 export interface NoticeProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'title'> {
-  className?: string;
-  title?: string | ReactElement;
   description: string | ReactElement;
+  title?: string | ReactElement;
   variant?: NoticeVariant;
   onClose?: () => void;
 }
