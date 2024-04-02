ed1d723af14559a4c6395e4c1fa9b16de5bcf67c
therealemjy <maxime.julian.creations@gmail.com>
2024-04-02 12:45:07 +0200

    refactor: re-enable Prime feature on Ethereum

--- apps/evm/src/hooks/useIsFeatureEnabled/index.tsx
+++ apps/evm/src/hooks/useIsFeatureEnabled/index.tsx
@@ -3,7 +3,7 @@ import { ChainId } from 'types';
 
 export const featureFlags = {
   integratedSwap: [ChainId.BSC_TESTNET, ChainId.BSC_MAINNET],
-  prime: [ChainId.BSC_MAINNET, ChainId.BSC_TESTNET, ChainId.SEPOLIA],
+  prime: [ChainId.BSC_MAINNET, ChainId.BSC_TESTNET, ChainId.SEPOLIA, ChainId.ETHEREUM],
   primeCalculator: [ChainId.BSC_MAINNET, ChainId.BSC_TESTNET],
   tusdMigrationWarning: [ChainId.BSC_MAINNET],
   trxMigrationWarning: [ChainId.BSC_MAINNET],
