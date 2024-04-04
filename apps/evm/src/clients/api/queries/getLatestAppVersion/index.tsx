acfbea00c4ddcdbc76e1ae65613ed53c24a78043
therealemjy <maxime.julian.creations@gmail.com>
2024-04-04 13:28:12 +0200

    feat: test

--- apps/evm/src/clients/api/queries/getLatestAppVersion/index.tsx
+++ apps/evm/src/clients/api/queries/getLatestAppVersion/index.tsx
@@ -1,3 +1,5 @@
+import { version as APP_VERSION } from 'constants/version';
+
 export type GetLatestAppVersionOutput = {
   version?: string;
 };
@@ -7,6 +9,9 @@ export const PUBLIC_VERSION_FILE_URL = `${window.location.origin}/version.json`;
 export const getLatestAppVersion = async (): Promise<GetLatestAppVersionOutput> => {
   const data = await fetch(PUBLIC_VERSION_FILE_URL);
   const { version } = await data.json();
+
+  console.log('Fetched version', version);
+  console.log('Local version', APP_VERSION);
 
   return {
     version,
