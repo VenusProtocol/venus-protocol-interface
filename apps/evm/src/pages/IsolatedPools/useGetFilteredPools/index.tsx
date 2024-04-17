69ae2f983042777f68a9e4f8e4ea6695ec54b12b
therealemjy <maxime.julian.creations@gmail.com>
2024-04-17 10:45:14 +0200

    fix: stats on Isolated Pools page

---
+++ apps/evm/src/pages/IsolatedPools/useGetFilteredPools/index.tsx
@@ -0,0 +1,24 @@
+import { useGetIsolatedPools } from 'clients/api';
+import { useGetChainMetadata } from 'hooks/useGetChainMetadata';
+import { useMemo } from 'react';
+
+export const useGetFilteredPools = () => {
+  const { data: getPoolsData } = useGetIsolatedPools();
+  const chainMetaData = useGetChainMetadata();
+
+  const pools = useMemo(
+    () =>
+      (getPoolsData?.pools || []).filter(
+        pool =>
+          pool.comptrollerAddress !== chainMetaData.corePoolComptrollerContractAddress &&
+          pool.comptrollerAddress !== chainMetaData.lidoPoolComptrollerContractAddress,
+      ),
+    [
+      getPoolsData?.pools,
+      chainMetaData.corePoolComptrollerContractAddress,
+      chainMetaData.lidoPoolComptrollerContractAddress,
+    ],
+  );
+
+  return { pools };
+};
