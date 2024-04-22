224e7f45b44076b2fd9d3e9ed27d3f2273e61bf4
therealemjy <maxime.julian.creations@gmail.com>
2024-04-22 16:38:13 +0200

    refactor: update Market header order

--- apps/evm/src/containers/Layout/Header/MarketInfo/index.tsx
+++ apps/evm/src/containers/Layout/Header/MarketInfo/index.tsx
@@ -115,29 +115,29 @@ export const MarketInfo = () => {
 
   return (
     <div className="pt-4 pb-12 md:pb-10 border-b-lightGrey border-b space-y-8">
-      <div className="flex items-center gap-3 flex-wrap px-4 md:px-6 xl:px-10 max-w-[1360px] mx-auto">
-        <div className="flex items-center h-8">
-          <Link to={goBackTo} className="h-full pr-3 flex items-center">
-            <Icon name="chevronLeft" className="w-6 h-6 text-offWhite" />
-          </Link>
+      <div className="flex items-center h-8 px-4 md:px-6 xl:px-10 max-w-[1360px] mx-auto">
+        <Link to={goBackTo} className="h-full pr-3 flex items-center">
+          <Icon name="chevronLeft" className="w-6 h-6 text-offWhite" />
+        </Link>
 
-          {asset && pool ? (
-            <>
-              <TokenIcon token={asset.vToken.underlyingToken} className="h-full w-8 mr-3" />
+        {asset && pool ? (
+          <div className="flex items-center gap-3">
+            <TokenIcon token={asset.vToken.underlyingToken} className="h-full w-8 shrink-0" />
 
+            <div className="flex flex-wrap items-center gap-x-3 gap-y-1">
               <span className="font-bold">
                 {asset.vToken.underlyingToken.symbol} ({pool?.name})
               </span>
-            </>
-          ) : (
-            <Spinner className="h-full" />
-          )}
-        </div>
 
-        {pool?.isIsolated && <Pill>{t('layout.header.isolated')}</Pill>}
+              {pool.isIsolated && <Pill>{t('layout.header.isolated')}</Pill>}
+            </div>
 
-        {asset && isUserConnected && canAddTokenToWallet() && (
-          <AddTokenToWalletButton token={asset.vToken.underlyingToken} />
+            {isUserConnected && canAddTokenToWallet() && (
+              <AddTokenToWalletButton className="shrink-0" token={asset.vToken.underlyingToken} />
+            )}
+          </div>
+        ) : (
+          <Spinner className="h-full" />
         )}
       </div>
 
