fd72e036724649a954fd66b751c1610df9948562
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2024-04-25 16:17:36 +0200

    refactor: remove local supply/borrow disabled token actions (#2599)

---
+++ .changeset/curly-mayflies-begin.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": patch
+---
+
+Remove local supply/borrow disabled token actions. These will now be fully controlled from contracts
--- apps/evm/src/libs/tokens/infos/disabledTokenActions/bscMainnet.ts
+++ apps/evm/src/libs/tokens/infos/disabledTokenActions/bscMainnet.ts
@@ -1,56 +1,6 @@
 import type { DisabledTokenAction } from '../../types';
 
 export const disabledTokenActions: DisabledTokenAction[] = [
-  {
-    // BUSD
-    address: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // UST
-    address: '0x3d4350cD54aeF9f9b2C29435e0fa809957B3F30a',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // LUNA
-    address: '0x156ab3346823B651294766e23e6Cf87254d68962',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // SXP
-    address: '0x47BEAd2563dCBf3bF2c9407fEa4dC236fAbA485A',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // TRXOLD
-    address: '0x85EAC5Ac2F758618dFa09bDbe0cf174e7d574D5B',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // TUSDOLD
-    address: '0x14016e85a25aeb13065688cafb43044c2ef86784',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // BETH
-    address: '0x250632378E573c6Be1AC2f97Fcdf00515d0Aa91B',
-    disabledActions: ['borrow'],
-  },
-  {
-    // stkBNB
-    address: '0xc2E9d07F66A89c44062459A47a0D2Dc038E4fb16',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // SnBNB
-    address: '0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B',
-    disabledActions: ['borrow', 'supply'],
-  },
-  {
-    // EURA
-    address: '0x12f31b73d812c6bb0d735a218c086d44d5fe5f89',
-    disabledActions: ['borrow', 'supply'],
-  },
   // BNB
   {
     address: '0x0000000000000000000000000000000000000000',
