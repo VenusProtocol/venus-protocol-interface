66b83892494e4b2e218954d74513e639da7547c4
Corey <coreyarice@gmail.com>
2024-05-13 08:01:09 -0300

    fix: correct discord link

--- apps/evm/src/containers/Layout/Footer/constants.ts
+++ apps/evm/src/containers/Layout/Footer/constants.ts
@@ -1,4 +1,4 @@
-export const VENUS_DISCORD_URL = 'https://discord.com/invite/pTQ9EBHYtF';
+export const VENUS_DISCORD_URL = 'https://discord.gg/venus-protocol-912811548651708448';
 export const VENUS_TWITTER_URL = 'https://twitter.com/VenusProtocol';
 export const VENUS_GITHUB_URL = 'https://github.com/VenusProtocol/';
 export const VENUS_TELEGRAM_URL = 'https://t.me/venusprotocol';
