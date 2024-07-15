e3886a097882c756b10d3416328f438b360130a8
therealemjy <maxime.julian.creations@gmail.com>
2024-07-15 13:29:44 +0200

    fix: update ID of Binance Wallet connector based on environment

---
+++ .changeset/tough-pigs-sort.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": minor
+---
+
+update ID of Binance Wallet connector based on environment
--- apps/evm/src/utilities/walletDetection.ts
+++ apps/evm/src/utilities/walletDetection.ts
@@ -1,4 +1,6 @@
 export const isRunningInOperaBrowser = () => (window.ethereum as WindowEthereum)?.isOpera;
 
+export const isRunningInBinanceApp = () => (window.ethereum as WindowEthereum)?.isBinance;
+
 export const isRunningInInfinityWalletApp = () =>
   (window.ethereum as WindowEthereum)?.isInfinityWallet;
