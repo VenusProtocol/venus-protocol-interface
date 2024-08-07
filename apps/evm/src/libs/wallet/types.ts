838abbac384e6aa4a6922a7a5b23c56012b789d9
therealemjy <maxime.julian.creations@gmail.com>
2024-08-07 21:43:37 +0200

    feat: integrate ConnectKit

--- apps/evm/src/libs/wallet/types.ts
+++ apps/evm/src/libs/wallet/types.ts
@@ -5,37 +5,5 @@ import type {
   JsonRpcSigner,
 } from '@ethersproject/providers';
 
-export type ConnectorId =
-  | 'BinanceW3WSDK'
-  | 'wallet.binance.com'
-  | 'coinbaseWalletSDK'
-  | 'io.metamask'
-  | 'io.rabby'
-  | 'io.gate.wallet'
-  | 'io.infinitywallet'
-  | 'com.okex.wallet'
-  | 'com.bitget.web3'
-  | 'com.brave.wallet'
-  | 'walletConnect'
-  | 'injected';
-
-export type BaseWallet = {
-  name: string;
-  logoSrc: string;
-};
-
-export type Wallet = BaseWallet & {
-  connectorId: ConnectorId;
-  mainnetOnly?: boolean;
-};
-
-// Integrated wallets represent wallets that are integrated through third-party
-// apps but not directly from our dApp. These wallets are displayed on the UI as
-// options that on click redirect to a web page explaining how to set them up
-// through a third-party app (e.g.: MetaMask).
-export type IntegratedWallet = BaseWallet & {
-  linkUrl: string;
-};
-
 export type Provider = JsonRpcProvider | FallbackProvider | BaseProvider;
 export type Signer = JsonRpcSigner;
