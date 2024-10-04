3d8d05e767479af8615cb3bc97cd2b0a68270f5f
therealemjy <maxime.julian.creations@gmail.com>
2024-10-04 17:33:34 +0200

    feat: list proposals on Proposal page

---
+++ .changeset/rare-camels-switch.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": minor
+---
+
+list multichain proposals on Proposal page
--- apps/evm/src/hooks/useFormatTo/__tests__/index.spec.tsx
+++ apps/evm/src/hooks/useFormatTo/__tests__/index.spec.tsx
@@ -1,20 +1,11 @@
-import type Vi from 'vitest';
-
 import { renderHook } from 'testUtils/render';
 
-import { useChainId } from 'libs/wallet';
 import { CHAIN_ID_SEARCH_PARAM } from 'libs/wallet/constants';
 import { ChainId } from 'types';
 
 import { useFormatTo } from '..';
 
 describe('useFormatTo', () => {
-  beforeEach(() => {
-    (useChainId as Vi.Mock).mockImplementation(() => ({
-      chainId: ChainId.BSC_TESTNET,
-    }));
-  });
-
   it('formats string with existing query', () => {
     const { result } = renderHook(() => useFormatTo());
     const formattedTo = result.current.formatTo({ to: '/path?param=value' });
