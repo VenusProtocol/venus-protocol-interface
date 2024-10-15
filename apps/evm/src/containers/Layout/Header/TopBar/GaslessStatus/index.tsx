f766c71db863f72ec24b2fafe8d0cc2bfa74e91c
Gleiser Oliveira <gleiser.dev@gmail.com>
2024-10-14 23:08:15 -0300

    feat: add tooltip to GaslessStatus

--- apps/evm/src/containers/Layout/Header/TopBar/GaslessStatus/index.tsx
+++ apps/evm/src/containers/Layout/Header/TopBar/GaslessStatus/index.tsx
@@ -1,4 +1,6 @@
 import { useGetSponsorshipVaultData } from 'clients/api';
+import { Tooltip } from 'components';
+import { Link } from 'containers/Link';
 import { useTranslation } from 'libs/translations';
 import { gasGreen, gasSlashedGreen, useChainId } from 'libs/wallet';
 import type { ChainId } from 'types';
@@ -7,29 +9,52 @@ const GaslessStatus = ({
   chainId,
   variant,
 }: { chainId?: ChainId; variant: 'header' | 'chainSelector' | 'mobile' }) => {
-  const { t } = useTranslation();
+  const { t, Trans } = useTranslation();
   const { chainId: chainIdFromHook } = useChainId();
   const { data: sponsorshipVaultData } = useGetSponsorshipVaultData({
     chainId: chainId ?? chainIdFromHook,
   });
+
+  if (sponsorshipVaultData?.hasEnoughFunds !== true) return null;
 
   const containerClassName =
     variant === 'header'
       ? 'hidden md:flex flex-row text-green [font-variant:all-small-caps] mr-'
       : 'flex flex-row text-green [font-variant:all-small-caps] ml-1 mr-2';
 
-  return (
-    sponsorshipVaultData?.hasEnoughFunds &&
-    (variant === 'mobile' ? (
+  if (variant === 'mobile') {
+    return (
       <div className="block md:hidden">
         <img src={gasSlashedGreen} alt={t('gaslessTransactions.gas')} className="ml-1" />
       </div>
-    ) : (
+    );
+  }
+  if (variant === 'chainSelector') {
+    return (
       <div className={containerClassName}>
         <img src={gasGreen} alt={t('gaslessTransactions.gas')} className="mr-1" />
         <span>{t('gaslessTransactions.chainLabel')}</span>
       </div>
-    ))
+    );
+  }
+
+  // @TODO: add link to docs
+  return (
+    <Tooltip
+      title={
+        <Trans
+          i18nKey="gaslessTransactions.tooltip"
+          components={{
+            Link: <Link href={''} />,
+          }}
+        />
+      }
+    >
+      <div className={containerClassName}>
+        <img src={gasGreen} alt={t('gaslessTransactions.gas')} className="mr-1" />
+        <span>{t('gaslessTransactions.chainLabel')}</span>
+      </div>
+    </Tooltip>
   );
 };
 
