fde29cc4cecc62e1718807031720c0547f2883ea
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2024-10-23 17:10:08 +0200

    feat: use custom account details modal (#3415)

---
+++ .changeset/silver-bears-explain.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": minor
+---
+
+use custom account details modal
--- apps/evm/src/containers/Layout/ConnectButton/index.tsx
+++ apps/evm/src/containers/Layout/ConnectButton/index.tsx
@@ -1,9 +1,12 @@
 import { useGetPrimeToken } from 'clients/api';
-import { Button, type ButtonProps } from 'components';
+import { Button, type ButtonProps, Modal, SecondaryButton } from 'components';
 import { useTranslation } from 'libs/translations';
 import { useAccountAddress, useAuthModal } from 'libs/wallet';
 import { cn, truncateAddress } from 'utilities';
 
+import { CopyAddressButton } from 'containers/CopyAddressButton';
+import { useState } from 'react';
+import { useDisconnect } from 'wagmi';
 import { PrimeButton } from './PrimeButton';
 
 export interface ConnectButtonProps
@@ -23,9 +26,26 @@ export const ConnectButton: React.FC<ConnectButtonProps> = ({
   variant = 'primary',
   ...otherProps
 }) => {
+  const { disconnect } = useDisconnect();
   const { accountAddress } = useAccountAddress();
   const { openAuthModal } = useAuthModal();
   const { t } = useTranslation();
+  const [isAccountModalOpen, setIsAccountModalOpen] = useState(false);
+  const openAccountModal = () => setIsAccountModalOpen(true);
+  const closeAccountModal = () => setIsAccountModalOpen(false);
+
+  const handleConnectButtonClick = () => {
+    if (accountAddress) {
+      openAccountModal();
+    } else {
+      openAuthModal();
+    }
+  };
+
+  const handleDisconnect = () => {
+    disconnect();
+    closeAccountModal();
+  };
 
   const { data: getPrimeTokenData, isLoading: isGetPrimeTokenLoading } = useGetPrimeToken({
     accountAddress,
@@ -36,31 +56,47 @@ export const ConnectButton: React.FC<ConnectButtonProps> = ({
     return null;
   }
 
-  if (accountAddress && isAccountPrime) {
-    return (
-      <PrimeButton
-        accountAddress={accountAddress}
-        onClick={openAuthModal}
-        className={cn(variant === 'secondary' && connectedAccountButtonClasses, className)}
-        {...otherProps}
-      />
-    );
-  }
-
   return (
-    <Button
-      variant={accountAddress ? 'secondary' : 'primary'}
-      onClick={openAuthModal}
-      className={cn(
-        className,
-        variant === 'secondary' && accountAddress && connectedAccountButtonClasses,
-        variant === 'secondary' &&
-          !accountAddress &&
-          'border-transparent bg-offWhite text-background hover:border-transparent hover:bg-grey active:bg-grey active:border-transparent',
+    <>
+      {accountAddress && isAccountPrime ? (
+        <PrimeButton
+          accountAddress={accountAddress}
+          onClick={handleConnectButtonClick}
+          className={cn(variant === 'secondary' && connectedAccountButtonClasses, className)}
+          {...otherProps}
+        />
+      ) : (
+        <Button
+          variant={accountAddress ? 'secondary' : 'primary'}
+          onClick={handleConnectButtonClick}
+          className={cn(
+            className,
+            variant === 'secondary' && accountAddress && connectedAccountButtonClasses,
+            variant === 'secondary' &&
+              !accountAddress &&
+              'border-transparent bg-offWhite text-background hover:border-transparent hover:bg-grey active:bg-grey active:border-transparent',
+          )}
+          {...otherProps}
+        >
+          {accountAddress ? <>{truncateAddress(accountAddress)}</> : t('connectButton.connect')}
+        </Button>
       )}
-      {...otherProps}
-    >
-      {accountAddress ? <>{truncateAddress(accountAddress)}</> : t('connectButton.title')}
-    </Button>
+
+      <Modal isOpen={isAccountModalOpen} handleClose={closeAccountModal}>
+        <div className="space-y-10">
+          {!!accountAddress && (
+            <div className="flex items-center space-x-2 break-all">
+              <span className="flex-1">{accountAddress}</span>
+
+              <CopyAddressButton className="shrink-0" address={accountAddress} />
+            </div>
+          )}
+
+          <SecondaryButton onClick={handleDisconnect} className="w-full">
+            {t('connectButton.disconnect')}
+          </SecondaryButton>
+        </div>
+      </Modal>
+    </>
   );
 };
