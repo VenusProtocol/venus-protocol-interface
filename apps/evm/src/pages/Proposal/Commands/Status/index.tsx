53d27241846b9355cf83885db2eda07bdf904d73
therealemjy <maxime.julian.creations@gmail.com>
2024-11-05 12:11:50 +0100

    fix: small responsive issues with Governance feature

--- apps/evm/src/pages/Proposal/Commands/Status/index.tsx
+++ apps/evm/src/pages/Proposal/Commands/Status/index.tsx
@@ -32,7 +32,7 @@ export const Status: React.FC<StatusProps> = ({
   }, [type]);
 
   const statusDom = (
-    <div className="flex items-center">
+    <div className="flex items-center space-x-1">
       <Icon className="text-inherit w-5 h-5" name={iconName} />
       <span className="text-sm font-semibold">{status}</span>
     </div>
