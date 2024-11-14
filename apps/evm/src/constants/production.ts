5c20a808d757a9fa7b17aa704af60e306ec8b5c4
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2024-11-14 17:42:13 +0100

    refactor: use env variable to determine environment (#3521)

--- apps/evm/src/constants/production.ts
+++ apps/evm/src/constants/production.ts
@@ -1,3 +1,1 @@
 export const MAIN_PRODUCTION_HOST = 'app.venus.io';
-export const ALT_PRODUCTION_HOST = 'app-alt.venus.io';
-export const productionHosts = [MAIN_PRODUCTION_HOST, ALT_PRODUCTION_HOST];
