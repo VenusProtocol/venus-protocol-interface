74ad97365221359ffc6e6edb0537f33c1e698b4a
therealemjy <maxime.julian.creations@gmail.com>
2024-12-02 18:49:55 +0100

    refactor: increase precision of reserve and collateral factors

--- apps/evm/src/utilities/convertFactorFromSmartContract.ts
+++ apps/evm/src/utilities/convertFactorFromSmartContract.ts
@@ -3,6 +3,6 @@ import BigNumber from 'bignumber.js';
 import { COMPOUND_MANTISSA } from 'constants/compoundMantissa';
 
 const convertFactorFromSmartContract = ({ factor }: { factor: BigNumber }) =>
-  new BigNumber(factor).dividedBy(COMPOUND_MANTISSA).dp(2).toNumber();
+  new BigNumber(factor).dividedBy(COMPOUND_MANTISSA).dp(6).toNumber();
 
 export default convertFactorFromSmartContract;
