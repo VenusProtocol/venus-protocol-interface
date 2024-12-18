520255764f59b07948a71a38b99cc3b2e0d173f8
Gleiser Oliveira <gleiser.dev@gmail.com>
2024-12-18 12:26:21 -0600

    feat: add Base mainnet

--- apps/evm/src/libs/tokens/infos/commonTokens/baseMainnet.ts
+++ apps/evm/src/libs/tokens/infos/commonTokens/baseMainnet.ts
@@ -1,5 +1,9 @@
 import { NATIVE_TOKEN_ADDRESS } from 'constants/address';
+import cbbtcLogo from 'libs/tokens/img/cbbtc.svg';
 import ethLogo from 'libs/tokens/img/eth.svg';
+import usdcLogo from 'libs/tokens/img/usdc.svg';
+import wethLogo from 'libs/tokens/img/weth.svg';
+import xvsLogo from 'libs/tokens/img/xvs.svg';
 import type { Token } from 'types';
 
 const ethToken: Token = {
@@ -10,4 +14,31 @@ const ethToken: Token = {
   isNative: true,
 };
 
-export const tokens: Token[] = [ethToken];
+export const tokens: Token[] = [
+  ethToken,
+  {
+    address: '0x4200000000000000000000000000000000000006',
+    decimals: 18,
+    symbol: 'WETH',
+    asset: wethLogo,
+    tokenWrapped: ethToken,
+  },
+  {
+    address: '0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf',
+    decimals: 8,
+    symbol: 'cbBTC',
+    asset: cbbtcLogo,
+  },
+  {
+    address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+    decimals: 6,
+    symbol: 'USDC',
+    asset: usdcLogo,
+  },
+  {
+    address: '0xebB7873213c8d1d9913D8eA39Aa12d74cB107995',
+    decimals: 18,
+    symbol: 'XVS',
+    asset: xvsLogo,
+  },
+];
