03b9eebe1a7d52378c81411cec331d42909ab546
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2025-02-28 09:44:52 +0100

    feat: revert pausing of wUSDM market (#3949)
    Revert "feat: pause wUSDM on zkSync (#3948)"
    
    This reverts commit 013317ed9f0b8891d841394788303a03ff0da6f9.

--- apps/evm/src/libs/tokens/infos/disabledTokenActions/zkSyncMainnet.ts
+++ apps/evm/src/libs/tokens/infos/disabledTokenActions/zkSyncMainnet.ts
@@ -1,9 +1,3 @@
 import type { DisabledTokenAction } from '../../types';
 
-export const disabledTokenActions: DisabledTokenAction[] = [
-  // vwUSDM - Core pool
-  {
-    address: '0x183dE3C349fCf546aAe925E1c7F364EA6FB4033c',
-    disabledActions: ['supply', 'borrow'],
-  },
-];
+export const disabledTokenActions: DisabledTokenAction[] = [];
