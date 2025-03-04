004cccbf2eb1ddfd1cc592ff3191834e60427d42
Gleiser Oliveira <gleiser.dev@gmail.com>
2025-03-04 09:43:03 -0600

    test: add coverage around external rewards

--- apps/evm/src/containers/Layout/testIds.ts
+++ apps/evm/src/containers/Layout/testIds.ts
@@ -4,5 +4,5 @@ export default {
   claimRewardBreakdown: 'layout-claim-reward-breakdown',
   claimExternalRewardBreakdown: 'layout-claim-external-reward-breakdown',
   claimRewardSubmitButton: 'layout-claim-reward-submit-button',
-  claimRewardMobileOverlay: 'layout-claim-reward-mobile-overlay',
+  claimRewardExternalRewards: 'layout-claim-reward-external-rewards',
 };
