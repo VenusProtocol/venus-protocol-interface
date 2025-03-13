f866c702dfb38625a9171469d470366680b22787
Gleiser Oliveira <gleiser.dev@gmail.com>
2025-03-13 11:37:20 -0600

    test: update test with price endpoint mock

--- apps/evm/src/clients/api/queries/getPendingRewards/__tests__/index.spec.ts
+++ apps/evm/src/clients/api/queries/getPendingRewards/__tests__/index.spec.ts
@@ -1,21 +1,14 @@
 import fakeAddress from '__mocks__/models/address';
 import tokens from '__mocks__/models/tokens';
 
-import type {
-  PoolLens,
-  Prime,
-  ResilientOracle,
-  VaiVault,
-  VenusLens,
-  XvsVault,
-} from 'libs/contracts';
+import type { PoolLens, Prime, VaiVault, VenusLens, XvsVault } from 'libs/contracts';
 
+import BigNumber from 'bignumber.js';
 import getPendingRewards from '..';
 import {
   fakeGetIsolatedPoolPendingRewardsOutput,
   fakeGetLegacyPoolPendingRewardsOutput,
   fakeGetPendingXvsOutput,
-  fakeGetPriceOutput,
   fakeGetPrimePendingRewardsOutput,
   fakeGetVaultPaused,
   fakeGetXvsVaultPendingRewardOutput,
@@ -25,10 +18,6 @@ import {
 
 const fakeLegacyPoolComptrollerAddress = '0x94d1820b2D1c7c7452A163983Dc888CEC546b77D';
 const fakeIsolatedPoolComptrollerAddress = '0x1291820b2D1c7c7452A163983Dc888CEC546b78k';
-
-const fakeResilientOracleContract = {
-  getPrice: async () => fakeGetPriceOutput,
-} as unknown as ResilientOracle;
 
 const fakePoolLensContract = {
   getPendingRewards: async () => fakeGetIsolatedPoolPendingRewardsOutput,
@@ -57,9 +46,19 @@ const fakePrimeContract = {
   },
 } as unknown as Prime;
 
+const fakeGetApiTokenPrice = async (tokenAddresses: string[]) =>
+  tokenAddresses.reduce(
+    (acc, tokenAddress) => ({
+      ...acc,
+      [tokenAddress]: new BigNumber('0x30f7dc8a6370b000', 16),
+    }),
+    {},
+  );
+
 describe('getPendingRewards', () => {
   test('returns pool rewards of the user in the correct format on success', async () => {
     const res = await getPendingRewards({
+      getApiTokenPrice: fakeGetApiTokenPrice,
       legacyPoolComptrollerContractAddress: fakeLegacyPoolComptrollerAddress,
       isolatedPoolComptrollerAddresses: [fakeIsolatedPoolComptrollerAddress],
       tokens,
@@ -67,7 +66,6 @@ describe('getPendingRewards', () => {
       accountAddress: fakeAddress,
       poolLensContract: fakePoolLensContract,
       venusLensContract: fakeVenusLensContract,
-      resilientOracleContract: fakeResilientOracleContract,
       vaiVaultContract: fakeVaiVaultContract,
       xvsVaultContract: fakeXvsVaultContract,
     });
@@ -77,6 +75,7 @@ describe('getPendingRewards', () => {
 
   test('returns pool rewards of the user, including Prime rewards, in the correct format on success', async () => {
     const res = await getPendingRewards({
+      getApiTokenPrice: fakeGetApiTokenPrice,
       legacyPoolComptrollerContractAddress: fakeLegacyPoolComptrollerAddress,
       isolatedPoolComptrollerAddresses: [fakeIsolatedPoolComptrollerAddress],
       tokens,
@@ -84,7 +83,6 @@ describe('getPendingRewards', () => {
       accountAddress: fakeAddress,
       poolLensContract: fakePoolLensContract,
       venusLensContract: fakeVenusLensContract,
-      resilientOracleContract: fakeResilientOracleContract,
       vaiVaultContract: fakeVaiVaultContract,
       xvsVaultContract: fakeXvsVaultContract,
       primeContract: fakePrimeContract,
