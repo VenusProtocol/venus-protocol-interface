4ba7fa52d42a6bbe88f57fa63e562d2f57bf5e5b
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2025-03-14 20:17:05 +0100

    feat: increase minimum Paymaster balance to enable gasless transactions (#4019)

--- apps/evm/src/constants/gasLess.ts
+++ apps/evm/src/constants/gasLess.ts
@@ -2,7 +2,7 @@ import BigNumber from 'bignumber.js';
 import { ChainId } from 'types';
 import type { Address } from 'viem';
 
-export const MIN_PAYMASTER_BALANCE_MANTISSA = new BigNumber(10).pow(14);
+export const MIN_PAYMASTER_BALANCE_MANTISSA = new BigNumber(10).pow(15);
 
 export const zyFiWalletAddresses: Partial<Record<ChainId, Address>> = {
   [ChainId.ZKSYNC_MAINNET]: '0x5768CDebd229F690F4bC045AF8e140FC97Ce275D',
