a8db0d88ea8cbcc29c9547f6863e7a260d7cd089
therealemjy <maxime.julian.creations@gmail.com>
2025-05-01 11:02:07 +0200

    Revert "feat: temporarily disable zkETH market on ZKsync (#4227)"
    This reverts commit 11f462e8b79693487472b9d7d740af932d0583da.

--- apps/evm/src/libs/tokens/infos/disabledTokenActions/zkSyncMainnet.ts
+++ apps/evm/src/libs/tokens/infos/disabledTokenActions/zkSyncMainnet.ts
@@ -1,9 +1,3 @@
 import type { DisabledTokenAction } from '../../types';
 
-export const disabledTokenActions: DisabledTokenAction[] = [
-  // zkETH
-  {
-    address: '0xCEb7Da150d16aCE58F090754feF2775C23C8b631',
-    disabledActions: ['supply', 'borrow', 'withdraw', 'repay'],
-  },
-];
+export const disabledTokenActions: DisabledTokenAction[] = [];
