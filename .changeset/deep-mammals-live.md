c4a9a218d9dcc679b5a8c9533a50bf21610e28c9
therealemjy <maxime.julian.creations@gmail.com>
2025-05-16 13:43:11 +0200

    feat: update Twitter to X

---
+++ .changeset/deep-mammals-live.md
@@ -0,0 +1,6 @@
+---
+"@venusprotocol/landing": minor
+"@venusprotocol/evm": minor
+---
+
+update Twitter to X
--- apps/evm/src/containers/Layout/Footer/constants.ts
+++ apps/evm/src/containers/Layout/Footer/constants.ts
@@ -1,5 +1,5 @@
 export const VENUS_DISCORD_URL = 'https://discord.gg/venus-protocol-912811548651708448';
-export const VENUS_TWITTER_URL = 'https://twitter.com/VenusProtocol';
+export const VENUS_X_URL = 'https://x.com/VenusProtocol';
 export const VENUS_GITHUB_URL = 'https://github.com/VenusProtocol/';
 export const VENUS_TELEGRAM_URL = 'https://t.me/venusprotocol';
 export const VENUS_DOC_URL = 'https://docs-v4.venus.io';
