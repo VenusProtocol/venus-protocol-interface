f3ca70a291318765df8fb54b2efeb56b2ab7434c
Gleiser Oliveira <gleiser.dev@gmail.com>
2025-07-08 12:52:19 -0600

    feat: add temporary record to support the vBNB underlying transition

--- apps/evm/src/constants/address.ts
+++ apps/evm/src/constants/address.ts
@@ -1,2 +1,3 @@
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000' as const;
 export const NATIVE_TOKEN_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' as const;
+export const NEW_NATIVE_TOKEN_ADDRESS = '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB' as const;
