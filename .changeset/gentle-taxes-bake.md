f304cc4ea3d471d646d588b7eb139282719d0155
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2025-07-09 16:18:50 +0200

    fix: only show import position modal if user has importable positions (#4508)

---
+++ .changeset/gentle-taxes-bake.md
@@ -0,0 +1,5 @@
+---
+"@venusprotocol/evm": minor
+---
+
+only show import position modal if user has importable positions
--- apps/evm/src/containers/ImportPositionsModal/index.tsx
+++ apps/evm/src/containers/ImportPositionsModal/index.tsx
@@ -1,5 +1,7 @@
+import { useGetProfitableImports } from 'hooks/useGetProfitableImports';
 import { useUserChainSettings } from 'hooks/useUserChainSettings';
 import { useAccountAddress } from 'libs/wallet';
+import { useMeeClient } from 'libs/wallet';
 import { Suspense } from 'react';
 import { safeLazyLoad } from 'utilities';
 
@@ -8,8 +10,15 @@ const Modal = safeLazyLoad(() => import('./Modal'));
 const ImportPositionsModal: React.FC = () => {
   const { accountAddress } = useAccountAddress();
   const [userChainSettings] = useUserChainSettings();
+  const { importablePositionsCount } = useGetProfitableImports();
+  const { data: getMeeClientData } = useMeeClient();
 
-  if (!accountAddress || userChainSettings.doNotShowImportPositionsModal) {
+  if (
+    !accountAddress ||
+    userChainSettings.doNotShowImportPositionsModal ||
+    importablePositionsCount === 0 ||
+    !getMeeClientData
+  ) {
     return undefined;
   }
 
