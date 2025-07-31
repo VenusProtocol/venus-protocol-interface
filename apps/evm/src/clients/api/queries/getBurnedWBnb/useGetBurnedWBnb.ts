2cf6674b7c579b6e6fc0110d27cda6ab361d88f2
therealemjy <maxime.julian.creations@gmail.com>
2025-07-31 10:49:39 +0200

    feat: only fetch burned BNB when feature is enabled

--- apps/evm/src/clients/api/queries/getBurnedWBnb/useGetBurnedWBnb.ts
+++ apps/evm/src/clients/api/queries/getBurnedWBnb/useGetBurnedWBnb.ts
@@ -1,6 +1,7 @@
 import { type QueryObserverOptions, useQuery } from '@tanstack/react-query';
 
 import FunctionKey from 'constants/functionKey';
+import { useIsFeatureEnabled } from 'hooks/useIsFeatureEnabled';
 import { usePublicClient } from 'libs/wallet';
 import { type GetBurnedWBnbOutput, getBurnedWBnb } from '.';
 
@@ -16,10 +17,13 @@ type Options = QueryObserverOptions<
 
 export const useGetBurnedWBnb = (options?: Partial<Options>) => {
   const { publicClient } = usePublicClient();
+  const isBurnedWBnbButtonFeatureEnabled = useIsFeatureEnabled({ name: 'burnedWBnbButton' });
 
   return useQuery({
     queryKey: [FunctionKey.GET_BURNED_BNB],
     queryFn: () => getBurnedWBnb({ publicClient }),
     ...options,
+    enabled:
+      (options?.enabled === undefined || options?.enabled) && isBurnedWBnbButtonFeatureEnabled,
   });
 };
