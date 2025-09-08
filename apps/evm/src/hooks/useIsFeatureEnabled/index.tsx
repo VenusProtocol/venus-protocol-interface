183eb3a468fc82f11d1c92e0992c7ddc573eae66
Gleiser Oliveira <gleiser.dev@gmail.com>
2025-09-07 21:43:07 -0600

    feat: add historicalTransactions feature flag

--- apps/evm/src/hooks/useIsFeatureEnabled/index.tsx
+++ apps/evm/src/hooks/useIsFeatureEnabled/index.tsx
@@ -156,6 +156,7 @@ export const featureFlags = {
     ChainId.BASE_SEPOLIA,
     ChainId.UNICHAIN_SEPOLIA,
   ],
+  transactionHistory: [ChainId.BSC_MAINNET, ChainId.ETHEREUM, ChainId.UNICHAIN_MAINNET],
 };
 
 export type FeatureFlag = keyof typeof featureFlags;
