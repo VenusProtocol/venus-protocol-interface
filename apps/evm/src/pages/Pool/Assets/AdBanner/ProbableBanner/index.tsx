de66adab6d9bb4056ca2c0e152404751d16d4630
david-sun-venus <david.sun@venus.io>
2026-01-06 15:22:00 +0800

    feat(evm): add probable banner, isolated-pool sunset tooltips

---
+++ apps/evm/src/pages/Pool/Assets/AdBanner/ProbableBanner/index.tsx
@@ -0,0 +1,38 @@
+import { useTranslation } from 'libs/translations';
+import { Banner } from '../Banner';
+import probaleIcon from './probaleIcon.png';
+
+const LEARN_MORE_URL = 'https://probable.markets';
+
+export const ProbableBanner: React.FC = () => {
+  const { t, Trans } = useTranslation();
+
+  return (
+    <Banner
+      className="bg-gradient-to-r from-[#0E3FB7] from-10% via-[#00193A] via-[70.38%] to-[#001E68] to-[110.07%]"
+      title={<span className="text-offWhite">{t('probableBanner.title')}</span>}
+      description={
+        <span className="text-grey">
+          <Trans
+            i18nKey="probableBanner.description"
+            components={{
+              White: <span className="text-offWhite" />,
+            }}
+          />
+        </span>
+      }
+      illustration={
+        <div className="h-10 w-14 sm:h-12 sm:w-[78px] lg:h-14">
+          <img
+            src={probaleIcon}
+            alt={t('probableBanner.probableAltText')}
+            className="absolute size-14 -mt-4 sm:-mt-[22px] sm:size-[78px]"
+          />
+        </div>
+      }
+      contentContainerClassName="gap-x-1"
+      learnMoreUrl={LEARN_MORE_URL}
+      learnMoreText={t('adBanner.startNow')}
+    />
+  );
+};
