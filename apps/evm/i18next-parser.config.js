26e4b87c522845c17a4947d4083eeb22f32d0fa5
Maxime Julian <44675210+therealemjy@users.noreply.github.com>
2026-01-19 08:24:07 +0100

    feat: add support for multiple languages (#5226)

--- apps/evm/i18next-parser.config.js
+++ apps/evm/i18next-parser.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   defaultValue: 'TRANSLATION NEEDED',
-  locales: ['en'],
+  locales: ['en', 'zh-Hans', 'zh-Hant', 'ja', 'vi', 'th', 'tr'],
   output: './src/libs/translations/translations/$LOCALE.json',
   input: './src/**/*.{ts,tsx}',
   sort: true,
--- apps/evm/src/libs/translations/index.tsx
+++ apps/evm/src/libs/translations/index.tsx
@@ -1,38 +1,74 @@
 import { format as formatDate, formatDistanceToNowStrict, isDate } from 'date-fns';
 import { enUS } from 'date-fns/locale';
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-import EnLocales from './translations/en.json';
+import { supportedLanguages } from './constants';
+import enLocales from './translations/en.json';
+import jaLocales from './translations/ja.json';
+import thLocales from './translations/th.json';
+import trLocales from './translations/tr.json';
+import viLocales from './translations/vi.json';
+import zhHansLocales from './translations/zh-Hans.json';
+import zhHantLocales from './translations/zh-Hant.json';
 
 export { default as en } from './translations/en.json';
 
 const init = () => {
-  i18next.use(initReactI18next).init({
-    resources: {
-      en: {
-        translation: EnLocales,
+  i18next
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      resources: {
+        en: {
+          translation: enLocales,
+        },
+        th: {
+          translation: thLocales,
+        },
+        tr: {
+          translation: trLocales,
+        },
+        ja: {
+          translation: jaLocales,
+        },
+        vi: {
+          translation: viLocales,
+        },
+        'zh-Hans': {
+          translation: zhHansLocales,
+        },
+        'zh-Hant': {
+          translation: zhHantLocales,
+        },
       },
-    },
-    lng: 'en', // We only support English for now, but we'll need to detect the user's locale once we support more languages
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-      format: (value, format, lng) => {
-        const locales = { en: enUS };
+      supportedLngs: supportedLanguages.map(language => language.bcp47Tag),
+      fallbackLng: 'en',
+      detection: {
+        order: ['localStorage', 'navigator'],
+        caches: ['localStorage'],
+        // Normalize odd inputs like en_US -> en-US
+        convertDetectedLanguage: (lng: string) => lng.replace('_', '-'),
+      },
+      interpolation: {
+        escapeValue: false,
+        format: (value, format, lng) => {
+          const locales = { en: enUS };
 
-        if (isDate(value)) {
-          const locale = lng && lng in locales ? locales[lng as keyof typeof locales] : locales.en;
-          if (format === 'distanceToNow') {
-            return formatDistanceToNowStrict(value, { locale });
+          if (isDate(value)) {
+            const locale =
+              lng && lng in locales ? locales[lng as keyof typeof locales] : locales.en;
+            if (format === 'distanceToNow') {
+              return formatDistanceToNowStrict(value, { locale });
+            }
+
+            return formatDate(value, format || 'dd MMM yyyy h:mm a', { locale });
           }
-
-          return formatDate(value, format || 'dd MMM yyyy h:mm a', { locale });
-        }
-        return value;
+          return value;
+        },
       },
-    },
-  });
+    });
   i18next.loadNamespaces('errors');
   return i18next;
 };
@@ -42,6 +78,6 @@ const i18NextInstance = init();
 // Only use this function when you need to render a string from outside a
 // component. Otherwise, use the t function or Trans component returned by the
 // useTranslation hook.
-export const { t } = i18NextInstance;
+export const { t, changeLanguage } = i18NextInstance;
 
 export * from './useTranslation';
