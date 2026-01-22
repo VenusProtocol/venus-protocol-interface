0c462e9a92391d4b6e179c62646e79d032c2adf9
david-sun-venus <david.sun@venus.io>
2026-01-23 00:05:27 +0800

    feat(evm): new landing phase 2

--- apps/evm/src/pages/Landing/Protection/index.tsx
+++ apps/evm/src/pages/Landing/Protection/index.tsx
@@ -8,14 +8,12 @@ interface IProtectionProps {
 }
 
 const itemClassName = cn(
-  'relative flex flex-1 flex-col min-h-105 justify-between p-6 pb-0 border border-solid border-lightGrey rounded-3xl sm:min-h-100 xl:min-h-139 xl:p-10 xl:pb-0',
+  'relative flex md:flex-1 overflow-hidden flex-col h-87.5 justify-between p-6 pb-0 border border-solid border-lightGrey rounded-3xl md:h-112.5 lg:h-139 xl:p-10 xl:pb-0',
 );
 
-const bgImgClassName = cn('w-full bottom-0');
-
 const textClassName = cn(
-  '[&_h2]:text-[1.5rem] [&_h2]:font-semibold xl:[&_h2]:text-[2rem]',
-  '[&_p]:text-grey [&_p]:text-[1rem] xl:[&_p]:text-[1.125rem]',
+  '[&_h6]:text-p2s [&_h6]:md:text-p1s [&_h6]:lg:text-h6',
+  '[&_p]:text-grey [&_p]:text-b1r [&_p]:lg:text-p3r',
 );
 
 export const Protection: React.FC<IProtectionProps> = ({ className }) => {
@@ -23,20 +21,20 @@ export const Protection: React.FC<IProtectionProps> = ({ className }) => {
 
   return (
     <div className={cn('mt-15 md:mt-20 lg:mt-25', textClassName, className)}>
-      <ul className={'flex flex-col justify-between gap-6 sm:flex-row xl:gap-8'}>
+      <ul className={'flex flex-col justify-between gap-6 md:flex-row xl:gap-8'}>
         <li
           className={cn(
             itemClassName,
             'bg-radial-[62.14%_57.90%_at_50.00%_50.00%,rgba(23,46,98,0.50)_0%,rgba(18,22,32,0.50)_100%]',
           )}
         >
-          <div className={'xl:mb-21'}>
-            <h2 className="mb-4">{t('landing.protection.challengeTitle')}</h2>
+          <div className={'mb-6 xl:mb-12'}>
+            <h6 className="mb-4">{t('landing.protection.challengeTitle')}</h6>
             <p className="mb-8 md:mb-0">{t('landing.protection.challengeText')}</p>
           </div>
           <img
             loading="lazy"
-            className={bgImgClassName}
+            className={'w-full'}
             src={bugBountyImg}
             alt={t('landing.protection.bugBountyAlt')}
           />
@@ -47,13 +45,15 @@ export const Protection: React.FC<IProtectionProps> = ({ className }) => {
             'bg-radial-[62.14%_57.90%_at_50.00%_50.00%,rgba(30,75,100,0.50)_0%,rgba(18,22,32,0.50)_100%]',
           )}
         >
-          <div className={'xl:mb-21'}>
-            <h2 className="mb-4">{t('landing.protection.protectionTitle')}</h2>
-            <p className="mb-8 md:mb-0">{t('landing.protection.protectionText')}</p>
+          <div className={'xl:mb-2'}>
+            <h6 className="mb-4">{t('landing.protection.protectionTitle')}</h6>
+            <p className="mb-4 sm:mb-3 md:mb-0">{t('landing.protection.protectionText')}</p>
           </div>
           <img
             loading="lazy"
-            className={bgImgClassName}
+            className={
+              'max-sm:w-full max-sm:pb-8 object-cover object-[0%_50%] md:min-w-[160%] md:pb-8 md:-translate-x-[15%] xl:min-w-[130%] xl:-translate-x-[10%]'
+            }
             src={protectionImg}
             alt={t('landing.protection.protectionAlt')}
           />
