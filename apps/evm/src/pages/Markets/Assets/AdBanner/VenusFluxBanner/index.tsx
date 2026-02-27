168cbb8e5167b1b71f02afa8e988403c8ba6d10a
therealemjy <maxime.julian.creations@gmail.com>
2026-02-27 13:37:07 +0100

    feat: update Venus Flux banner

--- apps/evm/src/pages/Markets/Assets/AdBanner/VenusFluxBanner/index.tsx
+++ apps/evm/src/pages/Markets/Assets/AdBanner/VenusFluxBanner/index.tsx
@@ -1,47 +1,39 @@
+import { ButtonWrapper } from '@venusprotocol/ui';
 import { VENUS_FLUX_URL } from 'constants/production';
+import { Link } from 'containers/Link';
+import { useBreakpointUp } from 'hooks/responsive';
 import { useTranslation } from 'libs/translations';
-import { Banner } from '../Banner';
-import bgIllustrationSrc from './bgIllustration.jpg';
-import venusFluxIconSrc from './venusFluxIcon.png';
+import mobileIllustrationSrc from './mobileIllustration.png';
+import tabletIllustrationSrc from './tabletIllustration.png';
 
 export const VenusFluxBanner: React.FC = () => {
-  const { t, Trans } = useTranslation();
+  const { t } = useTranslation();
+  const isSmOrUp = useBreakpointUp('sm');
 
   return (
-    <Banner
-      className="bg-black"
-      title={<span className="text-offWhite">{t('venusFluxBanner.title')}</span>}
-      description={
-        <span className="text-grey">
-          <Trans
-            i18nKey="venusFluxBanner.description"
-            components={{
-              White: <span className="text-offWhite" />,
-            }}
-          />
-        </span>
-      }
-      illustration={
-        <div className="h-10 w-16 lg:h-14">
-          <img
-            src={venusFluxIconSrc}
-            alt={t('venusFluxBanner.iconAltText')}
-            className="absolute size-16 -mt-5.5 sm:-mt-3"
-          />
-        </div>
-      }
-      backgroundIllustration={
-        <div className="absolute inset-0">
-          <img
-            src={bgIllustrationSrc}
-            alt={t('venusFluxBanner.bgIllustrationAltText')}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      }
-      contentContainerClassName="sm:gap-x-3"
-      learnMoreUrl={VENUS_FLUX_URL}
-      learnMoreLabel={t('adBanner.startNow')}
-    />
+    <div className="relative rounded-lg h-26.25 px-4 pt-2 pb-3 flex flex-col gap-y-3 bg-linear-to-r from-[#363636] to-[#0C0C0C] sm:h-18 sm:flex-row sm:items-center sm:justify-between sm:py-3 sm:pl-31">
+      <img
+        src={mobileIllustrationSrc}
+        className="absolute left-4 bottom-0 h-12 sm:hidden"
+        alt={t('venusFluxBanner.illustrationAltText')}
+      />
+
+      <img
+        src={tabletIllustrationSrc}
+        className="hidden absolute bottom-0 left-3 h-19 sm:block"
+        alt={t('venusFluxBanner.illustrationAltText')}
+      />
+
+      <div className="space-y-0.5">
+        <p className="font-semibold text-sm lg:text-base">{t('venusFluxBanner.title')}</p>
+        <p className="text-xs text-grey lg:text-sm">{t('venusFluxBanner.description')}</p>
+      </div>
+
+      <ButtonWrapper size={isSmOrUp ? 'md' : 'xs'} className="self-end" asChild>
+        <Link href={VENUS_FLUX_URL} target="_blank" noStyle>
+          {t('adBanner.startNow')}
+        </Link>
+      </ButtonWrapper>
+    </div>
   );
 };
