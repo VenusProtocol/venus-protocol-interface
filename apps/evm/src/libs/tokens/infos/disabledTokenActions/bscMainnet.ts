3d281ad2e3e83216dfbf6ceaa7d748acc98d9642
Gleiser Oliveira <gleiser.dev@gmail.com>
2026-03-15 08:00:49 -0600

    fix: pause vTHE borrows

--- apps/evm/src/libs/tokens/infos/disabledTokenActions/bscMainnet.ts
+++ apps/evm/src/libs/tokens/infos/disabledTokenActions/bscMainnet.ts
@@ -5,4 +5,8 @@ export const disabledTokenActions: DisabledTokenAction[] = [
     address: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
     disabledActions: ['boost', 'repayWithCollateral', 'swapAndSupply', 'swapAndRepay'],
   },
+  {
+    address: '0x86e06EAfa6A1eA631Eab51DE500E3D474933739f',
+    disabledActions: ['borrow'],
+  },
 ];
