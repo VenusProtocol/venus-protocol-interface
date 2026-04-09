c71329476c50ecf8275b7a7deda64f7ed4389ba9
cuzz-venus <cuzz.d@venus.io>
2026-04-09 16:13:25 +0800

    fix: support br on trans

--- apps/evm/src/pages/Landing/Safety/index.tsx
+++ apps/evm/src/pages/Landing/Safety/index.tsx
@@ -71,7 +71,7 @@ const auditors: AuditorProps[] = [
 ];
 
 export const Safety: React.FC<ISafetyProps> = ({ className }) => {
-  const { t } = useTranslation();
+  const { t, Trans } = useTranslation();
 
   return (
     <section className={className}>
@@ -96,7 +96,12 @@ export const Safety: React.FC<ISafetyProps> = ({ className }) => {
               )}
               href={SECURITY_DOC_URL}
             >
-              <p className="max-w-38 text-b1r">{t('landing.safety.otherAuditors')}</p>
+              <p className="max-w-38 text-b1r">
+                <Trans
+                  i18nKey="landing.safety.otherAuditors"
+                  components={{ br: <br /> }}
+                />
+              </p>
             </Link>
           </div>
         </div>
