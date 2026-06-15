7379dc11052ae782d84eb5625230695b1474b99b
cuzz-venus <cuzz.d@venus.io>
2026-06-15 18:52:20 +0800

    fix: remove last cycle modal

--- apps/evm/src/pages/PrimeLeaderboard/RankCard/RankActions/index.tsx
+++ apps/evm/src/pages/PrimeLeaderboard/RankCard/RankActions/index.tsx
@@ -1,35 +1,25 @@
 import { Button, ButtonWrapper } from '@venusprotocol/ui';
-import { useState } from 'react';
 
 import { Icon } from 'components';
 import { routes } from 'constants/routing';
 import { Link } from 'containers/Link';
 import { useTranslation } from 'libs/translations';
 
-import { LastCycleSummaryModal } from '../../LastCycleSummaryModal';
-
 export const RankActions: React.FC = () => {
   const { t } = useTranslation();
-  const [isSummaryModalOpen, setIsSummaryModalOpen] = useState(false);
 
   return (
-    <>
-      <div className="flex gap-x-2.5">
-        <ButtonWrapper asChild className="flex-1">
-          <Link to={routes.vaults.path}>{t('primeLeaderboard.rankCard.stakeButton')}</Link>
-        </ButtonWrapper>
+    <div className="flex gap-x-2.5">
+      <ButtonWrapper asChild className="flex-1">
+        <Link to={routes.vaults.path}>{t('primeLeaderboard.rankCard.stakeButton')}</Link>
+      </ButtonWrapper>
 
-        <Button variant="secondary" className="flex-1" onClick={() => setIsSummaryModalOpen(true)}>
-          <div className="flex items-center gap-x-2">
-            <Icon name="graduationCap" className="size-5" />
-            {t('primeLeaderboard.rankCard.rulesButton')}
-          </div>
-        </Button>
-      </div>
-
-      {isSummaryModalOpen && (
-        <LastCycleSummaryModal isOpen handleClose={() => setIsSummaryModalOpen(false)} />
-      )}
-    </>
+      <Button variant="secondary" className="flex-1">
+        <div className="flex items-center gap-x-2">
+          <Icon name="graduationCap" className="size-5" />
+          {t('primeLeaderboard.rankCard.rulesButton')}
+        </div>
+      </Button>
+    </div>
   );
 };
